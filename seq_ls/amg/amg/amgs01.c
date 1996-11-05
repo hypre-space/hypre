76dce8005e8cf8e645492ffaff66736f29fd8b7b
vhenson <vhenson>
1996-11-05 23:38:54 +0000

    shifted indices

--- seq_ls/amg/amg/amgs01.c
+++ seq_ls/amg/amg/amgs01.c
@@ -47,13 +47,14 @@ Data        *data;
    int      num_variables = ProblemNumVariables(problem);
    int      num_points    = ProblemNumPoints(problem);
 
-   int      num_levels;
    Matrix  *A;
+   Matrix  *P;
+
+   int      num_levels;
    int      ndimu;
    int      ndimp;
    int      ndima;
    int      ndimb;
-   Matrix  *P;
    int     *icdep;
    int     *imin;
    int     *imax;
@@ -62,12 +63,30 @@ Data        *data;
    int     *icg;
    int     *ifg;
    int     *ifc;
+   Matrix  **A_array;
+   Matrix  **P_array;
 
+   double  *a;
    int     *ia;
+   int     *ja; 
 
    double  *b;
    int     *ib;
    int     *jb;
+
+   int     *ip;
+   int     *iv;
+
+   int     *leva;
+   int     *numa;
+   int     *levb;
+   int     *numb;
+   int     *levv;
+   int     *numv;
+   int     *levp;
+   int     *nump;
+   int     *levi;
+
 
 
    /*----------------------------------------------------------
@@ -100,6 +119,16 @@ Data        *data;
    ifg        = ctalloc(int, ndimu);
    ifc        = ctalloc(int, ndimu);
 
+   leva       = ctalloc(int, levmax);
+   numa       = ctalloc(int, levmax);
+   levb       = ctalloc(int, levmax);
+   numb       = ctalloc(int, levmax);
+   levv       = ctalloc(int, levmax);
+   numv       = ctalloc(int, levmax);
+   levp       = ctalloc(int, levmax);
+   nump       = ctalloc(int, levmax);
+   levi       = ctalloc(int, levmax);
+
    /* set fine level point and variable bounds */
    ipmn[0] = 1;
    ipmx[0] = num_points;
@@ -114,11 +143,11 @@ Data        *data;
 
    AMGS01DataNumLevels(amgs01_data) = num_levels;
    AMGS01DataA(amgs01_data)         = A;
+   AMGS01DataP(amgs01_data)         = P;   
    AMGS01DataNDIMU(amgs01_data)     = ndimu;
    AMGS01DataNDIMP(amgs01_data)     = ndimp;
    AMGS01DataNDIMA(amgs01_data)     = ndima;
-   AMGS01DataNDIMB(amgs01_data)     = ndimb;
-   AMGS01DataP(amgs01_data)         = P;
+   AMGS01DataNDIMB(amgs01_data)     = ndimb;   
    AMGS01DataICDep(amgs01_data)     = icdep;
    AMGS01DataIMin(amgs01_data)      = imin;
    AMGS01DataIMax(amgs01_data)      = imax;
@@ -133,6 +162,184 @@ Data        *data;
     *----------------------------------------------------------*/
 
    CALL_SETUP(problem, amgs01_data);
+
+       
+{
+   int      index, size;
+   int     *data;
+   int      decr;
+
+   FILE    *fp;
+   int      j;
+   int      k;
+   int      num_cols;
+   int      num_coefs;
+   char     fnam[255];
+   char    *fnam_num;
+
+   num_levels = AMGS01DataNumLevels(amgs01_data);
+   A_array = talloc(Matrix*, num_levels);
+   P_array = talloc(Matrix*, num_levels-1);
+
+   ia = MatrixIA(A);
+   ja = MatrixJA(A);
+   a = MatrixData(A);
+   ib = MatrixIA(P);
+   jb = MatrixJA(P);
+   b = MatrixData(P);
+   ip = ProblemIP(problem);
+   iv = ProblemIV(problem);
+
+   A_array[0] = A;
+   P_array[0] = P;
+
+/* Shifting indices */
+
+#if 0
+   for (j = 0; j < num_levels; j++)
+   {
+       leva[j] = ia[imin[j]-1];
+       numa[j] = ia[imax[j]+1-1]-ia[imin[j]-1];
+       levb[j] = ib[imin[j]-1];
+       numb[j] = ib[imax[j]+1-1]-ib[imin[j]-1];
+       levv[j] = imin[j];
+       numv[j] = imax[j] - imin[j] + 1;
+       levp[j] = ipmn[j];
+       nump[j] = ipmx[j] - ipmn[j] + 1;
+       levi[j] = imin[j] + j;
+   }
+
+   for (j = num_levels-1; j > 0; j--)
+   {
+       for (k = numv[j]; k >= 0; k--)
+       {
+           ia[levi[j]+k-1] = ia[levv[j]+k-1];
+           ib[levi[j]+k-1] = ib[levv[j]+k-1];
+       }
+   }
+
+   decr = numv[0];
+   for (j = 1; j < num_levels; j++)
+   {
+       for (k = leva[j]; k < leva[j] + numa[j]; k++)
+       {
+           ja[k-1] -= decr;
+       }
+       decr += numv[j];
+   }
+   decr = numv[0];
+   for (j = 1; j < num_levels; j++)
+   {
+       for (k = levb[j]; k < levb[j] + numb[j]; k++)
+       {
+           jb[k-1] -= decr;
+       }
+       decr += numv[j];
+   }
+
+   decr = numa[0];
+   for (j = 1; j < num_levels; j++)
+   {
+       for (k = levi[j]; k < levi[j] + numv[j] + 1; k++)
+       {
+           ia[k-1] -= decr;
+       }
+       decr += numa[j];
+   }
+   decr = numb[0];
+   for (j = 1; j < num_levels; j++)
+   {
+       for (k = levi[j]; k < levi[j] + numv[j] + 1; k++)
+       {
+           ib[k-1] -= decr;
+       }
+       decr += numb[j];
+   }
+
+   decr = numv[0];
+   for (j = 1; j < num_levels; j++)
+   {
+       for (k = levp[j]; k < levp[j] + nump[j]; k++)
+       {
+           iv[k-1] -= decr;
+       }
+       decr += numv[j];
+   }
+
+   decr = nump[0];
+   for (j = 1; j < num_levels; j++)
+   {
+       for (k = levv[j]; k < levv[j] + numv[j]; k++)
+       {
+           ip[k-1] -= decr;
+       }
+       decr += nump[j];
+   }
+ 
+   decr = numv[0];  
+   for (j = 1; j < num_levels; j++)
+   {
+       imin[j] -= decr;
+       imax[j] -= decr;
+       decr += numv[j];
+   }
+ 
+   decr = nump[0];  
+   for (j = 1; j < num_levels; j++)
+   {
+       ipmn[j] -= decr;
+       ipmx[j] -= decr;
+       decr += nump[j];
+   }
+
+#endif
+
+   for (j = 1; j < num_levels; j++)
+   {
+        A_array[j] = NewMatrix(&a[ia[ipmn[j]-1]-1],&ia[ipmn[j]-1], \
+                              &ja[ia[ipmn[j]-1]-1],ipmx[j]-ipmn[j]+1);
+   }
+
+   for (j = 1; j < num_levels-1; j++)
+   {
+        P_array[j] = NewMatrix(&b[ib[ipmn[j]-1]-1],&ib[ipmn[j]-1], \
+                              &jb[ib[ipmn[j]-1]-1],ipmx[j]-ipmn[j]+1);
+   }
+
+   AMGS01DataAArray(amgs01_data) = A_array;
+   AMGS01DataPArray(amgs01_data) = P_array;   
+
+#if 0
+   for (j = 1; j < num_levels; j++)
+   {
+          sprintf(fnam,"level_%d.ysmp",j);
+          WriteYSMP(fnam, A_array[j]);
+   }
+/*   fclose(fp); */
+
+/*   WriteYSMP("zP.ysmp", AMGS01DataP(amgs01_data)); */
+
+   index = AMGS01DataIMin(amgs01_data)[0] - 1;
+   size = AMGS01DataIMax(amgs01_data)[0] - index;
+   data = AMGS01DataICG(amgs01_data) + index;
+   fp = fopen("zC.vec", "w");
+   fprintf(fp, "1 1\n");
+   fprintf(fp, "%d\n", size);
+   for (j = 0; j < size; j++)
+      fprintf(fp, "%d\n", data[j]);
+   fclose(fp);
+
+/* veh 
+   data = AMGS01DataIFG(amgs01_data) + index;
+   fp = fopen("zF.vec", "w");
+   fprintf(fp, "1 1\n");
+   fprintf(fp, "%d\n", size);
+   for (j = 0; j < size; j++)
+      fprintf(fp, "%d\n", data[j]);
+   fclose(fp);
+*/
+#endif
+}
 }
 
 /*--------------------------------------------------------------------------
@@ -284,3 +491,4 @@ Data        *data;
       tfree(amgs01_data);
    }
 }
+
