cd58d53c876bf73d48980702a0a6887a4c3ce33b
vhenson <vhenson>
1996-11-19 23:12:02 +0000

    allows fully specified filename- no default extensions

--- seq_linear_solvers/amg/tools/writeysmp.m
+++ seq_linear_solvers/amg/tools/writeysmp.m
@@ -1,10 +1,11 @@
 function writeysmp(nv,ia,ja,a,fname)
 %  function writeysmp(nv,ia,ja,a,fname)
 %
-%  writes ysmp file in 'fname'.in.ysmp
+%  writes ysmp file in 'fname' (no
+%  qualifiers added, eg. not 'fname.ysmp')
 %
 
-outdat = [fname '.in.ysmp'];
+outdat = [fname];
 fid=fopen(outdat,'w');
 
 fprintf(fid,'1 1\n'); % dummy row
