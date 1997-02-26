a62a069b86b9c5fea60dc9b720668a6932f11dc3
vhenson <vhenson>
1997-02-26 00:18:56 +0000

    Removed unneeded CALL_SOLVE macro

--- seq_linear_solvers/amg/amg/fortran.h
+++ seq_linear_solvers/amg/amg/fortran.h
@@ -63,73 +63,12 @@ void setup_(int *, int *, int *, double *, int *, double *, int *,
             int *, int *, int *, int *,
 	    char *, long);
 
-/* solve */
-#define CALL_SOLVE(Solve_err_flag, u, f, tol, amg_data) \
-solve_(&Solve_err_flag,\
-       &tol,\
-       &AMGDataNumLevels(amg_data),\
-       &AMGDataNCyc(amg_data),\
-       AMGDataMU(amg_data),\
-       AMGDataNTRLX(amg_data),\
-       AMGDataIPRLX(amg_data),\
-       AMGDataIERLX(amg_data),\
-       AMGDataIURLX(amg_data),\
-       &AMGDataIOutDat(amg_data),\
-       &AMGDataNumUnknowns(amg_data),\
-       AMGDataIMin(amg_data),\
-       AMGDataIMax(amg_data),\
-       VectorData(u),\
-       VectorData(f),\
-       AMGDataVecTemp(amg_data),\
-       MatrixData(AMGDataA(amg_data)),\
-       MatrixIA(AMGDataA(amg_data)),\
-       MatrixJA(AMGDataA(amg_data)),\
-       AMGDataIU(amg_data),\
-       AMGDataICG(amg_data),\
-       MatrixData(AMGDataP(amg_data)),\
-       MatrixIA(AMGDataP(amg_data)),\
-       MatrixJA(AMGDataP(amg_data)),\
-       AMGDataIPMN(amg_data),\
-       AMGDataIPMX(amg_data),\
-       AMGDataIV(amg_data),\
-       AMGDataIP(amg_data),\
-       AMGDataXP(amg_data),\
-       AMGDataYP(amg_data),\
-       &AMGDataNDIMU(amg_data),\
-       &AMGDataNDIMP(amg_data),\
-       &AMGDataNDIMA(amg_data),\
-       &AMGDataNDIMB(amg_data),\
-       AMGDataLevA(amg_data),\
-       AMGDataLevB(amg_data),\
-       AMGDataLevV(amg_data),\
-       AMGDataLevPI(amg_data),\
-       AMGDataLevI(amg_data),\
-       AMGDataNumA(amg_data),\
-       AMGDataNumB(amg_data),\
-       AMGDataNumV(amg_data),\
-       AMGDataNumP(amg_data),\
-       AMGDataLogFileName(amg_data),\
-       strlen(AMGDataLogFileName(amg_data)));
 
-void solve_(int *, double *, int *, int *, int *, int *, int *, 
-            int *, int *,
-	    int *, int *, int *, int *,
-	    double *, double *, double *,
-	    double *, int *, int *,
-	    int *, int *,
-	    double *, int *, int *,
-	    int *, int *, int *, int *,
-	    double *, double *,
-            int *, int *, int *, int *,
-            int *, int *, int *, int *, int *,
-            int *, int *, int *, int *,
-	    char *, long);
-
-/* New stuff */
+/* idec */
 
 void idec_(int *, int *, int *, int *);
 
-
+/* rsdl */
 #define CALL_RESIDUAL(energy,residual_nrm,resv,u,f,A_array,amg_data) \
 rsdl_(&energy,\
       &residual_nrm,\
@@ -147,7 +86,7 @@ rsdl_(&energy,\
 void rsdl_(double *, double *, double *, double *, int *, int *,
            double *, double *, double *, int *, int *, int *);  
 
-
+/* cycle */
 #define CALL_CYCLE(Solve_err_flag, u, f, tol, amg_data) \
 cycle_(&Solve_err_flag,\
        &AMGDataNumLevels(amg_data),\
