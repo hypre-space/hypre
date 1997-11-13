b6c513e2bedaa7b93690b6947b421eee02f727d4
falgout <falgout>
1997-11-13 21:54:48 +0000

    Initial checkin.

---
+++ struct_linear_solvers/smg3_solve.c
@@ -0,0 +1,25 @@
+/*BHEADER**********************************************************************
+ * (c) 1997   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+/******************************************************************************
+ *
+ *
+ *****************************************************************************/
+
+#include "headers.h"
+
+/*--------------------------------------------------------------------------
+ * zzz_SMG3Solve
+ *--------------------------------------------------------------------------*/
+
+int
+zzz_SMG3Solve( zzz_SMG3Struct   *smg3_struct,
+               zzz_StructVector *x,
+               zzz_StructVector *b           )
+{
+}
