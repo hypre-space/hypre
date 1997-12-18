730745506f4b9a18bf55be982cc30999a25f30f5
falgout <falgout>
1997-12-18 19:19:52 +0000

    Adding skeleton of file for other developers to work on.

---
+++ struct_linear_solvers/smg_setup_rap.c
@@ -0,0 +1,45 @@
+/*BHEADER**********************************************************************
+ * (c) 1997   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+/******************************************************************************
+ *
+ *
+ *****************************************************************************/
+
+#include "headers.h"
+#include "smg.h"
+
+/*--------------------------------------------------------------------------
+ * zzz_SMGNewRAPOp
+ *--------------------------------------------------------------------------*/
+ 
+zzz_StructMatrix *
+zzz_SMGNewRAPOp( zzz_StructMatrix *R,
+                 zzz_StructMatrix *A,
+                 zzz_StructMatrix *PT )
+{
+   zzz_StructMatrix *RAP;
+ 
+   return RAP;
+}
+
+/*--------------------------------------------------------------------------
+ * zzz_SMGSetupRAPOp
+ *--------------------------------------------------------------------------*/
+ 
+int
+zzz_SMGSetupRAPOp( zzz_StructMatrix *R,
+                   zzz_StructMatrix *A,
+                   zzz_StructMatrix *PT,
+                   zzz_StructMatrix *Ac )
+{
+   int ierr;
+ 
+   return ierr;
+}
+
