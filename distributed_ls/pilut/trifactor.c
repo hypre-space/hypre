4c6c0e71d3ed6c8c922b4b1239600adb3cd00b32
chow <chow>
1998-03-03 17:49:30 +0000

    Added Makefile.alpha; fixed trifactor.c for 1 processor case.

--- distributed_ls/pilut/trifactor.c
+++ distributed_ls/pilut/trifactor.c
@@ -42,6 +42,8 @@ void LDUSolve(DataDistType *ddist, FactorMatType *ldu, double *x, double *b,
   double *lx, *ux, *values, *dvalues, *gatherbuf, **raddr, xx;
   MPI_Status Status;
 
+  int ntemp;
+
   PrintLine("LDUSolve start", globals);
 
   lnrows    = ddist->ddist_lnrows;
@@ -76,9 +78,12 @@ void LDUSolve(DataDistType *ddist, FactorMatType *ldu, double *x, double *b,
   colind = ldu->lcolind;
   values = ldu->lvalues;
 
+  ntemp = nnodes[1];
+  if (nlevels == 0) ntemp = nnodes[0];
+
   /* Do the local first.
    * For forward substitution we do local+1st MIS == nnodes[1] (NOT [0]!) */
-  for (i=0; i<nnodes[1]; i++) {
+  for (i=0; i<ntemp; i++) {
     xx = 0.0;
     for (j=rowptr[i]; j<rowptr[i+1]; j++) 
       xx += values[j]*lx[colind[j]];
