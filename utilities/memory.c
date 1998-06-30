a692a6dcb69c1eb286aa8ee32daa456ff31b8d39
falgout <falgout>
1998-06-30 19:40:05 +0000

    Put commented out "out of memory" code into the memory utilities.

--- utilities/memory.c
+++ utilities/memory.c
@@ -14,12 +14,26 @@
  *****************************************************************************/
 
 #include "memory.h"
+#include <stdio.h>
 
 /******************************************************************************
  *
  * Standard routines
  *
  *****************************************************************************/
+
+/*--------------------------------------------------------------------------
+ * hypre_OutOfMemory
+ *--------------------------------------------------------------------------*/
+
+int
+hypre_OutOfMemory( int size )
+{
+   printf("Out of memory trying to allocate %d bytes\n", size);
+   fflush(stdout);
+
+   return 0;
+}
 
 /*--------------------------------------------------------------------------
  * hypre_MAlloc
@@ -31,9 +45,18 @@ hypre_MAlloc( int size )
    char *ptr;
 
    if (size > 0)
+   {
       ptr = malloc(size);
+
+#if 0
+      if (ptr == NULL)
+        hypre_OutOfMemory(size);
+#endif
+   }
    else
+   {
       ptr = NULL;
+   }
 
    return ptr;
 }
@@ -52,6 +75,11 @@ hypre_CAlloc( int count,
    if (size > 0)
    {
       ptr = calloc(count, elt_size);
+
+#if 0
+      if (ptr == NULL)
+        hypre_OutOfMemory(count * elt_size);
+#endif
    }
    else
    {
@@ -70,6 +98,11 @@ hypre_ReAlloc( char *ptr,
                int   size )
 {
    ptr = realloc(ptr, size);
+
+#if 0
+   if (ptr == NULL)
+     hypre_OutOfMemory(size);
+#endif
 
    return ptr;
 }
