a6963bc32cf57f270a1ed863fff3a9987515bb44
ulrikey <ulrikey>
1998-09-25 15:50:41 +0000

    added par_rap_communication.c

--- parcsr_linear_solvers/protos.h
+++ parcsr_linear_solvers/protos.h
@@ -15,6 +15,13 @@ int BuildCSRJDataType P((int num_nonzeros, double *a_data, int *a_j,
 	MPI_Datatype *csr_jdata_datatype));
 hypre_CSRMatrix *hypre_ExchangeRAPData P((hypre_CSRMatrix *RAP_int,
 	hypre_CommPkg *comm_pkg_RT));
+
+/* par_rap_communication.c */
+int hypre_GetCommPkgRTFromCommPkgA P((hypre_ParCSRMatrix *RT, 
+	hypre_ParCSRMatrix *A, int *partitioning));
+hypre_CommPkg *hypre_GenerateSendMapAndCommPkg P((MPI_Comm comm, 
+	int num_sends, int num_recvs, int *recv_procs, int *send_procs,
+        int *recv_vec_starts, hypre_ParCSRMatrix *A));
 int hypre_GenerateRAPCommPkg P((hypre_ParCSRMatrix *RAP, 
 	hypre_ParCSRMatrix *A, int *partitioning));
 
