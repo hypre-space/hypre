fe43420b02a2abfceec8fd7ce55c79224718d2d8
dwalker <dwalker>
1998-10-06 21:03:30 +0000

    added log file info

--- test/test_drivers.sh
+++ test/test_drivers.sh
@@ -3,5 +3,9 @@
 #===========================================================================
 #   runs test drivers and logs results to file
 #===========================================================================
+
+HYPRE_AUTOTEST_LOG_FILE="autotest.log"
+
 rm -f $HYPRE_AUTOTEST_LOG.FILE
 ./struct_linear_solvers >> $HYPRE_AUTOTEST_LOG_FILE
+
