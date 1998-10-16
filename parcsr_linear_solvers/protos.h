11dbd8803952841e2df3db7d22b2a7a7b6a7c242
ulrikey <ulrikey>
1998-10-16 21:40:34 +0000

    changed parameter sequences of various functions, removed partitioning due to change of hypre_ParCSRMatrix structure

--- parcsr_linear_solvers/protos.h
+++ parcsr_linear_solvers/protos.h
@@ -47,20 +47,20 @@ int hypre_ParAMGSetup P((void *vamg_data , hypre_ParCSRMatrix *A ));
 int hypre_ParAMGBuildInterp P((hypre_ParCSRMatrix *A , int *CF_marker , hypre_ParCSRMatrix *S , hypre_ParCSRMatrix **P_ptr , int **coarse_partitioning_ptr ));
 
 /* par_laplace.c */
-hypre_ParCSRMatrix *hypre_GenerateLaplacian P((MPI_Comm comm , int nx , int ny , int nz , int P , int Q , int R , int p , int q , int r , double *value , int **global_part_ptr ));
+hypre_ParCSRMatrix *hypre_GenerateLaplacian P((MPI_Comm comm , int nx , int ny , int nz , int P , int Q , int R , int p , int q , int r , double *value ));
 int map P((int ix , int iy , int iz , int p , int q , int r , int P , int Q , int R , int *nx_part , int *ny_part , int *nz_part , int *global_part ));
 void qsort0 P((int *v , int left , int right ));
 void swap P((int *v , int i , int j ));
 
 /* par_rap.c */
-int hypre_ParAMGBuildCoarseOperator P((hypre_ParCSRMatrix *RT , hypre_ParCSRMatrix *A , hypre_ParCSRMatrix *P , hypre_ParCSRMatrix **RAP_ptr , int *coarse_partitioning ));
+int hypre_ParAMGBuildCoarseOperator P((hypre_ParCSRMatrix *RT , hypre_ParCSRMatrix *A , hypre_ParCSRMatrix *P , hypre_ParCSRMatrix **RAP_ptr ));
 hypre_CSRMatrix *hypre_GeneratePExt P((hypre_ParCSRMatrix *P , hypre_ParCSRMatrix *A ));
 hypre_CSRMatrix *hypre_ExchangeRAPData P((hypre_CSRMatrix *RAP_int , hypre_CommPkg *comm_pkg_RT ));
 
 /* par_rap_communication.c */
-int hypre_GetCommPkgRTFromCommPkgA P((hypre_ParCSRMatrix *RT , hypre_ParCSRMatrix *A , int *partitioning ));
+int hypre_GetCommPkgRTFromCommPkgA P((hypre_ParCSRMatrix *RT , hypre_ParCSRMatrix *A ));
 hypre_CommPkg *hypre_GenerateSendMapAndCommPkg P((MPI_Comm comm , int num_sends , int num_recvs , int *recv_procs , int *send_procs , int *recv_vec_starts , hypre_ParCSRMatrix *A ));
-int hypre_GenerateRAPCommPkg P((hypre_ParCSRMatrix *RAP , hypre_ParCSRMatrix *A , int *partitioning ));
+int hypre_GenerateRAPCommPkg P((hypre_ParCSRMatrix *RAP , hypre_ParCSRMatrix *A ));
 
 /* random.c */
 void hypre_SeedRand P((int seed ));
