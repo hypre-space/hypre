c916c8c17088754d92728f17eeb60d528ae8e096
ulrikey <ulrikey>
1998-10-16 20:50:46 +0000

    driver to test hypre_ParMatmul

---
+++ parcsr_mv/driver_matmul.c
@@ -0,0 +1,57 @@
+ 
+#include "headers.h"
+ 
+/*--------------------------------------------------------------------------
+ * Test driver for unstructured matrix interface 
+ *--------------------------------------------------------------------------*/
+ 
+int
+main( int   argc,
+      char *argv[] )
+{
+   hypre_ParCSRMatrix     *A;
+   hypre_ParCSRMatrix     *B;
+   hypre_ParCSRMatrix     *C;
+   hypre_CSRMatrix *As;
+   hypre_CSRMatrix *Bs;
+   int *row_starts, *col_starts;
+   int num_procs, my_id;
+
+   /* Initialize MPI */
+   MPI_Init(&argc, &argv);
+
+   MPI_Comm_size(MPI_COMM_WORLD,&num_procs);
+   MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
+   row_starts = NULL;
+   col_starts = NULL;
+
+   if (my_id == 0)
+   {
+   	As = hypre_ReadCSRMatrix("inpr");
+   	printf(" read input A\n");
+   	Bs = hypre_ReadCSRMatrix("input");
+   	printf(" read input B\n");
+   }
+   A = hypre_CSRMatrixToParCSRMatrix(MPI_COMM_WORLD, As, row_starts,
+	col_starts);
+   B = hypre_CSRMatrixToParCSRMatrix(MPI_COMM_WORLD, Bs, col_starts,
+	row_starts);
+   C = hypre_ParMatmul(B,A);
+   hypre_PrintParCSRMatrix(C, "result");
+
+   if (my_id == 0)
+   {
+	hypre_DestroyCSRMatrix(As);
+   	hypre_DestroyCSRMatrix(Bs);
+   }
+   hypre_DestroyParCSRMatrix(A);
+   hypre_DestroyParCSRMatrix(B);
+   hypre_DestroyParCSRMatrix(C);
+   hypre_TFree(row_starts);
+   hypre_TFree(col_starts);
+
+   MPI_Finalize();
+
+   return 0;
+}
+
