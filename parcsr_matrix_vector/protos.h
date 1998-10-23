b137dddf505edb80752d42646ad239034a241a28
ulrikey <ulrikey>
1998-10-23 18:24:35 +0000

    made changes due to change of hypre_ParVector structure

--- parcsr_matrix_vector/protos.h
+++ parcsr_matrix_vector/protos.h
@@ -7,10 +7,13 @@
 
 /* par_vector.c */
 hypre_ParVector *hypre_CreateParVector P((MPI_Comm comm, int global_size, 
-	int first_index, int local_size ));
+	int *partitioning));
 int hypre_DestroyParVector P((hypre_ParVector *vector ));
 int hypre_InitializeParVector P((hypre_ParVector *vector ));
 int hypre_SetParVectorDataOwner P((hypre_ParVector *vector , int owns_data ));
+int hypre_SetParVectorPartitioningOwner P((hypre_ParVector *vector , 
+	int owns_partitioning));
+hypre_ParVector *hypre_ReadParVector P((MPI_Comm comm, char *file_name ));
 int hypre_PrintParVector P((hypre_ParVector *vector, char *file_name ));
 int hypre_SetParVectorConstantValues P((hypre_ParVector *v , double value ));
 int hypre_CopyParVector P((hypre_ParVector *x , hypre_ParVector *y ));
@@ -19,20 +22,15 @@ int hypre_ParAxpy P((double alpha , hypre_ParVector *x , hypre_ParVector *y ));
 double hypre_ParInnerProd P((MPI_Comm comm, hypre_ParVector *x , 
    	hypre_ParVector *y ));
 hypre_ParVector *hypre_VectorToParVector P((MPI_Comm comm, hypre_Vector *v,
-	int **vec_starts_ptr));
+	int *vec_starts));
 hypre_Vector *hypre_ParVectorToVectorAll P((MPI_Comm comm, 
-	hypre_ParVector *par_v, int *vec_starts));
-int hypre_BuildParVectorMPITypes P((MPI_Comm comm, int vec_len, 
-	int *vec_starts, MPI_Datatype *vector_mpi_types));
+	hypre_ParVector *par_v));
 
 /* communication.c */
 hypre_CommHandle *hypre_InitializeCommunication P((int job,
 	hypre_CommPkg *comm_pkg, void *send_data, void *recv_data));
 int hypre_FinalizeCommunication P((hypre_CommHandle *comm_handle));
 int hypre_GenerateMatvecCommunicationInfo P((hypre_ParCSRMatrix *A));
-hypre_VectorCommPkg *hypre_InitializeVectorCommPkg P((MPI_Comm comm, 
-	int vec_len, int *vec_starts));
-int hypre_DestroyVectorCommPkg P(( hypre_VectorCommPkg *vector_comm_pkg));
 int hypre_DestroyMatvecCommPkg P(( hypre_CommPkg *comm_pkg));
 int hypre_BuildCSRMatrixMPIDataType P((int num_nonzeros, int num_rows,
 	double *a_data, int *a_i, int *a_j, MPI_Datatype *csr_matrix_datatype));
