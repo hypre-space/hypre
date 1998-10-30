8f20bfaf9c9fc149fcae8a13c5b9483766c86b9c
jjones <jjones>
1998-10-30 00:51:26 +0000

    Added to tests

--- test/struct_linear_solvers.sh
+++ test/struct_linear_solvers.sh
@@ -8,6 +8,14 @@
 # $Revision$
 #EHEADER***********************************************************************
 
+
+#===========================================================================
+#
+# To do: - test symmetric/non-symmetric in driver.
+#        - add "true" 1d capability. Driver has this - breaks solver.
+#        - answer why 2d results differ (see: NOTE below).
+#===========================================================================
+
 #===========================================================================
 # Define HYPRE_ARCH and MPIRUN
 #===========================================================================
@@ -15,12 +23,113 @@
 . ./autotest_arch.sh
 
 MPIRUN="./mpirun.$HYPRE_ARCH"
+SLS="struct_linear_solvers"
 
 #=============================================================================
-# Run ...
+# Run base 3d case
 #=============================================================================
 
-$MPIRUN -np 1 struct_linear_solvers
+$MPIRUN -np 1 $SLS -n 12 12 12 -c 2.0 3.0 40 
+tail -3 $SLS.log > $SLS.testdata
+
+#=============================================================================
+# Test parallel and blocking by diffing against base 3d case
+#=============================================================================
+
+$MPIRUN -np 8 $SLS -n 6 6 6 -P 2 2 2  -c 2.0 3.0 40
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 4 $SLS -n 3 12 12 -P 4 1 1 -c 2.0 3.0 40
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 4 $SLS -n 12 3 12 -P 1 4 1 -c 2.0 3.0 40
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 4 $SLS -n 12 12 3 -P 1 1 4 -c 2.0 3.0 40
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 1 $SLS -n 3 4 3 -b 4 3 4  -c 2.0 3.0 40
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 8 $SLS -n 3 3 3 -b 2 2 2 -P 2 2 2  -c 2.0 3.0 40
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+#=============================================================================
+# Run base "true" 2d case
+#=============================================================================
+
+$MPIRUN -np 1 $SLS -n 12 12 1  -d 2
+tail -3 $SLS.log > $SLS.testdata
+
+#=============================================================================
+# Test parallel and blocking by diffing against base "true" 2d case.
+#=============================================================================
+
+$MPIRUN -np 3 $SLS -n 4 12 1 -P 3 1 1 -d 2 
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 3 $SLS -n 4 4 1 -P 1 3 1 -b 3 1 1 -d 2 
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+#=============================================================================
+# Test 2d run as 3d by diffing against base "true" 2d case
+# (NOTE:) skip diff for last configuration as results are not the
+# same (why?).
+#=============================================================================
+
+$MPIRUN -np 4 $SLS -n 3 1 12 -P 4 1 1 -c 1 0 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 2 $SLS -n 1 12 6 -P 1 1 2 -c 0 1 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 3 $SLS -n 12 4 1 -P 1 3 1 -c 1 1 0
+#tail -3 $SLS.log > $SLS.testdata.temp
+#diff $SLS.testdata $SLS.testdata.temp >&2
+
+#=============================================================================
+# Test 1d run as 2d and 3d by diffing against each other.
+#=============================================================================
+
+$MPIRUN -np 1 $SLS -n 12 1 1  -c 1 0 0 -d 2
+tail -3 $SLS.log > $SLS.testdata
+
+$MPIRUN -np 2 $SLS -n 2 1 1 -P 2 1 1 -b 3 1 1 -c 1 0 0
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+#=============================================================================
+# Test 1d runs as 2d and 3d in different configurations.
+#=============================================================================
+
+$MPIRUN -np 1 $SLS -n 1 12 1  -c 0 1 0 -d 2
+
+$MPIRUN -np 3 $SLS -n 1 2 1 -P 1 3 1 -b 1 2 1 -c 0 1 0
+
+$MPIRUN -np 4 $SLS -n 1 1 3 -P 1 1 4  -c 0 0 1
+
+#=============================================================================
+# Test SMGCG and DSCG.
+#=============================================================================
+
+$MPIRUN -np 3 $SLS -P 1 3 1 -solver 1
+$MPIRUN -np 3 $SLS -P 3 1 1 -solver 2
+
+#=============================================================================
+# Test V(1,0) SMG.
+#=============================================================================
+
+$MPIRUN -np 3 $SLS -P 1 1 3 -v 1 0
 
 if [ "0" = "1" ]
 then
