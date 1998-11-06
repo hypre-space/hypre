47914c1613f9cfc792a0b7f1e5851ed1e83377f6
elliott <elliott>
1998-11-06 23:33:33 +0000

    11-6-98  Initial committal of threading header and pthreads files          Noah Elliott

---
+++ utilities/threading.c
@@ -0,0 +1,123 @@
+/*BHEADER********************************************************************** 
+ * (c) 1997   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+*********************************************************************EHEADER*/
+#include <malloc.h>
+#include <assert.h>
+#include <stdio.h>
+#include <signal.h>
+#include "mpi.h"
+#include "threading.h"
+
+void ifetchadd(int*,pthread_mutex_t*);
+
+
+int HYPRE_InitPthreads (MPI_Comm comm)
+{
+   int err;
+   int i;
+   hypre_qptr =
+          (hypre_workqueue_t) malloc(sizeof(struct hypre_workqueue_struct) +
+                                      (MAX_QUEUE * sizeof(void *)));
+
+
+   if (hypre_qptr != NULL) {
+      pthread_mutex_init(&hypre_qptr->lock, NULL);
+      pthread_cond_init(&hypre_qptr->work_wait, NULL);
+      pthread_cond_init(&hypre_qptr->finish_wait, NULL);
+      hypre_qptr->n_working = hypre_qptr->n_waiting = hypre_qptr->n_queue = 0;
+      hypre_qptr->inp = hypre_qptr->outp = 0;
+      for (i=0; i < NUM_THREADS; i++) {
+         err=pthread_create(&hypre_thread[i], NULL, 
+                            (void *(*)(void *))hypre_pthread_worker,
+                            (void *)i);
+         assert(err == 0);
+      }
+   }
+
+   pthread_mutex_init(&hypre_mutex_boxloops)
+   phtread_cond_init(&hypre_cond_boxloops)
+
+   return (err);
+}   
+
+void HYPRE_DestroyPthreads (void)
+{
+   int i,x;
+
+   for (i=0; i < NUM_THREADS; i++) {
+      x= pthread_cancel(hypre_thread[i]);
+   }
+
+   pthread_mutex_destroy(&hypre_qptr->lock);
+   pthread_mutex_destroy(&hypre_mutex_boxloops);
+   pthread_cond_destroy(&hypre_qptr->work_wait);
+   pthread_cond_destroy(&hypre_qptr->finish_wait);
+   pthread_cond_destroy(&hypre_cond_boxloops);
+
+   free (hypre_qptr);
+}
+
+void hypre_pthread_worker (int threadid)
+{
+   void *argptr;
+   hypre_work_proc_t funcptr;
+
+   pthread_mutex_lock(&hypre_qptr->lock);
+
+   hypre_qptr->n_working++;
+
+   for(;;) {
+      while (hypre_qptr->n_queue == 0) {
+         if (--hypre_qptr->n_working == 0)
+            pthread_cond_signal(&hypre_qptr->finish_wait);         
+         hypre_qptr->n_waiting++;
+         pthread_cond_wait(&hypre_qptr->work_wait, &hypre_qptr->lock);
+         hypre_qptr->n_waiting--;
+         hypre_qptr->n_working++;
+      }
+      hypre_qptr->n_queue--;
+      funcptr = hypre_qptr->worker_proc_queue[hypre_qptr->outp];
+      argptr = hypre_qptr->argqueue[hypre_qptr->outp];
+      
+      hypre_qptr->outp = (hypre_qptr->outp + 1) % MAX_QUEUE;
+
+      pthread_mutex_unlock(&hypre_qptr->lock);
+
+      (*funcptr)(argptr);
+
+      pthread_mutex_lock(&hypre_qptr->lock);
+   }
+}
+
+void hypre_work_put(hypre_work_proc_t funcptr, void *argptr)
+{
+   pthread_mutex_lock(&hypre_qptr->lock);
+   if (hypre_qptr->n_waiting) {
+      /* idle workers to be awakened */
+      pthread_cond_signal(&hypre_qptr->work_wait);
+   }
+   assert(hypre_qptr->n_queue != MAX_QUEUE);
+
+   hypre_qptr->n_queue++;
+   hypre_qptr->worker_proc_queue[hypre_qptr->inp] = funcptr;
+   hypre_qptr->argqueue[hypre_qptr->inp] = argptr;
+   hypre_qptr->inp = (hypre_qptr->inp + 1) % MAX_QUEUE;
+   pthread_mutex_unlock(&hypre_qptr->lock);
+}
+
+
+/* Wait until all work is done and workers quiesce. */
+void hypre_work_wait(void)
+{       
+   pthread_mutex_lock(&hypre_qptr->lock);
+   while(hypre_qptr->n_queue !=0 || hypre_qptr->n_working != 0)
+      pthread_cond_wait(&hypre_qptr->finish_wait, &hypre_qptr->lock);
+   pthread_mutex_unlock(&hypre_qptr->lock);
+}                               
+
+
---
+++ utilities/threading.h
@@ -0,0 +1,53 @@
+/*BHEADER**********************************************************************
+ * (c) 1997   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+*********************************************************************EHEADER*/
+
+#ifndef hypre_THREADING_HEADER
+#define hypre_THREADING_HEADER
+
+#ifndef NUM_THREADS
+#define NUM_THREADS 4
+#endif
+#ifndef MAX_QUEUE
+#define MAX_QUEUE 256
+#endif
+
+
+#include<pthread.h>
+#include "mpi.h"
+
+/* hypre_work_proc_t typedef'd to be a pointer to a function with a void*
+   argument and a void return type */
+typedef void (*hypre_work_proc_t)(void *);
+
+typedef struct hypre_workqueue_struct {
+   pthread_mutex_t lock;
+   pthread_cond_t work_wait;
+   pthread_cond_t finish_wait;
+   hypre_work_proc_t worker_proc_queue[MAX_QUEUE];
+   int n_working;
+   int n_waiting;
+   int n_queue;
+   int inp;
+   int outp;
+   void *argqueue[MAX_QUEUE];
+} *hypre_workqueue_t;
+
+void hypre_work_put(hypre_work_proc_t funcptr, void *argptr);
+void hypre_work_wait(void);
+int HYPRE_InitPthreads (MPI_Comm comm);
+void HYPRE_DestroyPthreads (void);
+void hypre_pthread_worker (int threadid);
+
+
+pthread_t hypre_thread[NUM_THREADS];
+pthread_cond_t hypre_cond_boxloops;
+pthread_mutex_t hypre_mutex_boxloops;
+hypre_workqueue_t hypre_qptr;
+
+#endif
