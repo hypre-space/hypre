999c1c49520a99bc2672785e9efd5b01a673992e
jjones <jjones>
1999-01-22 22:02:09 +0000

    Added "hypre_Index    periodic" to the grid structure. At the moment I'm thinking periodic=[px,py,pz] means the grid is periodic with the numbers px,py,pz giving the period. Any = 0 means that the grid is not periodic in that direction.

--- struct_matrix_vector/struct_grid.h
+++ struct_matrix_vector/struct_grid.h
@@ -33,6 +33,11 @@ typedef struct
    hypre_BoxNeighbors  *neighbors;    /* neighbors of boxes */
    int                  max_distance;
 
+<<<<<<< struct_grid.h
+   hypre_Index          periodic;     /* indicates if grid is periodic */
+
+
+=======
    /* keep this for now to (hopefully) improve SMG setup performance */
    hypre_BoxArray      *all_boxes;      /* valid only before Assemble */
    int                 *processes;
@@ -42,6 +47,7 @@ typedef struct
    hypre_Index          pstride;        /* project base_all_boxes onto   */
    int                  alloced;        /* boolean used to free up */
 
+>>>>>>> 1.7
 } hypre_StructGrid;
 
 /*--------------------------------------------------------------------------
@@ -55,6 +61,9 @@ typedef struct
 #define hypre_StructGridLocalSize(grid)     ((grid) -> local_size)
 #define hypre_StructGridNeighbors(grid)     ((grid) -> neighbors)
 #define hypre_StructGridMaxDistance(grid)   ((grid) -> max_distance)
+<<<<<<< struct_grid.h
+#define hypre_StructGridPeriodic(grid)      ((grid) -> periodic)
+=======
 #define hypre_StructGridAllBoxes(grid)      ((grid) -> all_boxes)
 #define hypre_StructGridProcesses(grid)     ((grid) -> processes)
 #define hypre_StructGridBoxRanks(grid)      ((grid) -> box_ranks)
@@ -62,6 +71,7 @@ typedef struct
 #define hypre_StructGridPIndex(grid)        ((grid) -> pindex)
 #define hypre_StructGridPStride(grid)       ((grid) -> pstride)
 #define hypre_StructGridAlloced(grid)       ((grid) -> alloced)
+>>>>>>> 1.7
 
 #define hypre_StructGridBox(grid, i) \
 (hypre_BoxArrayBox(hypre_StructGridBoxes(grid), i))
@@ -76,3 +86,4 @@ typedef struct
 hypre_ForBoxI(i, hypre_StructGridBoxes(grid))
 
 #endif
+
