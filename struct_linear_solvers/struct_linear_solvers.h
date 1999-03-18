90c1b2d111543246904df1199bffb628f5ff35e2
jjones <jjones>
1999-03-18 18:52:26 +0000

    Added hybrid solver

--- struct_linear_solvers/struct_linear_solvers.h
+++ struct_linear_solvers/struct_linear_solvers.h
@@ -18,6 +18,23 @@ extern "C" {
 #endif
 
 
+/* HYPRE_struct_hybrid.c */
+int HYPRE_StructHybridInitialize P((MPI_Comm comm , HYPRE_StructSolver *solver ));
+int HYPRE_StructHybridFinalize P((HYPRE_StructSolver solver ));
+int HYPRE_StructHybridSetup P((HYPRE_StructSolver solver , HYPRE_StructMatrix A , HYPRE_StructVector b , HYPRE_StructVector x ));
+int HYPRE_StructHybridSolve P((HYPRE_StructSolver solver , HYPRE_StructMatrix A , HYPRE_StructVector b , HYPRE_StructVector x ));
+int HYPRE_StructHybridSetTol P((HYPRE_StructSolver solver , double tol ));
+int HYPRE_StructHybridSetConvergenceTol P((HYPRE_StructSolver solver , double cf_tol ));
+int HYPRE_StructHybridSetMaxDSIterations P((HYPRE_StructSolver solver , int max_ds_its ));
+int HYPRE_StructHybridSetMaxMGIterations P((HYPRE_StructSolver solver , int max_mg_its ));
+int HYPRE_StructHybridSetTwoNorm P((HYPRE_StructSolver solver , int two_norm ));
+int HYPRE_StructHybridSetRelChange P((HYPRE_StructSolver solver , int rel_change ));
+int HYPRE_StructHybridSetLogging P((HYPRE_StructSolver solver , int logging ));
+int HYPRE_StructHybridGetNumIterations P((HYPRE_StructSolver solver , int *num_its ));
+int HYPRE_StructHybridGetNumDSIterations P((HYPRE_StructSolver solver , int *num_ds_its ));
+int HYPRE_StructHybridGetNumMGIterations P((HYPRE_StructSolver solver , int *num_mg_its ));
+int HYPRE_StructHybridGetFinalRelativeResidualNorm P((HYPRE_StructSolver solver , double *norm ));
+
 /* HYPRE_struct_pcg.c */
 int HYPRE_StructPCGInitialize P((MPI_Comm comm , HYPRE_StructSolver *solver ));
 int HYPRE_StructPCGFinalize P((HYPRE_StructSolver solver ));
@@ -62,6 +79,23 @@ int hypre_CyclicReductionFinalize P((void *cyc_red_vdata ));
 /* general.c */
 int hypre_Log2 P((int p ));
 
+/* hybrid.c */
+void *hypre_HybridInitialize P((MPI_Comm comm ));
+int hypre_HybridFinalize P((void *hybrid_vdata ));
+int hypre_HybridSetTol P((void *hybrid_vdata , double tol ));
+int hypre_HybridSetConvergenceTol P((void *hybrid_vdata , double cf_tol ));
+int hypre_HybridSetMaxDSIterations P((void *hybrid_vdata , int max_ds_its ));
+int hypre_HybridSetMaxMGIterations P((void *hybrid_vdata , int max_mg_its ));
+int hypre_HybridSetTwoNorm P((void *hybrid_vdata , int two_norm ));
+int hypre_HybridSetRelChange P((void *hybrid_vdata , int rel_change ));
+int hypre_HybridSetLogging P((void *hybrid_vdata , int logging ));
+int hypre_HybridGetNumIterations P((void *hybrid_vdata , int *num_its ));
+int hypre_HybridGetNumDSIterations P((void *hybrid_vdata , int *num_ds_its ));
+int hypre_HybridGetNumMGIterations P((void *hybrid_vdata , int *num_mg_its ));
+int hypre_HybridGetFinalRelativeResidualNorm P((void *hybrid_vdata , double *final_rel_res_norm ));
+int hypre_HybridSetup P((void *hybrid_vdata , hypre_StructMatrix *A , hypre_StructVector *b , hypre_StructVector *x ));
+int hypre_HybridSolve P((void *hybrid_vdata , hypre_StructMatrix *A , hypre_StructVector *b , hypre_StructVector *x ));
+
 /* pcg.c */
 int hypre_PCGIdentitySetup P((void *vdata , void *A , void *b , void *x ));
 int hypre_PCGIdentity P((void *vdata , void *A , void *b , void *x ));
@@ -70,6 +104,7 @@ int hypre_PCGFinalize P((void *pcg_vdata ));
 int hypre_PCGSetup P((void *pcg_vdata , void *A , void *b , void *x ));
 int hypre_PCGSolve P((void *pcg_vdata , void *A , void *b , void *x ));
 int hypre_PCGSetTol P((void *pcg_vdata , double tol ));
+int hypre_PCGSetConvergenceFactorTol P((void *pcg_vdata , double cf_tol ));
 int hypre_PCGSetMaxIter P((void *pcg_vdata , int max_iter ));
 int hypre_PCGSetTwoNorm P((void *pcg_vdata , int two_norm ));
 int hypre_PCGSetRelChange P((void *pcg_vdata , int rel_change ));
@@ -114,11 +149,15 @@ int hypre_SMGSetStructVectorConstantValues P((hypre_StructVector *vector , doubl
 hypre_StructMatrix *hypre_SMG2NewRAPOp P((hypre_StructMatrix *R , hypre_StructMatrix *A , hypre_StructMatrix *PT ));
 int hypre_SMG2BuildRAPSym P((hypre_StructMatrix *A , hypre_StructMatrix *PT , hypre_StructMatrix *R , hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
 int hypre_SMG2BuildRAPNoSym P((hypre_StructMatrix *A , hypre_StructMatrix *PT , hypre_StructMatrix *R , hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
+int hypre_SMG2RAPPeriodicSym P((hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
+int hypre_SMG2RAPPeriodicNoSym P((hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
 
 /* smg3_setup_rap.c */
 hypre_StructMatrix *hypre_SMG3NewRAPOp P((hypre_StructMatrix *R , hypre_StructMatrix *A , hypre_StructMatrix *PT ));
 int hypre_SMG3BuildRAPSym P((hypre_StructMatrix *A , hypre_StructMatrix *PT , hypre_StructMatrix *R , hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
 int hypre_SMG3BuildRAPNoSym P((hypre_StructMatrix *A , hypre_StructMatrix *PT , hypre_StructMatrix *R , hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
+int hypre_SMG3RAPPeriodicSym P((hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
+int hypre_SMG3RAPPeriodicNoSym P((hypre_StructMatrix *RAP , hypre_Index cindex , hypre_Index cstride ));
 
 /* smg_intadd.c */
 void *hypre_SMGIntAddInitialize P((void ));
