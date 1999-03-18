66f7bc9c909bdece262c1c1c6e14e12b2b555f18
vhenson <vhenson>
1999-03-18 06:52:23 +0000

    Initial entry.  headers for AMG coarsening linked list routines

---
+++ utilities/amg_linklist.h
@@ -0,0 +1,43 @@
+/*BHEADER**********************************************************************
+ * (c) 1997   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+
+/******************************************************************************
+ *
+ * Header file link lists
+ *
+ *****************************************************************************/
+
+#ifndef HYPRE_LINKLIST_HEADER
+#define HYPRE_LINKLIST_HEADER
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LIST_HEAD -1
+#define LIST_TAIL -2
+
+struct double_linked_list
+{
+       int                        data;
+       struct double_linked_list *next_elt;
+       struct double_linked_list *prev_elt;
+       int                        head;
+       int                        tail;
+};
+
+typedef struct double_linked_list hypre_ListElement;
+typedef hypre_ListElement  *hypre_LinkList;  
+
+
+#endif
