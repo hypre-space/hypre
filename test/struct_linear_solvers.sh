d3c79c3b089dc7f486b7ca487cfaecb60ea5d5ed
falgout <falgout>
1999-03-19 09:14:19 +0000

    Uncommented all tests. Uncommented an SMG test that used to fail and now works.

--- test/struct_linear_solvers.sh
+++ test/struct_linear_solvers.sh
@@ -83,8 +83,6 @@ diff $SLS.testdata $SLS.testdata.temp >&2
 
 #=============================================================================
 # SMG: Test 2d run as 3d by diffing against base "true" 2d case
-# (NOTE:) skip diff for last configuration as results are not the
-# same (why?).
 #=============================================================================
 
 $MPIRUN -np 4 $SLS -n 3 1 12 -P 4 1 1 -c 1 0 1 -solver 0
@@ -96,8 +94,8 @@ tail -3 $SLS.log > $SLS.testdata.temp
 diff $SLS.testdata $SLS.testdata.temp >&2
 
 $MPIRUN -np 3 $SLS -n 12 4 1 -P 1 3 1 -c 1 1 0 -solver 0
-#tail -3 $SLS.log > $SLS.testdata.temp
-#diff $SLS.testdata $SLS.testdata.temp >&2
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
 
 rm -f $SLS.testdata $SLS.testdata.temp
 
@@ -176,63 +174,61 @@ rm -f $SLS.testdata $SLS.testdata.temp
 # PFMG: Run base "true" 2d case
 #=============================================================================
 
-#$MPIRUN -np 1 $SLS -n 12 12 1  -d 2 -solver 1
-#tail -3 $SLS.log > $SLS.testdata
+$MPIRUN -np 1 $SLS -n 12 12 1  -d 2 -solver 1
+tail -3 $SLS.log > $SLS.testdata
 
 #=============================================================================
 # PFMG: Test parallel and blocking by diffing against base "true" 2d case.
 #=============================================================================
 
-#$MPIRUN -np 3 $SLS -n 4 12 1 -P 3 1 1 -d 2 -solver 1
-#tail -3 $SLS.log > $SLS.testdata.temp
-#diff $SLS.testdata $SLS.testdata.temp >&2
-#
-#$MPIRUN -np 3 $SLS -n 4 4 1 -P 1 3 1 -b 3 1 1 -d 2 -solver 1
-#tail -3 $SLS.log > $SLS.testdata.temp
-#diff $SLS.testdata $SLS.testdata.temp >&2
+$MPIRUN -np 3 $SLS -n 4 12 1 -P 3 1 1 -d 2 -solver 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 3 $SLS -n 4 4 1 -P 1 3 1 -b 3 1 1 -d 2 -solver 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
 
 #=============================================================================
 # PFMG: Test 2d run as 3d by diffing against base "true" 2d case
-# (NOTE:) skip diff for last configuration as results are not the
-# same (why?).
 #=============================================================================
 
-#$MPIRUN -np 4 $SLS -n 3 1 12 -P 4 1 1 -c 1 0 1 -solver 1
-#tail -3 $SLS.log > $SLS.testdata.temp
-#diff $SLS.testdata $SLS.testdata.temp >&2
-#
-#$MPIRUN -np 2 $SLS -n 1 12 6 -P 1 1 2 -c 0 1 1 -solver 1
-#tail -3 $SLS.log > $SLS.testdata.temp
-#diff $SLS.testdata $SLS.testdata.temp >&2
-#
-#$MPIRUN -np 3 $SLS -n 12 4 1 -P 1 3 1 -c 1 1 0 -solver 1
-##tail -3 $SLS.log > $SLS.testdata.temp
-##diff $SLS.testdata $SLS.testdata.temp >&2
-#
-#rm -f $SLS.testdata $SLS.testdata.temp
+$MPIRUN -np 4 $SLS -n 3 1 12 -P 4 1 1 -c 1 0 1 -solver 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 2 $SLS -n 1 12 6 -P 1 1 2 -c 0 1 1 -solver 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+$MPIRUN -np 3 $SLS -n 12 4 1 -P 1 3 1 -c 1 1 0 -solver 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+rm -f $SLS.testdata $SLS.testdata.temp
 
 #=============================================================================
 # PFMG: Test 1d run as 2d and 3d by diffing against each other.
 #=============================================================================
 
-#$MPIRUN -np 1 $SLS -n 12 1 1  -c 1 0 0 -d 2 -solver 1
-#tail -3 $SLS.log > $SLS.testdata
-#
-#$MPIRUN -np 2 $SLS -n 2 1 1 -P 2 1 1 -b 3 1 1 -c 1 0 0 -solver 1
-#tail -3 $SLS.log > $SLS.testdata.temp
-#diff $SLS.testdata $SLS.testdata.temp >&2
-#
-#rm -f $SLS.testdata $SLS.testdata.temp
+$MPIRUN -np 1 $SLS -n 12 1 1  -c 1 0 0 -d 2 -solver 1
+tail -3 $SLS.log > $SLS.testdata
+
+$MPIRUN -np 2 $SLS -n 2 1 1 -P 2 1 1 -b 3 1 1 -c 1 0 0 -solver 1
+tail -3 $SLS.log > $SLS.testdata.temp
+diff $SLS.testdata $SLS.testdata.temp >&2
+
+rm -f $SLS.testdata $SLS.testdata.temp
 
 #=============================================================================
 # PFMG: Test 1d runs as 2d and 3d in different configurations.
 #=============================================================================
 
-#$MPIRUN -np 1 $SLS -n 1 12 1  -c 0 1 0 -d 2 -solver 1
-#
-#$MPIRUN -np 3 $SLS -n 1 2 1 -P 1 3 1 -b 1 2 1 -c 0 1 0 -solver 1
-#
-#$MPIRUN -np 4 $SLS -n 1 1 3 -P 1 1 4  -c 0 0 1 -solver 1
+$MPIRUN -np 1 $SLS -n 1 12 1  -c 0 1 0 -d 2 -solver 1
+
+$MPIRUN -np 3 $SLS -n 1 2 1 -P 1 3 1 -b 1 2 1 -c 0 1 0 -solver 1
+
+$MPIRUN -np 4 $SLS -n 1 1 3 -P 1 1 4  -c 0 0 1 -solver 1
 
 #=============================================================================
 # PFMG: Test solve of the same problem in different orientations
