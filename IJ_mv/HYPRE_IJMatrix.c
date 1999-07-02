224fb1b55c65105b8c556f0256894d09d11f74f1
cleary <cleary>
1999-07-02 22:50:44 +0000

    First versions of these routines with the HYPRE_IJMatrix outer level implementation.

---
+++ IJ_mv/HYPRE_IJMatrix.c
@@ -0,0 +1,515 @@
+/*BHEADER**********************************************************************
+ * (c) 1999   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+/******************************************************************************
+ *
+ * HYPRE_IJMatrix interface
+ *
+ *****************************************************************************/
+
+#include "./IJ_matrix.h"
+
+/*--------------------------------------------------------------------------
+ * HYPRE_NewIJMatrix
+ *--------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------- */
+/** 
+"IJ" represents the "linear algebraic" conceptual view of a matrix. 
+The "I" and "J" in the name
+are meant to be reminiscent of traditional matrix notation like A(I,J).
+
+Collective.
+
+{\bf Note:} Must be the first function called using "matrix" as an actual argument.
+@return integer error code
+@param HYPRE_IJMatrix &IJmatrix: the matrix to be initialized.
+@param MPI_Comm comm: a single MPI_Communicator that contains exactly the MPI processes that are to
+participate in any collective operations.
+@param int global_m, global_n: the dimensions of the entire, global matrix.
+@param int local_m, local_n: the dimensions of the locally stored matrix.
+*/
+/*---------------------------------------------------------------------- */
+
+int HYPRE_NewIJMatrix( HYPRE_IJMatrix &in_matrix_ptr, MPI_Comm comm, 
+          int global_m, int global_n, int local_m, int local_n )
+
+{
+   int ierr=0;
+
+   hypre_IJMatrix    *matrix;
+
+   matrix = hypre_CTAlloc(hypre_IJMatrix, 1);
+
+   hypre_IJMatrixContext(matrix) = context;
+   hypre_IJMatrixM(matrix)    = -1;
+   hypre_IJMatrixN(matrix)    = -1;
+   hypre_IJMatrixLocalStorage(matrix) = NULL;
+   hypre_IJMatrixTranslator(matrix) = NULL;
+   hypre_IJMatrixLocalStorageType(matrix) = HYPRE_UNITIALIZED;
+   hypre_IJMatrixInsertionSemantics = 0;
+   hypre_IJMatrixReferenceCount = 1;
+
+   *in_matrix_ptr = (HYPRE_IJMatrix) matrix;
+  
+   return( ierr ); 
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_FreeIJMatrix
+ *--------------------------------------------------------------------------*/
+
+int 
+HYPRE_FreeIJMatrix( HYPRE_IJMatrix IJmatrix )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if (matrix)
+   {
+      hypre_IJMatrixReferenceCount( matrix ) --;
+   
+      if ( hypre_IJMatrixReferenceCount( matrix ) <= 0 )
+      {
+         if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+            ierr = hypre_FreeIJMatrixPETSc( matrix );
+         else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+            ierr = hypre_FreeIJMatrixISIS( matrix );
+         else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+            ierr = hypre_FreeIJMatrixParcsr( matrix );
+         else
+            ierr = -1;
+
+         hypre_TFree(matrix);
+      }
+   }
+   else
+   {
+      ierr = -1;
+   }
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_InitializeIJMatrix
+ *--------------------------------------------------------------------------*/
+
+int 
+HYPRE_InitializeIJMatrix( HYPRE_IJMatrix IJmatrix )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_InitializeIJMatrixPETSc( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_InitializeIJMatrixISIS( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_InitializeIJMatrixParcsr( matrix );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_AssembleIJMatrix
+ *--------------------------------------------------------------------------*/
+
+int 
+HYPRE_AssembleIJMatrix( HYPRE_IJMatrix IJmatrix )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if( (hypre_IJMatrixM(matrix) < 0 ) ||
+       (hypre_IJMatrixN(matrix) < 0 ) )
+      return(-1);
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_AssembleIJMatrixPETSc( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_AssembleIJMatrixISIS( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_AssembleIJMatrixParcsr( matrix );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_SetIJMatrixLocalStorageType
+ *--------------------------------------------------------------------------*/
+
+/**
+Tells "matrix" which underlying "storage type" it should build from the IJMatrix interface
+calls. Conceptually, "IJMatrix" is just an "interface" (an abstract base class in C++-speak),
+with no implementation. Specific concrete classes implement this interface. One way for a user
+to choose which concrete class to use is by *instantiating* the class of choice (in an OO
+implementation) or by *linking* in the implementation of choice (a "compile time" choice). An
+intermediate method is through the use of HYPRE_SetIJMatrixStorageType that *may* be provided
+by specific implementations. For example, it is possible to build either a PETSc MPIAIJ matrix
+or a ParCSR matrix from the IJ matrix interface, and this choice can be controlled through this
+function.
+
+Not collective, but must be same on all processors in group that stores matrix.
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+ the matrix to be operated on. 
+@param int type [IN}
+possible types should be documented with each implementation. The first HYPRE implementation
+will list its possible values in HYPRE.h (at least for now). Note that this function is optional,
+as all implementations must have a default. Eventually the plan is to let solvers choose a storage
+type that they can work with automatically, but this is in the future.
+*/
+
+int 
+HYPRE_SetIJMatrixLocalStorageType( HYPRE_IJMatrix IJmatrix, int type )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   hypre_IJMatrixLocalStorage(matrix) = local_storage;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_SetIJMatrixDiagRowSizes
+ *--------------------------------------------------------------------------*/
+
+/**
+* HYPRE_SetIJMatrixDiagRowSizes( HYPRE_IJMatrix IJmatrix, int *sizes); 
+
+Not collective.
+Tells "matrix" how many nonzeros to expect in each row corresponding to other rows also
+on this processor, also known as the "diagonal block" corresponding to this processor. Knowing
+this quantity apriori may have a significant impact on the time needed for the Assemble phase,
+and this option should always be utilized if the information is available.It is most useful in conjunction
+with the next function.
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN] 
+the matrix to be operated on.
+@param int *sizes [IN]
+a vector of length = local_m giving the estimated sizes for the diagonal parts of
+all local_m rows, in order from lowest globally numbered local row to highest.
+*/
+
+int 
+HYPRE_SetIJMatrixDiagRowSizes( HYPRE_IJMatrix IJmatrix, int *sizes )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_SetIJMatrixDiagRowSizesPETSc( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_SetIJMatrixDiagRowSizesISIS( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_SetIJMatrixDiagRowSizesParcsr( matrix );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+
+/*--------------------------------------------------------------------------
+ * HYPRE_SetIJMatrixOffDiagRowSizes
+ *--------------------------------------------------------------------------*/
+
+/**
+* HYPRE_SetIJMatrixOffDiagRowSizes( HYPRE_IJMatrix matrix, int *sizes);
+
+Tells "matrix" how many nonzeros to expect in each corresponding to rows NOT
+on this processor, i.e. the off-diagonal block corresponding to this processor. As above, this option
+should always be utilized if the information is available, as the setting of coefficients and assemble
+operations can be very expensive without them.
+
+Not collective.
+
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+the matrix to be operated on
+@param int *sizes [IN]
+a vector of length = local_m giving the estimated sizes for the off-diagonal parts of
+all local_m rows, in order from lowest globally numbered local row to highest
+*/
+
+int 
+HYPRE_SetIJMatrixOffDiagRowSizes( HYPRE_IJMatrix IJmatrix, int *sizes )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_SetIJMatrixOffDiagRowSizesPETSc( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_SetIJMatrixOffDiagRowSizesISIS( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_SetIJMatrixOffDiagRowSizesParcsr( matrix );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_SetIJMatrixTotalSize
+ *--------------------------------------------------------------------------*/
+
+/** 
+Tells "matrix" how many nonzeros to expect in each row. This option is preferable to using NO
+"SetSizes" commands but it may lead to less efficient Assemble calls than using the two functions
+above.
+
+Not collective.
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+the matrix to be initialized.
+@param int size [IN]
+total number of coefficients expected on this processor.
+
+{\bf note} All sizes given in the SetIJMatrixSize routines do not have to be exact, that is, it is not
+an error for them to be incorrect. Incorrect values may degrade performance, however.
+*/
+
+int 
+HYPRE_SetIJMatrixTotalSize( HYPRE_IJMatrix IJmatrix, int size )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_SetIJMatrixTotalSizePETSc( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_SetIJMatrixTotalSizeISIS( matrix );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_SetIJMatrixTotalSizeParcsr( matrix );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_QueryIJMatrixInsertionSemantics
+ *--------------------------------------------------------------------------*/
+
+/** 
+{\bf Note:} It is probably best to ignore  HYPRE_QueryIJMatrixInsertionSemantics for the time being as it is really
+planned as a possible future contingency and is confusing as it is now. -AJC, 6/99
+
+There are three possible semantic "levels" in the integer parameter "row" used in Set and Add
+functions, and each implementation supports one of them. This function returns a value indicating
+the semantics supported by the instantiated IJMatrix. 
+level = -1: processors may include values for "row" for any row number in the global matrix.
+level = 0: processors may only include values for row representing locally stored rows.
+level = > 0: in addition to the above, processors may also include values for "row" representing
+rows within the set of locally stored rows plus the next "level" levels of nearest neighbors, 
+also known as "level sets" or "nearest neighbors".
+Since level 0 is the most restrictive, it is also the easiest to implement, and the safest to use, as
+all IJ matrices MUST support level 0.
+In contrast, level 1 is the most general and most difficult to implement, least safe to use, and
+potentially least efficient.
+Levels greater than 0 represent a compromise that is appropriate for many engineering applications,
+like finite element applications, where contributions to a particular matrix row may be made from
+more than one processor, typically in some small neighborhood around that row. 
+
+Not collective.
+
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+the matrix to be initialized.
+@param int *level [OUT]
+level of off-processor value-setting that this implementation supports
+*/
+
+int 
+HYPRE_QueryIJMatrixInsertionSemantics( HYPRE_IJMatrix IJmatrix, int *level )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   *level = hypre_IJMatrixInsertionSemantics(matrix);
+
+   return(ierr);
+
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_InsertIJMatrixBlock
+ *--------------------------------------------------------------------------*/
+
+/** 
+Inserts a block of coefficients into an IJMatrix, overwriting any coefficients
+in the event of a collision.
+
+Not collective.
+
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+the matrix to be operated on.
+@param int m, n [IN]
+the size of the block of values to be added.
+@param int *rows [IN]
+an integer vector of length m giving the indices in the global matrix
+corresponding to the rows in "values".
+@param int *cols [IN]
+an integer vector of length n giving the indices in the global matrix
+corresponding to the columns in "values".
+@param double *values {IN]
+The values to be inserted into the matrix, stored in a dense, row-major
+block of size m X n.
+
+*/
+
+int 
+HYPRE_InsertIJMatrixBlock( HYPRE_IJMatrix IJmatrix, int m, int n,
+                           int *rows, int *cols, double *values)
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_InsertIJMatrixBlockPETSc( matrix, m, n, rows, cols, values );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_InsertIJMatrixBlockISIS( matrix, m, n, rows, cols, values );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_InsertIJMatrixBlockParcsr( matrix, m, n, rows, cols, values );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_AddBlockToIJMatrix
+ *--------------------------------------------------------------------------*/
+
+/** 
+Modifies the values stored in matrix by adding in a block.
+If there is no value already in a particular matrix position, the structure is augmented
+with a new entry. In the event of a collision, the corresponding values are summed.
+
+Not collective.
+
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+the matrix to be operated on.
+@param int m, n [IN]
+the size of the block of values to be added.
+@param int *rows [IN]
+an integer vector of length m giving the indices in the global matrix
+corresponding to the rows in "values".
+@param int *cols [IN]
+an integer vector of length n giving the indices in the global matrix
+corresponding to the columns in "values".
+@param double *values {IN]
+The values to be inserted into the matrix, stored in a dense, row-major
+block of size m X n.
+
+*/
+
+int 
+HYPRE_AddBlockToIJMatrix( HYPRE_IJMatrix IJmatrix, int m, int n,
+                           int *rows, int *cols, double *values)
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_AddBlockToIJMatrixPETSc( matrix, m, n, rows, cols, values );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_AddBlockToIJMatrixISIS( matrix, m, n, rows, cols, values );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_AddBlockToIJMatrixParcsr( matrix, m, n, rows, cols, values );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*--------------------------------------------------------------------------
+ * HYPRE_InsertIJMatrixRow
+ *--------------------------------------------------------------------------*/
+
+/** 
+Inserts a row into the matrix. This is generally a high-speed but inflexible method to build the
+matrix. This call replaces any previously existing row structure with the structure represented by
+indices and coeffs.
+
+Not collective.
+
+@return integer error code
+@param HYPRE_IJMatrix &matrix [IN]
+the matrix to be operated on.
+@param int m, n [IN]
+the size of the block of values to be added.
+@param int *rows [IN]
+an integer vector of length m giving the indices in the global matrix
+corresponding to the rows in "values".
+@param int *cols [IN]
+an integer vector of length n giving the indices in the global matrix
+corresponding to the columns in "values".
+@param double *values {IN]
+The values to be inserted into the matrix.
+*/
+
+int 
+HYPRE_InsertIJMatrixRow( HYPRE_IJMatrix IJmatrix, int n,
+                           int row, int *cols, double *values)
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PETSC_MATRIX )
+      ierr = hypre_InsertIJMatrixRowPETSc( matrix, n, row, cols, values );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_ISIS_MATRIX )
+      ierr = hypre_InsertIJMatrixRowISIS( matrix, n, row, cols, values );
+   else if ( hypre_IJMatrixLocalStorageType(matrix) == HYPRE_PARCSR_MATRIX )
+      ierr = hypre_InsertIJMatrixRowParcsr( matrix, n, row, cols, values );
+   else
+      ierr = -1;
+
+   return(ierr);
+}
+
+/*********************************************************************************/
+/* The following are routines that are not generally used by or supported for users */
+
+
+/*--------------------------------------------------------------------------
+ * hypre_RefIJMatrix
+ *--------------------------------------------------------------------------*/
+
+/**
+Sets a reference to point to an IJMatrix.
+
+@return integer error code
+@param IJMatrix [IN]
+The matrix to be pointed to.
+@param reference [OUT]
+The pointer to be set to point to IJMatrix.
+*/
+
+int 
+hypre_RefIJMatrix( HYPRE_IJMatrix IJmatrix, HYPRE_IJMatrix *reference )
+{
+   int ierr = 0;
+   hypre_IJMatrix *matrix = (hypre_IJMatrix *) IJmatrix;
+
+   hypre_IJMatrixReferenceCount(matrix) ++;
+
+   *reference = IJMatrix;
+
+   return(ierr);
+}
+
---
+++ IJ_mv/IJ_matrix.h
@@ -0,0 +1,69 @@
+/*BHEADER**********************************************************************
+ * (c) 1999   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+/******************************************************************************
+ *
+ * Header info for the hypre_IJMatrix structures
+ *
+ *****************************************************************************/
+
+#ifndef hypre_IJ_MATRIX_HEADER
+#define hypre_IJ_MATRIX_HEADER
+
+#include "../utilities/general.h"
+#include "../utilities/utilities.h"
+
+#include "../HYPRE.h"
+
+#include "./HYPRE_IJ_matrix_types.h"
+
+/*--------------------------------------------------------------------------
+ * hypre_IJMatrix:
+ *--------------------------------------------------------------------------*/
+
+typedef struct
+{
+   MPI_Comm      context;
+
+   int M, N;                               /* number of rows and cols in matrix */
+
+
+   void         *local_storage;            /* Structure for storing local portion */
+   int      	 local_storage_type;       /* Indicates the type of "local storage" */
+   void         *translator;               /* optional storage_type specfic structure
+                                              for holding additional local info */
+
+   int           insertion_semantics;      /* Flag that indicates for the current
+                                              object to what extent values can be set
+                                              from different processors than the one that
+                                              stores the row. */
+                                           /* 0: minimum definition, values can only be set on-processor. */
+   int           ref_count;                /* reference count for memory management */
+} hypre_IJMatrix;
+
+/*--------------------------------------------------------------------------
+ * Accessor macros: hypre_IJMatrix
+ *--------------------------------------------------------------------------*/
+
+#define hypre_IJMatrixContext(matrix)              ((matrix) -> context)
+#define hypre_IJMatrixM(matrix)                    ((matrix) -> M)
+#define hypre_IJMatrixN(matrix)                    ((matrix) -> N)
+
+#define hypre_IJMatrixLocalStorageType(matrix)     ((matrix) -> local_storage_type)
+#define hypre_IJMatrixTranslator(matrix)           ((matrix) -> translator)
+#define hypre_IJMatrixLocalStorage(matrix)         ((matrix) -> local_storage)
+
+#define hypre_IJMatrixInsertionSemantics(matrix)   ((matrix) -> insertion_semantics)
+#define hypre_IJMatrixReferenceCount(matrix)       ((matrix) -> ref_count)
+
+/*--------------------------------------------------------------------------
+ * prototypes for operations on local objects
+ *--------------------------------------------------------------------------*/
+#include "./internal_protos.h"
+
+#endif
