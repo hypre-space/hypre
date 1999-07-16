a76091d7d0d000c1484944c068f70c3e56121a74
ulrikey <ulrikey>
1999-07-16 23:09:40 +0000

    *** empty log message ***

---
+++ IJ_matrix_vector/headers.h
@@ -0,0 +1,18 @@
+
+/*BHEADER**********************************************************************
+ * (c) 1998   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "HYPRE_IJMatrix.h"
+#include "IJ_matrix_vector.h"
+
+
