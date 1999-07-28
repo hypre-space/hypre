a019f0b1fe9948ee80b98762263c102a302632c7
cleary <cleary>
1999-07-28 18:17:09 +0000

    Prototype file now includes type definitions that are used.

--- matrix_matrix/HYPRE_matrix_matrix_protos.h
+++ matrix_matrix/HYPRE_matrix_matrix_protos.h
@@ -1,5 +1,7 @@
 # define	P(s) s
 
+#include "../distributed_matrix/HYPRE_distributed_matrix_types.h"
+
 #ifdef PETSC_AVAILABLE
 /* HYPRE_ConvertPETScMatrixToDistributedMatrix.c */
 int HYPRE_ConvertPETScMatrixToDistributedMatrix P((Mat PETSc_matrix , HYPRE_DistributedMatrix *DistributedMatrix ));
@@ -8,6 +10,7 @@ int HYPRE_ConvertPETScMatrixToDistributedMatrix P((Mat PETSc_matrix , HYPRE_Dist
 /* HYPRE_ConvertParCSRMatrixToDistributedMatrix.c */
 int HYPRE_ConvertParCSRMatrixToDistributedMatrix P((HYPRE_ParCSRMatrix parcsr_matrix , HYPRE_DistributedMatrix *DistributedMatrix ));
 
+#include "../IJ_matrix_vector/HYPRE_IJMatrix.h
 /* HYPRE_BuildIJMatrixFromDistributedMatrix.c */
 int HYPRE_BuildIJMatrixFromDistributedMatrix P((HYPRE_DistributedMatrix DistributedMatrix, HYPRE_IJMatrix *ij_matrix ));
 #undef P
