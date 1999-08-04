2f10598c70780ba2a76b295cf2f43b3804da7e55
falgout <falgout>
1999-08-04 06:47:23 +0000

    Fixed private variable definitions.

--- struct_mv/hypre_box_smp_forloop.h
+++ struct_mv/hypre_box_smp_forloop.h
@@ -8,10 +8,12 @@
  *********************************************************************EHEADER*/
 
 #if HYPRE_USING_PGCC_SMP
-#define HYPRE_SMP_PRIVATE HYPRE_BOX_SMP_PRIVATE,hypre_block
+#define HYPRE_SMP_PRIVATE \
+HYPRE_BOX_SMP_PRIVATE,hypre__nx,hypre__ny,hypre__nz,hypre__block
 #include "../utilities/hypre_smp_forloop.h"
 #else
-#define HYPRE_SMP_PRIVATE HYPRE_BOX_SMP_PRIVATE
+#define HYPRE_SMP_PRIVATE \
+HYPRE_BOX_SMP_PRIVATE,hypre__nx,hypre__ny,hypre__nz
 #include "../utilities/hypre_smp_forloop.h"
 #endif
 #undef HYPRE_BOX_SMP_PRIVATE
