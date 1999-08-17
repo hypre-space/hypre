58b5f66ab76112304ff357a22c800380fb001eba
lambda <lambda>
1999-08-17 02:34:54 +0000

    Contents of fortran.h now in parcsr_matrix_vector.h

--- parcsr_matrix_vector/parcsr_matrix_vector.h
+++ parcsr_matrix_vector/parcsr_matrix_vector.h
@@ -98,6 +98,35 @@ typedef struct
 #define hypre_CommHandleRequest(comm_handle, i)  (comm_handle -> requests[i])
 
 /*BHEADER**********************************************************************
+ * (c) 1996   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+
+/******************************************************************************
+ *
+ * Fortran <-> C interface macros
+ *
+ *****************************************************************************/
+
+#ifndef HYPRE_FORTRAN_HEADER
+#define HYPRE_FORTRAN_HEADER
+
+#if defined(IRIX) || defined(DEC)
+#define hypre_NAME_C_FOR_FORTRAN(name) name##_
+#define hypre_NAME_FORTRAN_FOR_C(name) name##_
+#else
+#define hypre_NAME_C_FOR_FORTRAN(name) name##__
+#define hypre_NAME_FORTRAN_FOR_C(name) name##_
+#endif
+
+#define hypre_F90_IFACE(iface_name) hypre_NAME_FORTRAN_FOR_C(iface_name)
+
+#endif
+/*BHEADER**********************************************************************
  * (c) 1998   The Regents of the University of California
  *
  * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
