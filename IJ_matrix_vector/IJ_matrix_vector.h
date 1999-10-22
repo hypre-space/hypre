54eaaee0ad4ac8a86cbfd8bee60a2e31c7a6bdc9
lambda <lambda>
1999-10-22 22:40:08 +0000

    Removed the fortran.h cat from headers, and ran the script to generate a new IJ_matrix_vector.h

--- IJ_matrix_vector/IJ_matrix_vector.h
+++ IJ_matrix_vector/IJ_matrix_vector.h
@@ -24,35 +24,6 @@ extern "C" {
 
 /******************************************************************************
  *
- * Fortran <-> C interface macros
- *
- *****************************************************************************/
-
-#ifndef HYPRE_FORTRAN_HEADER
-#define HYPRE_FORTRAN_HEADER
-
-#if defined(IRIX) || defined(DEC)
-#define hypre_NAME_C_FOR_FORTRAN(name) name##_
-#define hypre_NAME_FORTRAN_FOR_C(name) name##_
-#else
-#define hypre_NAME_C_FOR_FORTRAN(name) name##__
-#define hypre_NAME_FORTRAN_FOR_C(name) name##_
-#endif
-
-#define hypre_F90_IFACE(iface_name) hypre_NAME_FORTRAN_FOR_C(iface_name)
-
-#endif
-/*BHEADER**********************************************************************
- * (c) 1996   The Regents of the University of California
- *
- * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
- * notice, contact person, and disclaimer.
- *
- * $Revision$
- *********************************************************************EHEADER*/
-
-/******************************************************************************
- *
  * Header info for Auxiliary Parallel CSR Matrix data structures
  *
  * Note: this matrix currently uses 0-based indexing.
