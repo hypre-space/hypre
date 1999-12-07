b8e87604198e50b67a61449676c41e96dd19383e
lambda <lambda>
1999-12-07 22:45:14 +0000

    Fixed underscoring cases for Blue, added definitions of hypre_F90_NAME and hypre_BLAS_NAME

--- utilities/fortran.h
+++ utilities/fortran.h
@@ -17,17 +17,29 @@
 #define HYPRE_FORT_HEADER
 
 #if defined(HYPRE_SOLARIS) || defined(HYPRE_ALPHA)
-#define hypre_NAME_C_CALLING_FORT(name) name##_
-#define hypre_NAME_FORT_CALLING_C(name) name##_
+
+#  define hypre_NAME_C_CALLING_FORT(name) name##_
+#  define hypre_NAME_FORT_CALLING_C(name) name##_
+
 #elif defined(HYPRE_RS6000)
-#define hypre_NAME_C_CALLING_FORT(name) name
-#define hypre_NAME_FORT_CALLING_C(name) name
+
+#  define hypre_NAME_C_CALLING_FORT(name) name
+#  define hypre_NAME_FORT_CALLING_C(name) name
+
 #else
-#define hypre_NAME_C_CALLING_FORT(name) name##_
-#define hypre_NAME_FORT_CALLING_C(name) name##_
+
+#  define hypre_NAME_C_CALLING_FORT(name) name##_
+#  define hypre_NAME_FORT_CALLING_C(name) name##_
+
+#endif
+
+#if defined(HYPRE_BLAS)
+#  define hypre_NAME_C_CALLING_FORT(name) name##_
+#  define hypre_NAME_HYPRE_CALLING_BLAS(name) name##_
 #endif
 
 #define hypre_F90_IFACE(iface_name) hypre_NAME_FORT_CALLING_C(iface_name)
 #define hypre_F90_NAME(iface_name)  hypre_NAME_C_CALLING_FORT(iface_name)
+#define hypre_BLAS_NAME(iface_name)  hypre_NAME_HYPRE_CALLING_BLAS(iface_name)
 
 #endif
