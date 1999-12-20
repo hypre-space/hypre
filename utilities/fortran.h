1934bcd54ea59b04f573b86ec12989c7e3e176bc
falgout <falgout>
1999-12-20 21:23:14 +0000

    Removed hypre_BLAS_NAME because it was used nowhere in the library, and it was broken anyway.

--- utilities/fortran.h
+++ utilities/fortran.h
@@ -33,13 +33,7 @@
 
 #endif
 
-#if defined(HYPRE_BLAS)
-#  define hypre_NAME_C_CALLING_FORT(name) name##_
-#  define hypre_NAME_HYPRE_CALLING_BLAS(name) name##_
-#endif
-
 #define hypre_F90_IFACE(iface_name) hypre_NAME_FORT_CALLING_C(iface_name)
 #define hypre_F90_NAME(iface_name)  hypre_NAME_C_CALLING_FORT(iface_name)
-#define hypre_BLAS_NAME(iface_name)  hypre_NAME_HYPRE_CALLING_BLAS(iface_name)
 
 #endif
