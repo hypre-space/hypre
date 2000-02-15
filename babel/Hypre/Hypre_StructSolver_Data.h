b442830794bb97d532b2554d3110fc21a19756d6
painter <painter>
2000-02-15 17:43:08 +0000

    Implement new class, Hypre_StructSolver, which contains a pointer to a HYPRE_StructSolver and is inherited by the other solvers.

---
+++ babel/Hypre/Hypre_StructSolver_Data.h
@@ -0,0 +1,18 @@
+/* *****************************************************
+ *
+ *	File:  Hypre_StructSolver_DataMembers.h
+ *
+ *********************************************************/
+
+#ifndef Hypre_StructSolver_DataMembers_
+#define Hypre_StructSolver_DataMembers_
+
+#include "struct_linear_solvers.h"
+
+struct Hypre_StructSolver_private_type
+{
+   HYPRE_StructSolver * hssolver;
+}
+;
+#endif
+
