ab2faf78daf3fb8d618e2012612b729235537dd2
chtong <chtong>
2000-02-28 20:02:00 +0000

    FEI 1.2 additions - Tong (2.28/2000)

---
+++ FEI_mv/fei-base/cfei-isis.h
@@ -0,0 +1,42 @@
+#ifndef cfei_isis_h_included
+#define cfei_isis_h_included
+/*
+          This file is part of Sandia National Laboratories
+          copyrighted software.  You are legally liable for any
+          unauthorized use of this software.
+
+          NOTICE:  The United States Government has granted for
+          itself and others acting on its behalf a paid-up,
+          nonexclusive, irrevocable worldwide license in this
+          data to reproduce, prepare derivative works, and
+          perform publicly and display publicly.  Beginning five
+          (5) years after June 5, 1997, the United States
+          Government is granted for itself and others acting on
+          its behalf a paid-up, nonexclusive, irrevocable
+          worldwide license in this data to reproduce, prepare
+          derivative works, distribute copies to the public,
+          perform publicly and display publicly, and to permit
+          others to do so.
+
+          NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+          STATES DEPARTMENT OF ENERGY, NOR SANDIA CORPORATION,
+          NOR ANY OF THEIR EMPLOYEES, MAKES ANY WARRANTY, EXPRESS
+          OR IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR
+          RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR
+          USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR
+          PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT
+          INFRINGE PRIVATELY OWNED RIGHTS.
+*/
+
+#ifdef FEI_SER
+#include <mpiuni/mpi.h>
+#else
+#include <mpi.h>
+#endif
+
+#include "other/basicTypes.h"
+#include "src/cfei.h"
+
+
+#endif /*cfei_isis_h_included*/
+
