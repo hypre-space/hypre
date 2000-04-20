d7e4ad7a0dbc98d7ee4f0274d2f20973d2da94e4
painter <painter>
2000-04-20 17:24:33 +0000

    first working implementation of the Babel interface for Hypre ParCSR matrices.

--- babel/Hypre/Hypre_ParCSRMatrix.c
+++ babel/Hypre/Hypre_ParCSRMatrix.c
@@ -8,6 +8,10 @@
 #include "Hypre_ParCSRMatrix_Skel.h" 
 #include "Hypre_ParCSRMatrix_Data.h" 
 
+#include <assert.h>
+#include "Hypre_ParCSRVector_Skel.h" 
+#include "Hypre_ParCSRVector_Data.h" 
+#include "IJ_matrix_vector.h"
 
 /* *************************************************
  * Constructor
@@ -15,6 +19,10 @@
  *    and initialize here
  ***************************************************/
 void Hypre_ParCSRMatrix_constructor(Hypre_ParCSRMatrix this) {
+   this->d_table = (struct Hypre_ParCSRMatrix_private_type *)
+      malloc( sizeof( struct Hypre_ParCSRMatrix_private_type ) );
+
+   this->d_table->Hmat = (HYPRE_IJMatrix *) malloc( sizeof( HYPRE_IJMatrix ) );
 
 } /* end constructor */
 
@@ -23,13 +31,65 @@ void Hypre_ParCSRMatrix_constructor(Hypre_ParCSRMatrix this) {
  *      deallocate memory for private data here.
  ***************************************************/
 void Hypre_ParCSRMatrix_destructor(Hypre_ParCSRMatrix this) {
+   struct Hypre_ParCSRMatrix_private_type *Mp = this->d_table;
+   HYPRE_IJMatrix *M = Mp->Hmat;
+
+   HYPRE_IJMatrixDestroy( *M );
+   free(this->d_table);
 
 } /* end destructor */
 
 /* ********************************************************
  * impl_Hypre_ParCSRMatrixApply
- *       insert the library code below
+ * y = A*x where this=A
  **********************************************************/
-int  impl_Hypre_ParCSRMatrix_Apply(Hypre_ParCSRMatrix this, Hypre_Vector x, Hypre_Vector* y) {
+int  impl_Hypre_ParCSRMatrix_Apply
+(Hypre_ParCSRMatrix this, Hypre_Vector x, Hypre_Vector* y) {
+   int ierr = 0;
+/* We just extract the relevant hypre_ParCSRMatrix and hypre_ParVector's, and
+   call par_csr_matvec.c:hypre_ParCSRMatrixMatvec. It should stick the new data
+   into y (its y is the same as the y here).
+   There are lots of places where there could be more defensive coding,
+   asserts, requires, etc.
+*/
+   struct Hypre_ParCSRMatrix_private_type *Mp = this->d_table;
+   HYPRE_IJMatrix *HM = Mp->Hmat;
+   hypre_IJMatrix *hM = (hypre_IJMatrix *) (*HM);
+   hypre_ParCSRMatrix *parM;
+
+   Hypre_ParCSRVector xin;
+   struct Hypre_ParCSRVector_private_type *HPx;
+   HYPRE_IJVector *Hx;
+   hypre_IJVector *hx;
+   hypre_ParVector *par_x;
+
+   Hypre_ParCSRVector yin;
+   struct Hypre_ParCSRVector_private_type *HPy;
+   HYPRE_IJVector *Hy;
+   hypre_IJVector *hy;
+   hypre_ParVector *par_y;
+
+   assert(hypre_IJMatrixLocalStorage(hM));
+   parM = hypre_IJMatrixLocalStorage(hM);
+
+   xin = (Hypre_ParCSRVector) Hypre_Vector_castTo( x, "Hypre_ParCSRVector" );
+   if ( xin==NULL ) return 1;
+   HPx = xin->d_table;
+   Hx = HPx->Hvec;
+   hx = (hypre_IJVector *) *Hx;
+   assert( hypre_IJVectorLocalStorage(hx) );
+   par_x = hypre_IJVectorLocalStorage(hx);
+
+   yin = (Hypre_ParCSRVector) Hypre_Vector_castTo( *y, "Hypre_ParCSRVector" );
+   if ( yin==NULL ) return 1;
+   HPy = yin->d_table;
+   Hy = HPy->Hvec;
+   hy = (hypre_IJVector *) *Hy;
+   assert( hypre_IJVectorLocalStorage(hy) );
+   par_y = hypre_IJVectorLocalStorage(hy);
+
+   ierr += hypre_ParCSRMatrixMatvec( 1.0, parM, par_x, 0.0, par_y );
+
+   return ierr;
 } /* end impl_Hypre_ParCSRMatrixApply */
 
--- babel/Hypre/Hypre_ParCSRMatrix_Data.h
+++ babel/Hypre/Hypre_ParCSRMatrix_Data.h
@@ -6,14 +6,13 @@
 
 #ifndef Hypre_ParCSRMatrix_DataMembers_
 #define Hypre_ParCSRMatrix_DataMembers_
+
+#include "HYPRE_IJ_mv.h"
+
 struct Hypre_ParCSRMatrix_private_type
-/*{
-
-  *****************************************
-            Add data members here
-  *****************************************
-
-}*/
+{
+   HYPRE_IJMatrix *Hmat;
+}
 ;
 #endif
 
