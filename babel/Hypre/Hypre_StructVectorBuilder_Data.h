7b1a62d8b1c0e253ccc638c01cb520cb14062fa5
painter <painter>
2000-05-05 23:04:02 +0000

    name change

--- babel/Hypre/Hypre_StructVectorBuilder_Data.h
+++ babel/Hypre/Hypre_StructVectorBuilder_Data.h
@@ -1,22 +1,21 @@
 /* *****************************************************
  *
- *	File:  Hypre_StructVectorBldr_DataMembers.h
+ *	File:  Hypre_StructVectorBuilder_DataMembers.h
  *
  *********************************************************/
 
-#ifndef Hypre_StructVectorBldr_DataMembers_
-#define Hypre_StructVectorBldr_DataMembers_
+#ifndef Hypre_StructVectorBuilder_DataMembers_
+#define Hypre_StructVectorBuilder_DataMembers_
 
 #include "Hypre_StructVector.h"
 
-struct Hypre_StructVectorBldr_private_type
+struct Hypre_StructVectorBuilder_private_type
 {
    Hypre_StructVector newvec;
    /* ... the vector currently under construction */
    int vecgood;
    /* ... 1 if newvec is a valid vector, 0 if not, still under construction */
-};
-
+}
 ;
 #endif
 
