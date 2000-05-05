93a668d3a9078404700f6144806bda361025ab1e
painter <painter>
2000-05-05 22:45:35 +0000

    name change to class.  StructuredGrid is now an interface, not a class.

--- babel/Hypre/Hypre_StructGrid_Data.h
+++ babel/Hypre/Hypre_StructGrid_Data.h
@@ -1,11 +1,11 @@
 /* *****************************************************
  *
- *	File:  Hypre_StructuredGrid_DataMembers.h
+ *	File:  Hypre_StructGrid_DataMembers.h
  *
  *********************************************************/
 
-#ifndef Hypre_StructuredGrid_DataMembers_
-#define Hypre_StructuredGrid_DataMembers_
+#ifndef Hypre_StructGrid_DataMembers_
+#define Hypre_StructGrid_DataMembers_
 
 #include "HYPRE_mv.h"
 #include "struct_matrix_vector.h"
@@ -13,13 +13,14 @@
 #include "Hypre_MPI_Com_Skel.h"
 #include "Hypre_MPI_Com_Data.h"
 
-struct Hypre_StructuredGrid_private_type
+struct Hypre_StructGrid_private_type
 {
    HYPRE_StructGrid *hsgrid;
    Hypre_MPI_Com comm;
    /* ... the MPI communicator information is also available from
       the HYPRE_StructGrid.  Putting it at this level makes a somewhat
       cleaner interface.  It's used in building a StructVector. */
+
 }
 ;
 #endif
