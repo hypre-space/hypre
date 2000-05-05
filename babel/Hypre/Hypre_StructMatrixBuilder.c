bed97f1f310eb489f1bce2ebcb01ae1923c8703e
painter <painter>
2000-05-05 23:28:34 +0000

    name change, and various changes due to new SIDL file

--- babel/Hypre/Hypre_StructMatrixBuilder.c
+++ babel/Hypre/Hypre_StructMatrixBuilder.c
@@ -1,12 +1,12 @@
-
+/* TO DO: this file has unimplemented functions (marked as such).  Implement some of them */
 /******************************************************
  *
- *  File:  Hypre_StructMatrixBldr.c
+ *  File:  Hypre_StructMatrixBuilder.c
  *
  *********************************************************/
 
-#include "Hypre_StructMatrixBldr_Skel.h" 
-#include "Hypre_StructMatrixBldr_Data.h" 
+#include "Hypre_StructMatrixBuilder_Skel.h" 
+#include "Hypre_StructMatrixBuilder_Data.h" 
 
 /* This builder makes a StructMatrix.  It can only make one at a time;
    any matrix under construction will be abandoned if a new one is begun.
@@ -27,9 +27,9 @@
  *    Allocate Memory for private data
  *    and initialize here
  ***************************************************/
-void Hypre_StructMatrixBldr_constructor(Hypre_StructMatrixBldr this) {
-   this->d_table = (struct Hypre_StructMatrixBldr_private_type *)
-      malloc( sizeof( struct Hypre_StructMatrixBldr_private_type ) );
+void Hypre_StructMatrixBuilder_constructor(Hypre_StructMatrixBuilder this) {
+   this->d_table = (struct Hypre_StructMatrixBuilder_private_type *)
+      malloc( sizeof( struct Hypre_StructMatrixBuilder_private_type ) );
    this->d_table->newmat = NULL;
    this->d_table->matgood = 0;
 } /* end constructor */
@@ -38,7 +38,7 @@ void Hypre_StructMatrixBldr_constructor(Hypre_StructMatrixBldr this) {
  *  Destructor
  *      deallocate memory for private data here.
  ***************************************************/
-void Hypre_StructMatrixBldr_destructor(Hypre_StructMatrixBldr this) {
+void Hypre_StructMatrixBuilder_destructor(Hypre_StructMatrixBuilder this) {
    if ( this->d_table->newmat != NULL ) {
       Hypre_StructMatrix_deleteReference( this->d_table->newmat );
       /* ... will delete newmat if there are no other references to it */
@@ -47,30 +47,31 @@ void Hypre_StructMatrixBldr_destructor(Hypre_StructMatrixBldr this) {
 } /* end destructor */
 
 /* ********************************************************
- * impl_Hypre_StructMatrixBldrprint
+ * impl_Hypre_StructMatrixBuilderSetStencil
  **********************************************************/
-void  impl_Hypre_StructMatrixBldr_print(Hypre_StructMatrixBldr this) {
-   printf( "StructMatrixBldr\n" );
-} /* end impl_Hypre_StructMatrixBldrprint */
-
-/* ********************************************************
- * impl_Hypre_StructMatrixBldrSetStencil
- *       insert the library code below
- **********************************************************/
-int  impl_Hypre_StructMatrixBldr_SetStencil
-(Hypre_StructMatrixBldr this, Hypre_StructStencil stencil) {
+int  impl_Hypre_StructMatrixBuilder_SetStencil
+(Hypre_StructMatrixBuilder this, Hypre_StructStencil stencil) {
 /* not implemented; this functionality isn't in Hypre (though doesn't
    look too hard to put in)
    */
-   printf( "unimplemented function, Hypre_StructMatrixBldr_SetStencil, was called" );
-
-} /* end impl_Hypre_StructMatrixBldrSetStencil */
+   printf( "unimplemented function, Hypre_StructMatrixBuilder_SetStencil, was called\n" );
+   return 1;
+} /* end impl_Hypre_StructMatrixBuilderSetStencil */
 
 /* ********************************************************
- * impl_Hypre_StructMatrixBldrSetValues
+ * impl_Hypre_StructMatrixBuilderSetValue
  **********************************************************/
-int  impl_Hypre_StructMatrixBldr_SetValues
-(Hypre_StructMatrixBldr this, Hypre_Box box,
+int  impl_Hypre_StructMatrixBuilder_SetValue
+(Hypre_StructMatrixBuilder this, array1int where, double value) {
+   printf( "unimplemented function, Hypre_StructMatrixBuilder_SetValue, was called\n" );
+   return 1;
+} /* end impl_Hypre_StructMatrixBuilderSetValue */
+
+/* ********************************************************
+ * impl_Hypre_StructMatrixBuilderSetBoxValues
+ **********************************************************/
+int  impl_Hypre_StructMatrixBuilder_SetBoxValues
+(Hypre_StructMatrixBuilder this, Hypre_Box box,
  array1int stencil_indices, array1double values) {
    int i, ssize, lower[3], upper[3];
    struct Hypre_StructMatrix_private_type * SMp;
@@ -103,39 +104,40 @@ int  impl_Hypre_StructMatrixBldr_SetValues
       &(stencil_indices.data[*(stencil_indices.lower)]),
       &(values.data[*(values.lower)]) );
 
-} /* end impl_Hypre_StructMatrixBldrSetValues */
-
-
-/* ********************************************************
- * impl_Hypre_StructMatrixBldrApply
- *       insert the library code below
- **********************************************************/
-int  impl_Hypre_StructMatrixBldr_Apply(Hypre_StructMatrixBldr this, Hypre_StructVector b, Hypre_StructVector* x) {
-} /* end impl_Hypre_StructMatrixBldrApply */
+} /* end impl_Hypre_StructMatrixBuilderSetBoxValues */
 
 /* ********************************************************
- * impl_Hypre_StructMatrixBldrGetConstructedObject
+ * impl_Hypre_StructMatrixBuilderGetDims
  **********************************************************/
-Hypre_LinearOperator
-impl_Hypre_StructMatrixBldr_GetConstructedObject(Hypre_StructMatrixBldr this) {
-   Hypre_LinearOperator LO;
+int  impl_Hypre_StructMatrixBuilder_GetDims
+(Hypre_StructMatrixBuilder this, int* m, int* n) {
+   printf( "unimplemented function, Hypre_StructMatrixBuilderGetdims, was called\n");
+   return 1;
+} /* end impl_Hypre_StructMatrixBuilderGetDims */
+
+/* ********************************************************
+ * impl_Hypre_StructMatrixBuilderGetConstructedObject
+ **********************************************************/
+int  impl_Hypre_StructMatrixBuilder_GetConstructedObject
+(Hypre_StructMatrixBuilder this, Hypre_LinearOperator* obj) {
    Hypre_StructMatrix newmat = this->d_table->newmat;
    if ( newmat==NULL  ||  this->d_table->matgood==0 ) {
-      return (Hypre_LinearOperator) NULL;
+      *obj = (Hypre_LinearOperator) NULL;
+      return 1;
    };
-   LO = (Hypre_LinearOperator)
+   *obj = (Hypre_LinearOperator)
       Hypre_StructMatrix_castTo( newmat, "Hypre_LinearOperator" );
-   return LO;
-} /* end impl_Hypre_StructMatrixBldrGetConstructedObject */
+   return 0;
+} /* end impl_Hypre_StructMatrixBuilderGetConstructedObject */
 
 /* ********************************************************
- * impl_Hypre_StructMatrixBldrNew
+ * impl_Hypre_StructMatrixBuilderNew
  **********************************************************/
-int  impl_Hypre_StructMatrixBldr_New
-( Hypre_StructMatrixBldr this, Hypre_StructuredGrid grid,
-  Hypre_StructStencil stencil, int symmetric, array1int num_ghost) {
+int  impl_Hypre_StructMatrixBuilder_New
+(Hypre_StructMatrixBuilder this, Hypre_StructGrid grid,
+ Hypre_StructStencil stencil, int symmetric, array1int num_ghost) {
 
-   struct Hypre_StructuredGrid_private_type *Gp = grid->d_table;
+   struct Hypre_StructGrid_private_type *Gp = grid->d_table;
    HYPRE_StructGrid *G = Gp->hsgrid;
    hypre_StructGrid *g = (hypre_StructGrid *) *G;
 
@@ -165,10 +167,10 @@ int  impl_Hypre_StructMatrixBldr_New
    ierr += HYPRE_StructMatrixInitialize( *M );
 
    return ierr;
-} /* end impl_Hypre_StructMatrixBldrNew */
+} /* end impl_Hypre_StructMatrixBuilderNew */
 
 /* ********************************************************
- * impl_Hypre_StructMatrixBldrConstructor
+ * impl_Hypre_StructMatrixBuilderConstructor
  *
  * The arguments are ignored; they really belong in the New function
  * which is separately called.
@@ -176,16 +178,16 @@ int  impl_Hypre_StructMatrixBldr_New
  * class be its own builder, then the Constructor will call New directly,
  * and it needs the arguments for that call.
  **********************************************************/
-Hypre_StructMatrixBldr impl_Hypre_StructMatrixBldr_Constructor
-(Hypre_StructuredGrid grid, Hypre_StructStencil stencil,
+Hypre_StructMatrixBuilder  impl_Hypre_StructMatrixBuilder_Constructor
+(Hypre_StructGrid grid, Hypre_StructStencil stencil,
  int symmetric, array1int num_ghost) {
-   return Hypre_StructMatrixBldr_new();
-} /* end impl_Hypre_StructMatrixBldrConstructor */
+   return Hypre_StructMatrixBuilder_new();
+} /* end impl_Hypre_StructMatrixBuilderConstructor */
 
 /* ********************************************************
- * impl_Hypre_StructMatrixBldrSetup
+ * impl_Hypre_StructMatrixBuilderSetup
  **********************************************************/
-int  impl_Hypre_StructMatrixBldr_Setup(Hypre_StructMatrixBldr this) {
+int  impl_Hypre_StructMatrixBuilder_Setup(Hypre_StructMatrixBuilder this) {
    int ierr;
    struct Hypre_StructMatrix_private_type * SMp;
    HYPRE_StructMatrix * M;
@@ -201,4 +203,5 @@ int  impl_Hypre_StructMatrixBldr_Setup(Hypre_StructMatrixBldr this) {
    if ( ierr==0 ) this->d_table->matgood = 1;
    return ierr;
 
-} /* end impl_Hypre_StructMatrixBldrSetup */
+} /* end impl_Hypre_StructMatrixBuilderSetup */
+
