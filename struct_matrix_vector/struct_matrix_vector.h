6b394cc693a0b5c998fdf86e83860acf513df2f8
falgout <falgout>
2000-06-26 02:03:39 +0000

    Changed the order of the '.h' files.

--- struct_matrix_vector/struct_matrix_vector.h
+++ struct_matrix_vector/struct_matrix_vector.h
@@ -995,53 +995,6 @@ typedef struct hypre_BoxNeighbors_struct
  *********************************************************************EHEADER*/
 /******************************************************************************
  *
- * Header info for hypre_StructStencil data structures
- *
- *****************************************************************************/
-
-#ifndef hypre_STRUCT_STENCIL_HEADER
-#define hypre_STRUCT_STENCIL_HEADER
-
-/*--------------------------------------------------------------------------
- * hypre_StructStencil
- *--------------------------------------------------------------------------*/
-
-typedef struct hypre_StructStencil_struct
-{
-   hypre_Index   *shape;   /* Description of a stencil's shape */
-   int            size;    /* Number of stencil coefficients */
-   int            max_offset;
-                
-   int            dim;     /* Number of dimensions */
-
-   int            ref_count;
-
-} hypre_StructStencil;
-
-/*--------------------------------------------------------------------------
- * Accessor functions for the hypre_StructStencil structure
- *--------------------------------------------------------------------------*/
-
-#define hypre_StructStencilShape(stencil)      ((stencil) -> shape)
-#define hypre_StructStencilSize(stencil)       ((stencil) -> size)
-#define hypre_StructStencilMaxOffset(stencil)  ((stencil) -> max_offset)
-#define hypre_StructStencilDim(stencil)        ((stencil) -> dim)
-#define hypre_StructStencilRefCount(stencil)   ((stencil) -> ref_count)
-
-#define hypre_StructStencilElement(stencil, i) \
-hypre_StructStencilShape(stencil)[i]
-
-#endif
-/*BHEADER**********************************************************************
- * (c) 1997   The Regents of the University of California
- *
- * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
- * notice, contact person, and disclaimer.
- *
- * $Revision$
- *********************************************************************EHEADER*/
-/******************************************************************************
- *
  * Header info for the hypre_StructGrid structures
  *
  *****************************************************************************/
@@ -1106,6 +1059,53 @@ hypre_ForBoxI(i, hypre_StructGridBoxes(grid))
 
 #endif
 
+/*BHEADER**********************************************************************
+ * (c) 1997   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+/******************************************************************************
+ *
+ * Header info for hypre_StructStencil data structures
+ *
+ *****************************************************************************/
+
+#ifndef hypre_STRUCT_STENCIL_HEADER
+#define hypre_STRUCT_STENCIL_HEADER
+
+/*--------------------------------------------------------------------------
+ * hypre_StructStencil
+ *--------------------------------------------------------------------------*/
+
+typedef struct hypre_StructStencil_struct
+{
+   hypre_Index   *shape;   /* Description of a stencil's shape */
+   int            size;    /* Number of stencil coefficients */
+   int            max_offset;
+                
+   int            dim;     /* Number of dimensions */
+
+   int            ref_count;
+
+} hypre_StructStencil;
+
+/*--------------------------------------------------------------------------
+ * Accessor functions for the hypre_StructStencil structure
+ *--------------------------------------------------------------------------*/
+
+#define hypre_StructStencilShape(stencil)      ((stencil) -> shape)
+#define hypre_StructStencilSize(stencil)       ((stencil) -> size)
+#define hypre_StructStencilMaxOffset(stencil)  ((stencil) -> max_offset)
+#define hypre_StructStencilDim(stencil)        ((stencil) -> dim)
+#define hypre_StructStencilRefCount(stencil)   ((stencil) -> ref_count)
+
+#define hypre_StructStencilElement(stencil, i) \
+hypre_StructStencilShape(stencil)[i]
+
+#endif
 /*BHEADER**********************************************************************
  * (c) 1997   The Regents of the University of California
  *
