c88618d544af55c1dabda47d6ef9cdf087644472
chtong <chtong>
2000-10-03 22:00:54 +0000

    minor changes to the interface.

--- FEI_matrix_vector/fei-hypre/HYPRE_LSI_poly.h
+++ FEI_matrix_vector/fei-hypre/HYPRE_LSI_poly.h
@@ -24,17 +24,22 @@
 #include "parcsr_linear_solvers/HYPRE_parcsr_ls.h"
 #include "parcsr_matrix_vector/parcsr_matrix_vector.h"
 
+#ifdef __cplusplus
 extern "C"
 {
-   int HYPRE_LSI_PolyCreate( MPI_Comm comm, HYPRE_Solver *solver );
-   int HYPRE_LSI_PolyDestroy( HYPRE_Solver solver );
-   int HYPRE_LSI_PolySetOrder( HYPRE_Solver solver, int order);
-   int HYPRE_LSI_PolySetOutputLevel( HYPRE_Solver solver, int level);
-   int HYPRE_LSI_PolySolve( HYPRE_Solver solver, HYPRE_ParCSRMatrix A,
-                            HYPRE_ParVector b,   HYPRE_ParVector x );
-   int HYPRE_LSI_PolySetup( HYPRE_Solver solver, HYPRE_ParCSRMatrix A,
-                            HYPRE_ParVector b,   HYPRE_ParVector x );
+#endif
+
+extern int HYPRE_LSI_PolyCreate( MPI_Comm comm, HYPRE_Solver *solver );
+extern int HYPRE_LSI_PolyDestroy( HYPRE_Solver solver );
+extern int HYPRE_LSI_PolySetOrder( HYPRE_Solver solver, int order);
+extern int HYPRE_LSI_PolySetOutputLevel( HYPRE_Solver solver, int level);
+extern int HYPRE_LSI_PolySolve( HYPRE_Solver solver, HYPRE_ParCSRMatrix A,
+                                HYPRE_ParVector b,   HYPRE_ParVector x );
+extern int HYPRE_LSI_PolySetup( HYPRE_Solver solver, HYPRE_ParCSRMatrix A,
+                                HYPRE_ParVector b,   HYPRE_ParVector x );
+#ifdef __cplusplus
 }
+#endif
 
 #endif
 
