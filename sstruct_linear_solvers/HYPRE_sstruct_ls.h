eb4fce8f671190c0fbca1cc5a983add00d6df59f
falgout <falgout>
2000-10-23 16:13:18 +0000

    Added PCG prototypes to the header file.

--- sstruct_linear_solvers/HYPRE_sstruct_ls.h
+++ sstruct_linear_solvers/HYPRE_sstruct_ls.h
@@ -59,6 +59,100 @@ typedef int (*HYPRE_PtrToSStructSolverFcn)(HYPRE_SStructSolver,
  *--------------------------------------------------------------------------*/
 
 /**
+ * @name SStruct PCG Solver
+ **/
+/*@{*/
+
+/**
+ * Create a solver object.
+ **/
+int HYPRE_SStructPCGCreate(MPI_Comm             comm,
+                           HYPRE_SStructSolver *solver);
+
+/**
+ * Destroy a solver object.  An object should be explicitly destroyed
+ * using this destructor when the user's code no longer needs direct
+ * access to it.  Once destroyed, the object must not be referenced
+ * again.  Note that the object may not be deallocated at the
+ * completion of this call, since there may be internal package
+ * references to the object.  The object will then be destroyed when
+ * all internal reference counts go to zero.
+ **/
+int HYPRE_SStructPCGDestroy(HYPRE_SStructSolver solver);
+
+/**
+ **/
+int HYPRE_SStructPCGSetup(HYPRE_SStructSolver solver,
+                          HYPRE_SStructMatrix A,
+                          HYPRE_SStructVector b,
+                          HYPRE_SStructVector x);
+
+/**
+ * Solve the system.
+ **/
+int HYPRE_SStructPCGSolve(HYPRE_SStructSolver solver,
+                          HYPRE_SStructMatrix A,
+                          HYPRE_SStructVector b,
+                          HYPRE_SStructVector x);
+
+/**
+ * (Optional) Set the convergence tolerance.
+ **/
+int HYPRE_SStructPCGSetTol(HYPRE_SStructSolver solver,
+                           double              tol);
+
+/**
+ * (Optional) Set maximum number of iterations.
+ **/
+int HYPRE_SStructPCGSetMaxIter(HYPRE_SStructSolver solver,
+                               int                 max_iter);
+
+/**
+ * (Optional) Set type of norm to use in stopping criteria.
+ **/
+int
+HYPRE_SStructPCGSetTwoNorm( HYPRE_SStructSolver solver,
+                            int                 two_norm );
+
+/**
+ * (Optional) Set to use additional relative-change convergence test.
+ **/
+int
+HYPRE_SStructPCGSetRelChange( HYPRE_SStructSolver solver,
+                              int                 rel_change );
+
+/**
+ * (Optional) Set the preconditioner to use.
+ **/
+int HYPRE_SStructPCGSetPrecond(HYPRE_SStructSolver          solver,
+                               HYPRE_PtrToSStructSolverFcn  precond,
+                               HYPRE_PtrToSStructSolverFcn  precond_setup,
+                               void                        *precond_solver);
+
+/**
+ * (Optional) Set the amount of logging to do.
+ **/
+int HYPRE_SStructPCGSetLogging(HYPRE_SStructSolver solver,
+                               int                 logging);
+
+/**
+ * Return the number of iterations taken.
+ **/
+int HYPRE_SStructPCGGetNumIterations(HYPRE_SStructSolver  solver,
+                                     int                 *num_iterations);
+
+/**
+ * Return the norm of the final relative residual.
+ **/
+int HYPRE_SStructPCGGetFinalRelativeResidualNorm(HYPRE_SStructSolver  solver,
+                                                 double              *norm);
+
+/*@}*/
+
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------*/
+
+/**
  * @name SStruct GMRES Solver
  **/
 /*@{*/
@@ -131,7 +225,7 @@ int HYPRE_SStructGMRESSetStopCrit(HYPRE_SStructSolver solver,
 int HYPRE_SStructGMRESSetPrecond(HYPRE_SStructSolver          solver,
                                  HYPRE_PtrToSStructSolverFcn  precond,
                                  HYPRE_PtrToSStructSolverFcn  precond_setup,
-                                 void *          precond_solver);
+                                 void                        *precond_solver);
 
 /**
  * (Optional) Set the amount of logging to do.
