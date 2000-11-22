a7896f84c63e661b7842c9f0c2775a1531c87e39
chtong <chtong>
2000-11-22 16:42:54 +0000

    update links to fei.

--- FEI_mv/fei-hypre/HYPRE_Builder.h
+++ FEI_mv/fei-hypre/HYPRE_Builder.h
@@ -1,18 +1,18 @@
 #ifndef _HYPRE_Builder_h_
 #define _HYPRE_Builder_h_
 
-#include "base/Data.h"
+#include "Data.h"
 
 #include "utilities/utilities.h"
 
-#include "base/LinearSystemCore.h"
+#include "LinearSystemCore.h"
 #include "HYPRE.h"
 #include "../../IJ_matrix_vector/HYPRE_IJ_mv.h"
 #include "../../parcsr_matrix_vector/HYPRE_parcsr_mv.h"
 #include "../../parcsr_linear_solvers/HYPRE_parcsr_ls.h"
 #include "HYPRE_LinSysCore.h"
 
-#include "base/FEI_Implementation.h"
+#include "FEI_Implementation.h"
 
 class HYPRE_Builder {
  public:
