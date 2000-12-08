686f9408fd1eed0b78ae92de6c2145df6f18f151
treadway <treadway>
2000-12-08 00:03:29 +0000

    usually ignored, alway rebuilt after a configure, needed by osf make

---
+++ HYPRE_config.h
@@ -0,0 +1,56 @@
+/* HYPRE_config.h.  Generated automatically by configure.  */
+#ifndef HYPRE_SEQUENTIAL
+/* #undef HYPRE_SEQUENTIAL */
+#endif
+
+#ifndef HYPRE_MEMORY_DMALLOC
+/* #undef HYPRE_MEMORY_DMALLOC */
+#endif
+
+#ifndef HYPRE_TIMING
+/* #undef HYPRE_TIMING */
+#endif
+
+#ifndef HYPRE_USING_OPENMP
+/* #undef HYPRE_USING_OPENMP */
+#endif
+
+#ifndef HYPRE_USING_IBM_SMP
+/* #undef HYPRE_USING_IBM_SMP */
+#endif
+
+#ifndef HYPRE_USING_SGI_SMP
+/* #undef HYPRE_USING_SGI_SMP */
+#endif
+
+#ifndef HYPRE_USING_PGCC_SMP
+/* #undef HYPRE_USING_PGCC_SMP */
+#endif
+
+#ifndef HYPRE_USE_PTHREADS
+/* #undef HYPRE_USE_PTHREADS */
+#endif
+
+#ifndef HYPRE_SOLARIS
+#define HYPRE_SOLARIS 1
+#endif
+
+#ifndef HYPRE_ALPHA
+/* #undef HYPRE_ALPHA */
+#endif
+
+#ifndef HYPRE_RS6000
+/* #undef HYPRE_RS6000 */
+#endif
+
+#ifndef HYPRE_USING_HYPRE_BLAS
+#define HYPRE_USING_HYPRE_BLAS 1
+#endif
+
+#ifndef HYPRE_USING_ESSL
+/* #undef HYPRE_USING_ESSL */
+#endif
+
+#ifndef HYPRE_USING_DXML
+/* #undef HYPRE_USING_DXML */
+#endif
