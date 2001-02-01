d3eb6d23246b420dda78a939be842994aef3b4ee
painter <painter>
2001-02-01 16:38:34 +0000

    headers.h and headers script for Hypre-style make system

---
+++ mli/matrix/headers.h
@@ -0,0 +1,15 @@
+/*BHEADER**********************************************************************
+ * (c) 1998-2001   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ * $Revision$
+ *********************************************************************EHEADER*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "mli_matrix.h"
+
