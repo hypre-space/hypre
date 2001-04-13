57e8673507804e8bd6cb454a18ac8f4fd0428748
chtong <chtong>
2001-04-13 23:46:39 +0000

    More addition to SA.

--- mli/coarsen/mli_aggr_gen.c
+++ mli/coarsen/mli_aggr_gen.c
@@ -12,22 +12,45 @@
  * Inputs    : Amat, aggregation information 
  **********************************************************************/
 
-void MLI_AMG_SA_GenProlongator(MLI_Aggregation *mli_aggr, 
-                               hypre_ParCSRMatrix *Amat, 
-                               hypre_ParCSRMatrix *Pmat)
+int MLI_AMG_SA_GenAllProlongator(MLI_Aggregation *mli_aggr, 
+                                 hypre_ParCSRMatrix **Amat_array)
+                                 hypre_ParCSRMatrix **Pmat_array)
 {
-   hypre_ParCSRMatrix *A2mat;
-   int                blk_size;
+   hypre_ParCSRMatrix *curr_Amat;
+   int                coarsen_scheme;
 
-   blk_size = mli_aggr->num_PDEs;
-   MLI_Matrix_DoBlocking(Amat, A2mat, blksize);
-   mli_aggr->matrix = A2mat;
-   MLI_AMG_SA_FormGraph(mli_aggr);
-   MLI_AMG_SA_CoarsenLocal(mli_aggr);
-   hypre_ParCSRMatrixDestroy(A2mat);
-   free( A2mat );
-   mli_aggr->matrix = Amat;
-   MLI_AMG_SA_GenPmatLocal(mli_aggr, Pmat);
-   return;
+   /* --------------------------------------------------------------- */
+   /* fetch aggregation information                                   */
+   /* --------------------------------------------------------------- */
+
+   coarsen_scheme = mli_aggr->coarsen_scheme;
+   nlevels        = mli_aggr->nlevels;
+   curr_Amat      = Amat_array[nlevels-1];
+   assert( curr_Amat );
+
+   /* --------------------------------------------------------------- */
+   /* traverse all levels                                             */
+   /* --------------------------------------------------------------- */
+
+   for (level = nlevels-1; level > 0 ; level-- )
+   {
+      switch ( coarsen_scheme )
+      {
+         case MLI_AGGR_LOCAL :
+              MLI_AMG_SA_GenPLocal(mli_aggr, Amat, Pmat); 
+              break;
+      }
+      if ( Pmat == NULL ) break;
+      hypre_BoomerAMGBuildCoarseOperator( Pmat, curr_Amat, Pmat, &cAmat );
+      Pmat_array[i-1] = Pmat;
+      Amat_array[i-1] = cAmat;
+      curr_Amat = cAmat;
+   }
+
+   /* --------------------------------------------------------------- */
+   /* return the coarsest grid level number                           */
+   /* --------------------------------------------------------------- */
+
+   return level;
 }
 
