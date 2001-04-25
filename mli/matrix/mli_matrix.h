c07caa1e38041b58264c9cd1dbc0cebe1df5946e
chtong <chtong>
2001-04-25 20:51:58 +0000

    Formating.

--- mli/matrix/mli_matrix.h
+++ mli/matrix/mli_matrix.h
@@ -36,7 +36,7 @@ MLI_Matrix;
 MLI_Matrix *MLI_Matrix_Create(void *in_matrix, char *name,
                               void (*mat_destroyer)(void *))
 {
-   MLI_Matrix *mli_matrix = hypre_CTAlloc(MLI_Matrix, 1);
+   MLI_Matrix *mli_matrix = (MLI_Matrix *) calloc(MLI_Matrix, 1);
    mli_matrix->matrix = in_matrix;
    mli_matrix->destroy_func = mat_destroyer;
    strncpy(mli_matrix->name, name, 100);
@@ -48,10 +48,10 @@ void *MLI_Matrix_Destroy(MLI_Matrix *mli_matrix)
    if ( mli_matrix != NULL )
    {
       if ( mli_matrix->matrix != NULL && mli_matrix->destroy_func != NULL )
-         mli_matrix->destroy_func(mli_matrix->matrix);
-      mli_matrix->matrix = NULL;
-      mli_matrix->destroy_func = NULL;
-      hypre_TFree( mli_matrix );
+               mli_matrix->destroy_func(mli_matrix->matrix);
+            mli_matrix->matrix = NULL;
+            mli_matrix->destroy_func = NULL;
+            free( mli_matrix );
    }
 }
 
