9ac4f1fdccedfb609cc4245622124f62d8fcc20b
chtong <chtong>
2001-04-25 20:57:27 +0000

    Reformat files.

--- mli/matrix/mli_matrix.h
+++ mli/matrix/mli_matrix.h
@@ -8,7 +8,7 @@
 
 /******************************************************************************
  *
- * Header info for Parallel Matrix data structures
+ * Header info for MLI Matrix data structures
  *
  *****************************************************************************/
 
@@ -18,7 +18,7 @@
 #include "utilities.h"
 
 /*--------------------------------------------------------------------------
- * MLI_Matrix 
+ * MLI_Matrix declaration
  *--------------------------------------------------------------------------*/
 
 typedef struct
@@ -30,30 +30,22 @@ typedef struct
 MLI_Matrix;
 
 /*--------------------------------------------------------------------------
- * constructor and destructor functions for the MLI_Matrix 
+ * functions for the MLI_Matrix 
  *--------------------------------------------------------------------------*/
 
-MLI_Matrix *MLI_Matrix_Create(void *in_matrix, char *name,
-                              void (*mat_destroyer)(void *))
+#ifdef __cplusplus
+extern "C"
 {
-   MLI_Matrix *mli_matrix = (MLI_Matrix *) calloc(MLI_Matrix, 1);
-   mli_matrix->matrix = in_matrix;
-   mli_matrix->destroy_func = mat_destroyer;
-   strncpy(mli_matrix->name, name, 100);
-   return mli_matrix;
-}
+#endif
 
-void *MLI_Matrix_Destroy(MLI_Matrix *mli_matrix)
-{
-   if ( mli_matrix != NULL )
-   {
-      if ( mli_matrix->matrix != NULL && mli_matrix->destroy_func != NULL )
-               mli_matrix->destroy_func(mli_matrix->matrix);
-            mli_matrix->matrix = NULL;
-            mli_matrix->destroy_func = NULL;
-            free( mli_matrix );
-   }
+MLI_Matrix *MLI_Matrix_Create(void *in_matrix, char *name,
+                              void (*mat_destroyer)(void *));
+
+void *MLI_Matrix_Destroy(MLI_Matrix *mli_matrix);
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif
 
