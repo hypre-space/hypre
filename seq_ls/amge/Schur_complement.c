179a3a99e1b4da82296f1ad7b729ec50140e38fe
panayot <panayot>
2001-05-30 01:44:52 +0000

    minimal change to call matrix_inverse insted of mat_inv.

--- seq_ls/amge/Schur_complement.c
+++ seq_ls/amge/Schur_complement.c
@@ -169,7 +169,10 @@ int hypre_AMGeSchurComplement(int *i_domain_chord,
 	    A[first[j_loc]+first[i_loc]*local_dof_counter];
 
       if (first_counter > 0)
+	/*
 	ierr = mat_inv(X_11, A_11, first_counter);
+	*/
+	ierr = matrix_inverse(X_11, A_11, first_counter);
 
       /*
       printf("mat_inv_ierr: %d, first_counter: %d\n", ierr, first_counter);
