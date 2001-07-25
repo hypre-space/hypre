2759afc6d1ae55233d19508c992d593a95d464ea
hysom <hysom>
2001-07-25 00:05:52 +0000

    changed "HYPRE_ParCSREuclidxxx" to "HYPRE_Euclidxxx"

--- parcsr_ls/parcsr_ls.h
+++ parcsr_ls/parcsr_ls.h
@@ -17,15 +17,15 @@ extern "C" {
 
 
 /* HYPRE_parcsr_Euclid.c */
-int HYPRE_ParCSREuclidCreate( MPI_Comm comm , HYPRE_Solver *solver );
-int HYPRE_ParCSREuclidDestroy( HYPRE_Solver solver );
-int HYPRE_ParCSREuclidSetup( HYPRE_Solver solver , HYPRE_ParCSRMatrix A , HYPRE_ParVector b , HYPRE_ParVector x );
-int HYPRE_ParCSREuclidSolve( HYPRE_Solver solver , HYPRE_ParCSRMatrix A , HYPRE_ParVector bb , HYPRE_ParVector xx );
-int HYPRE_ParCSREuclidSetParams( HYPRE_Solver solver , int argc , char *argv []);
-int HYPRE_ParCSREuclidSetParamsFromFile( HYPRE_Solver solver , char *filename );
-int HYPRE_ParCSREuclidPrintParams( HYPRE_Solver solver );
-int HYPRE_ParCSREuclidReadRho( HYPRE_Solver solver , double *rho );
-int HYPRE_ParCSREuclidSetLogging( HYPRE_Solver solver , int logging );
+int HYPRE_EuclidCreate( MPI_Comm comm , HYPRE_Solver *solver );
+int HYPRE_EuclidDestroy( HYPRE_Solver solver );
+int HYPRE_EuclidSetup( HYPRE_Solver solver , HYPRE_ParCSRMatrix A , HYPRE_ParVector b , HYPRE_ParVector x );
+int HYPRE_EuclidSolve( HYPRE_Solver solver , HYPRE_ParCSRMatrix A , HYPRE_ParVector bb , HYPRE_ParVector xx );
+int HYPRE_EuclidSetParams( HYPRE_Solver solver , int argc , char *argv []);
+int HYPRE_EuclidSetParamsFromFile( HYPRE_Solver solver , char *filename );
+int HYPRE_EuclidPrintParams( HYPRE_Solver solver );
+int HYPRE_EuclidReadRho( HYPRE_Solver solver , double *rho );
+int HYPRE_EuclidSetLogging( HYPRE_Solver solver , int logging );
 
 /* HYPRE_parcsr_ParaSails.c */
 int HYPRE_ParCSRParaSailsCreate( MPI_Comm comm , HYPRE_Solver *solver );
