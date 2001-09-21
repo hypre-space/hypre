f7960a24c2d5ded798130b06734d5f20974c8c00
chtong <chtong>
2001-09-21 23:56:41 +0000

    Change headings.

--- FEI_mv/fei-hypre/HYPRE_parcsr_ml.c
+++ FEI_mv/fei-hypre/HYPRE_parcsr_ml.c
@@ -1,5 +1,5 @@
 /*BHEADER**********************************************************************
- * (c) 1998   The Regents of the University of California
+ * (c) 2000   The Regents of the University of California
  *
  * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
  * notice, contact person, and disclaimer.
