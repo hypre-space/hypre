27b94d1668827d83de763545808aec80d19348e1
chtong <chtong>
2002-01-31 18:24:27 +0000

    move to this directory.

---
+++ FEI_mv/femli/mapper/mli_mapper.h
@@ -0,0 +1,46 @@
+/*BHEADER**********************************************************************
+ * (c) 2002   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ *********************************************************************EHEADER*/
+
+/******************************************************************************
+ *
+ * Header info for the MLI_Mapper data structure
+ *
+ *****************************************************************************/
+
+#ifndef __MLIMAPPERH__
+#define __MLIMAPPERH__
+
+/*--------------------------------------------------------------------------
+ * include files 
+ *--------------------------------------------------------------------------*/
+
+#include <string.h>
+#include "utilities/utilities.h"
+
+/*--------------------------------------------------------------------------
+ * MLI_Mapper data structure declaration
+ *--------------------------------------------------------------------------*/
+
+class MLI_Mapper
+{
+   int nEntries;
+   int *tokenList;
+   int *tokenMap;
+public :
+
+   MLI_Mapper();
+   ~MLI_Mapper();
+
+   int   setMap(int nItems, int *itemList, int *mapList);
+   int   getMap(int nItems, int *itemList, int *mapList);
+   int   setParams(char *param_string, int argc, char **argv);
+ 
+};
+
+#endif
+
