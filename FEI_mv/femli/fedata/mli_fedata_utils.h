5d29757cc97c9669e5fab0baff349d7e5f5d1d05
chtong <chtong>
2002-01-31 18:23:22 +0000

    Move to this directory.

---
+++ FEI_mv/femli/fedata/mli_fedata_utils.h
@@ -0,0 +1,36 @@
+/*BHEADER**********************************************************************
+ * (c) 2001   The Regents of the University of California
+ *
+ * See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+ * notice, contact person, and disclaimer.
+ *
+ *********************************************************************EHEADER*/
+
+/**************************************************************************
+ **************************************************************************
+ * MLI_FEData utilities functions
+ **************************************************************************
+ **************************************************************************/
+
+#include <iostream.h>
+#include <stdio.h>
+#include <string.h>
+#include "HYPRE.h"
+#include <mpi.h>
+#include "parcsr_mv/parcsr_mv.h"
+#include "fedata/mli_fedata.h"
+#include "matrix/mli_matrix.h"
+
+/**************************************************************************
+ * functions 
+ *-----------------------------------------------------------------------*/
+
+void MLI_FEDataConstructElemNodeMatrix(MPI_Comm, MLI_FEData*, MLI_Matrix**);
+void MLI_FEDataConstructElemFaceMatrix(MPI_Comm, MLI_FEData*, MLI_Matrix**);
+void MLI_FEDataConstructFaceNodeMatrix(MPI_Comm, MLI_FEData*, MLI_Matrix**);
+void MLI_FEDataConstructNodeElemMatrix(MPI_Comm, MLI_FEData*, MLI_Matrix**);
+void MLI_FEDataConstructFaceElemMatrix(MPI_Comm, MLI_FEData*, MLI_Matrix**);
+void MLI_FEDataConstructNodeFaceMatrix(MPI_Comm, MLI_FEData*, MLI_Matrix**);
+void MLI_FEDataAgglomerateElemsLocal(MLI_Matrix *, int **macro_labels_out);
+void MLI_FEDataAgglomerateElemsLocalOld(MLI_Matrix *, int **macro_labels_out);
+
