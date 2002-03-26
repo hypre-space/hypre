d807a5accda6f4b81abf3975a926cfb5724e3eb6
chtong <chtong>
2002-03-26 17:17:58 +0000

    Change version number.

--- FEI_mv/fei-hypre/HYPRE_LinSysCore.h
+++ FEI_mv/fei-hypre/HYPRE_LinSysCore.h
@@ -13,7 +13,7 @@
 #ifndef _HYPRE_LinSysCore_h_
 #define _HYPRE_LinSysCore_h_
 
-#define HYPRE_FEI_Version() "FEI/HYPRE 2.0.1R13"
+#define HYPRE_FEI_Version() "FEI/HYPRE 2.0.1R14"
 
 // *************************************************************************
 // system libraries used
