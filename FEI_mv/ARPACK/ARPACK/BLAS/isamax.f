8c45ab9551afa9635ab0959cd2c4ac14c262d8c0
chtong <chtong>
2002-06-03 18:29:07 +0000

    Blas files.

---
+++ FEI_mv/ARPACK/ARPACK/BLAS/isamax.f
@@ -0,0 +1,39 @@
+      integer function isamax(n,sx,incx)
+c
+c     finds the index of element having max. absolute value.
+c     jack dongarra, linpack, 3/11/78.
+c     modified to correct problem with negative increment, 8/21/90.
+c
+      real sx(1),smax
+      integer i,incx,ix,n
+c
+      isamax = 0
+      if( n .lt. 1 ) return
+      isamax = 1
+      if(n.eq.1)return
+      if(incx.eq.1)go to 20
+c
+c        code for increment not equal to 1
+c
+      ix = 1
+      if(incx.lt.0)ix = (-n+1)*incx + 1
+      smax = abs(sx(ix))
+      ix = ix + incx
+      do 10 i = 2,n
+         if(abs(sx(ix)).le.smax) go to 5
+         isamax = i
+         smax = abs(sx(ix))
+    5    ix = ix + incx
+   10 continue
+      return
+c
+c        code for increment equal to 1
+c
+   20 smax = abs(sx(1))
+      do 30 i = 2,n
+         if(abs(sx(i)).le.smax) go to 30
+         isamax = i
+         smax = abs(sx(i))
+   30 continue
+      return
+      end
