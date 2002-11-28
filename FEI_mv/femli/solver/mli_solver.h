6c5b9721e04578a95f4ee9b8eb0564a0a5a66ad5
chtong <chtong>
2002-11-28 00:08:48 +0000

    Put solver defines here.

--- FEI_mv/femli/solver/mli_solver.h
+++ FEI_mv/femli/solver/mli_solver.h
@@ -16,6 +16,23 @@
 #define __MLI_SOLVER_H__
 
 /*--------------------------------------------------------------------------
+ * defines 
+ *--------------------------------------------------------------------------*/
+
+#define MLI_SOLVER_JACOBI_ID        301
+#define MLI_SOLVER_BJACOBI_ID       302
+#define MLI_SOLVER_GS_ID            303
+#define MLI_SOLVER_SGS_ID           304
+#define MLI_SOLVER_BSGS_ID          305
+#define MLI_SOLVER_PARASAILS_ID     306
+#define MLI_SOLVER_MLS_ID           307
+#define MLI_SOLVER_CHEBYSHEV_ID     308
+#define MLI_SOLVER_CG_ID            309
+#define MLI_SOLVER_SUPERLU_ID       310
+#define MLI_SOLVER_SEQSUPERLU_ID    311
+#define MLI_SOLVER_ARPACKSUPERLU_ID 312
+
+/*--------------------------------------------------------------------------
  * include files 
  *--------------------------------------------------------------------------*/
 
