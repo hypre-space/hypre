4d793b2bdce593c3622a89a86b441dc472acea6a
treadway <treadway>
2003-12-09 23:22:24 +0000

    strict check fixes

--- blas/blas_utils.c
+++ blas/blas_utils.c
@@ -69,12 +69,12 @@ r
  or   
           upper case 'Z'. */
 
-	if (inta >= 129 && inta <= 137 || inta >= 145 && inta <= 153 || inta 
-		>= 162 && inta <= 169) {
+	if ((inta >= 129 && inta <= 137) || (inta >= 145 && inta <= 153) || 
+                (inta >= 162 && inta <= 169)) {
 	    inta += 64;
 	}
-	if (intb >= 129 && intb <= 137 || intb >= 145 && intb <= 153 || intb 
-		>= 162 && intb <= 169) {
+	if ((intb >= 129 && intb <= 137) || (intb >= 145 && intb <= 153) || 
+                (intb >= 162 && intb <= 169)) {
 	    intb += 64;
 	}
 
@@ -100,6 +100,7 @@ e
     return ret_val;
 } /* hypre_lsame_ */
 
+#include <stdio.h>
 #include "f2c.h"
 
 /* Subroutine */ int hypre_xerbla_(char *srname, integer *info)
