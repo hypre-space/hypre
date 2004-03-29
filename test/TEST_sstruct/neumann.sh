7231256467705b4673b0f5b52ef1643cf6340937
hill66 <hill66>
2004-03-29 21:53:54 +0000

    Removed 2 least significant digits from final residual norm value so the diff'ing of output will find real differences rather than 'single bit' differences.

--- test/TEST_sstruct/neumann.sh
+++ test/TEST_sstruct/neumann.sh
@@ -12,14 +12,21 @@
 # sstruct: Test various blockings and distributions of default problem
 #=============================================================================
 
-tail -3 neumann.out.0 > neumann.testdata
+tail -3 neumann.out.0 > neumann.testa
+grep 'Final Relative Residual Norm' neumann.testa | sed 's/[0-9][0-9]e/e/' > neumann.testdata
 
-tail -3 neumann.out.2 > neumann.testdata.temp
+tail -3 neumann.out.2 > neumann.testb
+grep 'Final Relative Residual Norm' neumann.testb | sed 's/[0-9][0-9]e/e/' > neumann.testdata.temp
+
 diff neumann.testdata neumann.testdata.temp >&2
 
-tail -3 neumann.out.1 > neumann.testdata
 
-tail -3 neumann.out.3 > neumann.testdata.temp
+tail -3 neumann.out.1 > neumann.testa
+grep 'Final Relative Residual Norm' neumann.testa | sed 's/[0-9][0-9]e/e/' > neumann.testdata
+
+tail -3 neumann.out.3 > neumann.testb
+grep 'Final Relative Residual Norm' neumann.testb | sed 's/[0-9][0-9]e/e/' > neumann.testdata.temp
+
 diff neumann.testdata neumann.testdata.temp >&2
 
-rm -f neumann.testdata neumann.testdata.temp
+rm -f neumann.testdata neumann.testdata.temp neumann.testa neumann.testb
