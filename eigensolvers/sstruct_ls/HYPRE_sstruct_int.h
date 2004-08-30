c3f3064486894c5fb935713e3cdfca1ffc21afaf
chtong <chtong>
2004-08-30 20:19:28 +0000

    Add sstruct interpreter for lobpcg.

---
+++ eigensolvers/sstruct_ls/HYPRE_sstruct_int.h
@@ -0,0 +1,21 @@
+#ifndef HYPRE_PARCSR_INTERFACE_INTERPRETER
+#define HYPRE_PARCSR_INTERFACE_INTERPRETER
+
+#include <HYPRE_config.h>
+
+#include "sstruct_ls.h"
+
+#include "HYPRE_interpreter.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int
+HYPRE_SStructSetupInterpreter( HYPRE_InterfaceInterpreter *i );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
