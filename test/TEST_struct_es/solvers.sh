d754830511c08829a842d6dcbaf0c71583c1258f
hill66 <hill66>
2005-01-31 16:27:10 +0000

    Added tests for struct_es.

---
+++ test/TEST_struct_es/solvers.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh 
+#BHEADER***********************************************************************
+# (c) 1998   The Regents of the University of California
+#
+# See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+# notice, contact person, and disclaimer.
+#
+# $Revision$
+#EHEADER***********************************************************************
+
+#=============================================================================
+# sstruct_es: no test
+#=============================================================================
