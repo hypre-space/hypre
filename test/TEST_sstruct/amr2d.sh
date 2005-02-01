bb7b2c78e5ea84c7d1f4e161a67eb5bd86748a95
falgout <falgout>
2005-02-01 22:52:20 +0000

    Adding a 2D AMR-like test.

---
+++ test/TEST_sstruct/amr2d.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh 
+#BHEADER***********************************************************************
+# (c) 2005   The Regents of the University of California
+#
+# See the file COPYRIGHT_and_DISCLAIMER for a complete copyright
+# notice, contact person, and disclaimer.
+#
+# $Revision$
+#EHEADER***********************************************************************
+
+#=============================================================================
+#=============================================================================
+
