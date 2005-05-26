b3cb06965515bc41354b7c7619507fcd27d859eb
hill66 <hill66>
2005-05-26 15:57:23 +0000

    Removed duplicated routine hypre_StructVectorSetRandomValues.

--- sstruct_ls/HYPRE_sstruct_int.c
+++ sstruct_ls/HYPRE_sstruct_int.c
@@ -2,59 +2,6 @@
 
 #include "temp_multivector.h"
 
-int 
-hypre_StructVectorSetRandomValues( hypre_StructVector *vector,
-                                   int seed )
-{
-   int    ierr = 0;
-
-   hypre_Box          *v_data_box;
-                    
-   int                 vi;
-   double             *vp;
-
-   hypre_BoxArray     *boxes;
-   hypre_Box          *box;
-   hypre_Index         loop_size;
-   hypre_IndexRef      start;
-   hypre_Index         unit_stride;
-
-   int                 i;
-   int                 loopi, loopj, loopk;
-
-   /*-----------------------------------------------------------------------
-    * Set the vector coefficients
-    *-----------------------------------------------------------------------*/
-
-   srand( seed );
-
-   hypre_SetIndex(unit_stride, 1, 1, 1);
- 
-   boxes = hypre_StructGridBoxes(hypre_StructVectorGrid(vector));
-   hypre_ForBoxI(i, boxes)
-      {
-         box      = hypre_BoxArrayBox(boxes, i);
-         start = hypre_BoxIMin(box);
-
-         v_data_box =
-            hypre_BoxArrayBox(hypre_StructVectorDataSpace(vector), i);
-         vp = hypre_StructVectorBoxData(vector, i);
- 
-         hypre_BoxGetSize(box, loop_size);
-
-         hypre_BoxLoop1Begin(loop_size,
-                             v_data_box, start, unit_stride, vi);
-#define HYPRE_BOX_SMP_PRIVATE loopk,loopi,loopj,vi 
-#include "hypre_box_smp_forloop.h"
-         hypre_BoxLoop1For(loopi, loopj, loopk, vi)
-            {
-               vp[vi] = 2.0*rand()/RAND_MAX - 1.0;
-            }
-         hypre_BoxLoop1End(vi);
-      }
-
-   return ierr;
-}
 
 int 
 hypre_SStructPVectorSetRandomValues( hypre_SStructPVector *pvector, int seed )
@@ -70,7 +17,7 @@ hypre_SStructPVectorSetRandomValues( hypre_SStructPVector *pvector, int seed )
    {
       svector = hypre_SStructPVectorSVector(pvector, var);
 	  seed = rand();
-      hypre_StructVectorSetRandomValues(svector, seed);
+      hypre_SStructVectorSetRandomValues(svector, seed);
    }
 
    return ierr;
