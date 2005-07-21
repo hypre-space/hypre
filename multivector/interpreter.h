4a25cf7932c6483280c5840e507091742a58e50a
hill66 <hill66>
2005-07-21 22:24:25 +0000

    Updated LOBPCG from A. Knayzev

---
+++ multivector/interpreter.h
@@ -0,0 +1,44 @@
+#ifndef LOBPCG_INTERFACE_INTERPRETER
+#define LOBPCG_INTERFACE_INTERPRETER
+
+typedef struct
+{
+  /* vector operations */
+  void*  (*CreateVector)  ( void *vector );
+  int    (*DestroyVector) ( void *vector );
+
+  double (*InnerProd)     ( void *x, void *y );
+  int    (*CopyVector)    ( void *x, void *y );
+  int    (*ClearVector)   ( void *x );
+  int    (*SetRandomValues)   ( void *x, int seed );
+  int    (*ScaleVector)   ( double alpha, void *x );
+  int    (*Axpy)          ( double alpha, void *x, void *y );
+  int    (*VectorSize)    (void * vector);
+  
+  /* multivector operations */
+  /* do we need the following entry? */
+  void*  (*CreateMultiVector)  ( void*, int n, void *vector );
+  void*  (*CopyCreateMultiVector)  ( void *x, int );
+  void    (*DestroyMultiVector) ( void *x );
+
+  int    (*Width)  ( void *x );
+  int    (*Height) ( void *x );
+
+  void   (*SetMask) ( void *x, int *mask );
+
+  void   (*CopyMultiVector)    ( void *x, void *y );
+  void   (*ClearMultiVector)   ( void *x );
+  void   (*SetRandomVectors)   ( void *x, int seed );
+  void   (*MultiInnerProd)     ( void *x, void *y, int, int, int, double* );
+  void   (*MultiInnerProdDiag) ( void *x, void *y, int*, int, double* );
+  void   (*MultiVecMat)        ( void *x, int, int, int, double*, void *y );
+  void   (*MultiVecMatDiag)    ( void *x, int*, int, double*, void *y );
+  void   (*MultiAxpy)          ( double alpha, void *x, void *y );
+
+  /* do we need the following 2 entries? */
+  void   (*MultiXapy)          ( void *x, int, int, int, double*, void *y );
+  void   (*Eval)               ( void (*f)( void*, void*, void* ), void*, void *x, void *y );
+
+} mv_InterfaceInterpreter;
+
+#endif
