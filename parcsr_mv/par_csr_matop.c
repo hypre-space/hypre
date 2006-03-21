4611e91fde77cff411e85f43c1843b0bfe443db0
chtong <chtong>
2006-03-21 19:50:36 +0000

    Add spanning tree stuff.

--- parcsr_mv/par_csr_matop.c
+++ parcsr_mv/par_csr_matop.c
@@ -316,6 +316,11 @@ void hypre_ParCSRMatrixExtractSubmatrices(hypre_ParCSRMatrix *A_csr, int *indice
    comm = hypre_ParCSRMatrixComm(A_csr);
    MPI_Comm_rank(comm, &mypid);
    MPI_Comm_size(comm, &nprocs);
+   if (nprocs > 1)
+   {
+      printf("ExtractSubmatrices: cannot handle nprocs > 1 yet.\n");
+      exit(1);
+   }
 
    /* -----------------------------------------------------
     * compute new matrix dimensions
@@ -622,3 +627,269 @@ void hypre_ParCSRMatrixExtractSubmatrices(hypre_ParCSRMatrix *A_csr, int *indice
    free(proc_offsets2);
    free(exp_indices);
 }
+
+/* -----------------------------------------------------------------------------
+ * extract submatrices of a rectangular matrix
+ * ----------------------------------------------------------------------------- */
+
+void hypre_ParCSRMatrixExtractRowSubmatrices(hypre_ParCSRMatrix *A_csr, int *indices2,
+                                             hypre_ParCSRMatrix ***submatrices)
+{
+   int    nindices, *indices, nrows_A, *A_diag_i, *A_diag_j, mypid, nprocs;
+   int    i, j, k, *proc_offsets1, *proc_offsets2, *itmp_array, *exp_indices;
+   int    nnz11, nnz21, col, ncols_offd, nnz_offd, nnz_diag, *A_offd_i, *A_offd_j;
+   int    global_nrows, global_ncols, *row_starts, *col_starts, nrows, nnz;
+   int    *diag_i, *diag_j, row, *offd_i, *offd_j, nnz11_offd, nnz21_offd;
+   double *A_diag_a, *diag_a, *A_offd_a, *offd_a;
+   hypre_ParCSRMatrix *A11_csr, *A21_csr;
+   hypre_CSRMatrix    *A_diag, *diag, *A_offd, *offd;
+   MPI_Comm           comm;
+
+   /* -----------------------------------------------------
+    * first make sure the incoming indices are in order
+    * ----------------------------------------------------- */
+
+   nindices = indices2[0];
+   indices  = &(indices2[1]);
+   qsort0(indices, 0, nindices-1);
+
+   /* -----------------------------------------------------
+    * fetch matrix information
+    * ----------------------------------------------------- */
+
+   nrows_A = hypre_ParCSRMatrixGlobalNumRows(A_csr);
+   A_diag = hypre_ParCSRMatrixDiag(A_csr);
+   A_diag_i = hypre_CSRMatrixI(A_diag);
+   A_diag_j = hypre_CSRMatrixJ(A_diag);
+   A_diag_a = hypre_CSRMatrixData(A_diag);
+   A_offd = hypre_ParCSRMatrixOffd(A_csr);
+   A_offd_i = hypre_CSRMatrixI(A_offd);
+   A_offd_j = hypre_CSRMatrixJ(A_offd);
+   A_offd_a = hypre_CSRMatrixData(A_offd);
+   comm = hypre_ParCSRMatrixComm(A_csr);
+   MPI_Comm_rank(comm, &mypid);
+   MPI_Comm_size(comm, &nprocs);
+
+   /* -----------------------------------------------------
+    * compute new matrix dimensions
+    * ----------------------------------------------------- */
+
+   proc_offsets1 = (int *) malloc((nprocs+1) * sizeof(int));
+   proc_offsets2 = (int *) malloc((nprocs+1) * sizeof(int));
+   MPI_Allgather(&nindices, 1, MPI_INT, proc_offsets1, 1, MPI_INT, comm);
+   k = 0;
+   for (i = 0; i < nprocs; i++) 
+   {
+      j = proc_offsets1[i];
+      proc_offsets1[i] = k;
+      k += j;
+   } 
+   proc_offsets1[nprocs] = k;
+   itmp_array = hypre_ParCSRMatrixRowStarts(A_csr);
+   for (i = 0; i <= nprocs; i++) 
+      proc_offsets2[i] = itmp_array[i] - proc_offsets1[i];
+
+   /* -----------------------------------------------------
+    * assign id's to row and col for later processing
+    * ----------------------------------------------------- */
+
+   exp_indices = (int *) malloc(nrows_A * sizeof(int));
+   for (i = 0; i < nrows_A; i++) exp_indices[i] = -1;
+   for (i = 0; i < nindices; i++) 
+   {
+      if (exp_indices[indices[i]] == -1) exp_indices[indices[i]] = i;
+      else
+      {
+         printf("ExtractRowSubmatrices: wrong index %d %d\n", i, indices[i]);
+         exit(1);
+      }
+   }
+   k = 0;
+   for (i = 0; i < nrows_A; i++) 
+   {
+      if (exp_indices[i] < 0)
+      {
+         exp_indices[i] = - k - 1;
+         k++;
+      }
+   }
+
+   /* -----------------------------------------------------
+    * compute number of nonzeros for each block
+    * ----------------------------------------------------- */
+
+   nnz11 = nnz21 = nnz11_offd = nnz21_offd = 0;
+   for (i = 0; i < nrows_A; i++)
+   {
+      if (exp_indices[i] >= 0)
+      {
+         for (j = A_diag_i[i]; j < A_diag_i[i+1]; j++)
+         {
+            col = A_diag_j[j];
+            if (exp_indices[col] >= 0) nnz11++;
+         }
+         nnz11_offd += A_offd_i[i+1] - A_offd_i[i];
+      }
+      else
+      {
+         for (j = A_diag_i[i]; j < A_diag_i[i+1]; j++)
+         {
+            col = A_diag_j[j];
+            if (exp_indices[col] < 0) nnz21++;
+         }
+         nnz21_offd += A_offd_i[i+1] - A_offd_i[i];
+      }
+   }
+
+   /* -----------------------------------------------------
+    * create A11 matrix (assume sequential for the moment)
+    * ----------------------------------------------------- */
+
+   ncols_offd = hypre_CSRMatrixNumCols(hypre_ParCSRMatrixDiag(A_csr));
+   nnz_diag   = nnz11;
+   nnz_offd   = nnz11_offd; 
+
+   global_nrows = proc_offsets1[nprocs];
+   itmp_array   = hypre_ParCSRMatrixColStarts(A_csr);
+   global_ncols = itmp_array[nprocs];
+   row_starts = hypre_CTAlloc(int, nprocs+1);
+   col_starts = hypre_CTAlloc(int, nprocs+1);
+   for (i = 0; i <= nprocs; i++)
+   {
+      row_starts[i] = proc_offsets1[i];
+      col_starts[i] = itmp_array[i];
+   }
+   A11_csr = hypre_ParCSRMatrixCreate(comm, global_nrows, global_ncols,
+                    row_starts, col_starts, ncols_offd, nnz_diag, nnz_offd); 
+   nrows = nindices;
+   diag_i = hypre_CTAlloc(int, nrows+1);
+   diag_j = hypre_CTAlloc(int, nnz_diag);
+   diag_a = hypre_CTAlloc(double, nnz_diag);
+   nnz = 0;
+   row = 0;
+   diag_i[0] = 0;
+   for (i = 0; i < nrows_A; i++)
+   {
+      if (exp_indices[i] >= 0)
+      {
+         for (j = A_diag_i[i]; j < A_diag_i[i+1]; j++)
+         {
+            col = A_diag_j[j];
+            if (exp_indices[col] >= 0)
+            {
+               diag_j[nnz] = exp_indices[col];
+               diag_a[nnz++] = A_diag_a[j];
+            }
+         }
+         row++;
+         diag_i[row] = nnz;
+      }
+   }
+   diag = hypre_ParCSRMatrixDiag(A11_csr);
+   hypre_CSRMatrixI(diag) = diag_i;
+   hypre_CSRMatrixJ(diag) = diag_j;
+   hypre_CSRMatrixData(diag) = diag_a;
+
+   offd_i = hypre_CTAlloc(int, nrows+1);
+   offd_j = hypre_CTAlloc(int, nnz_offd);
+   offd_a = hypre_CTAlloc(double, nnz_offd);
+   nnz = 0;
+   row = 0;
+   offd_i[0] = 0;
+   for (i = 0; i < nrows_A; i++)
+   {
+      if (exp_indices[i] >= 0)
+      {
+         for (j = A_offd_i[i]; j < A_offd_i[i+1]; j++)
+         {
+            offd_j[nnz] = A_offd_j[j];
+            offd_a[nnz++] = A_diag_a[j];
+         }
+         row++;
+         offd_i[row] = nnz;
+      }
+   }
+   offd = hypre_ParCSRMatrixOffd(A11_csr);
+   hypre_CSRMatrixI(offd) = offd_i;
+   hypre_CSRMatrixJ(offd) = offd_j;
+   hypre_CSRMatrixData(offd) = offd_a;
+
+   /* -----------------------------------------------------
+    * create A21 matrix
+    * ----------------------------------------------------- */
+
+   ncols_offd = hypre_CSRMatrixNumCols(hypre_ParCSRMatrixDiag(A_csr));
+   nnz_offd   = nnz21_offd;
+   nnz_diag   = nnz21;
+   global_nrows = proc_offsets2[nprocs];
+   itmp_array   = hypre_ParCSRMatrixColStarts(A_csr);
+   global_ncols = itmp_array[nprocs];
+   row_starts = hypre_CTAlloc(int, nprocs+1);
+   col_starts = hypre_CTAlloc(int, nprocs+1);
+   for (i = 0; i <= nprocs; i++)
+   {
+      row_starts[i] = proc_offsets2[i];
+      col_starts[i] = itmp_array[i];
+   }
+   A21_csr = hypre_ParCSRMatrixCreate(comm, global_nrows, global_ncols,
+                    row_starts, col_starts, ncols_offd, nnz_diag, nnz_offd); 
+   nrows = nrows_A - nindices;
+   diag_i = hypre_CTAlloc(int, nrows+1);
+   diag_j = hypre_CTAlloc(int, nnz_diag);
+   diag_a = hypre_CTAlloc(double, nnz_diag);
+   nnz = 0;
+   row = 0;
+   diag_i[0] = 0;
+   for (i = 0; i < nrows_A; i++)
+   {
+      if (exp_indices[i] < 0)
+      {
+         for (j = A_diag_i[i]; j < A_diag_i[i+1]; j++)
+         {
+            diag_j[nnz] = A_diag_j[j];
+            diag_a[nnz++] = A_diag_a[j];
+         }
+         row++;
+         diag_i[row] = nnz;
+      }
+   }
+   diag = hypre_ParCSRMatrixDiag(A21_csr);
+   hypre_CSRMatrixI(diag) = diag_i;
+   hypre_CSRMatrixJ(diag) = diag_j;
+   hypre_CSRMatrixData(diag) = diag_a;
+
+   offd_i = hypre_CTAlloc(int, nrows+1);
+   offd_j = hypre_CTAlloc(int, nnz_offd);
+   offd_a = hypre_CTAlloc(double, nnz_offd);
+   nnz = 0;
+   row = 0;
+   offd_i[0] = 0;
+   for (i = 0; i < nrows_A; i++)
+   {
+      if (exp_indices[i] < 0)
+      {
+         for (j = A_offd_i[i]; j < A_offd_i[i+1]; j++)
+         {
+            offd_j[nnz] = A_offd_j[j];
+            offd_a[nnz++] = A_diag_a[j];
+         }
+         row++;
+         offd_i[row] = nnz;
+      }
+   }
+   offd = hypre_ParCSRMatrixOffd(A21_csr);
+   hypre_CSRMatrixI(offd) = offd_i;
+   hypre_CSRMatrixJ(offd) = offd_j;
+   hypre_CSRMatrixData(offd) = offd_a;
+
+   /* -----------------------------------------------------
+    * hand the matrices back to the caller and clean up 
+    * ----------------------------------------------------- */
+
+   (*submatrices)[0] = A11_csr;
+   (*submatrices)[1] = A21_csr;
+   free(proc_offsets1);
+   free(proc_offsets2);
+   free(exp_indices);
+}
+
