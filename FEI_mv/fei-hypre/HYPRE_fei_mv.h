f52f2d3a3a3aa1708e76051b5b453a63932017c9
chtong <chtong>
2006-08-16 00:21:51 +0000

    Add standard interface.

---
+++ FEI_mv/fei-hypre/HYPRE_fei_mv.h
@@ -0,0 +1,246 @@
+/*BHEADER**********************************************************************
+ * Copyright (c) 2006   The Regents of the University of California.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Written by the HYPRE team <hypre-users@llnl.gov>, UCRL-CODE-222953.
+ * All rights reserved.
+ *
+ * This file is part of HYPRE (see http://www.llnl.gov/CASC/hypre/).
+ * Please see the COPYRIGHT_and_LICENSE file for the copyright notice, 
+ * disclaimer and the GNU Lesser General Public License.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License (as published by the Free
+ * Software Foundation) version 2.1 dated February 1999.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Revision$
+ **********************************************************************EHEADER*/
+
+#ifndef HYPRE_FE_MV_HEADER
+#define HYPRE_FE_MV_HEADER
+
+#include "HYPRE_utilities.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------*/
+
+/**
+ * @name FEI System Interface
+ *
+ * This interface represents a FE conceptual view of a
+ * linear system.  
+ *
+ * @memo A FE conceptual interface
+ **/
+/*@{*/
+
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------*/
+
+/**
+ * @name FE Mesh
+ **/
+/*@{*/
+
+struct hypre_FEMesh_struct;
+typedef struct hypre_FEMesh_struct *HYPRE_FEMesh;
+
+/**
+ * Create a FE Mesh object.  
+ **/
+
+int HYPRE_FEMeshCreate(MPI_Comm comm, HYPRE_FEMesh *mesh);
+
+/**
+ * Destroy an FE Mesh object.  An object should be explicitly destroyed
+ * using this destructor when the user's code no longer needs direct
+ * access to it.  
+ **/
+
+int HYPRE_FEMeshDestroy(HYPRE_FEMesh mesh);
+
+/**
+ * initialize all fields in the finite element mesh
+ **/
+
+int HYPRE_FEMeshInitFields(HYPRE_FEMesh mesh, int numFields, 
+                           int *fieldSizes, int *fieldIDs);
+
+/**
+ * initialize an element block
+ **/
+
+int HYPRE_FEMeshInitElemBlock(HYPRE_FEMesh mesh, int blockID, int nElements,
+                int numNodesPerElement, int *numFieldsPerNode,
+                int **nodalFieldIDs, int numElemDOFFieldsPerElement,
+                int *elemDOFFieldIDs, int interleaveStrategy);
+
+/**
+ * initialize the connectivity of a given element
+ **/
+
+int HYPRE_FEMeshInitElem(HYPRE_FEMesh mesh, int blockID, int elemID,
+                         *elemConn);
+
+/**
+ * initialize the shared nodes between processors
+ **/
+
+int HYPRE_FEMeshInitSharedNodes(HYPRE_FEMesh mesh, int nShared,
+                                int *sharedIDs, int *sharedLeng,
+                                int **sharedProcs);
+
+/**
+ * initialization complete
+ **/
+
+int HYPRE_FEMeshInitComplete(HYPRE_FEMesh mesh);
+
+/**
+ * load node boundary conditions
+ **/
+
+int HYPRE_FEMeshLoadNodeBCs(HYPRE_FEMesh mesh, int numNodes,
+                            int *nodeIDs, int fieldID, double **alpha,
+                            double **beta, double **gamma);
+
+/*@}*/
+
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------*/
+
+/**
+ * @name FE Matrices
+ **/
+/*@{*/
+
+struct hypre_FEMatrix_struct;
+/**
+ * The matrix object
+ **/
+typedef struct hypre_FEMatrix_struct *HYPRE_FEMatrix;
+
+/**
+ * create a new FE matrix
+ **/
+
+int HYPRE_FEMatrixCreate(MPI_Comm comm, HYPRE_FEMesh mesh, 
+                         HYPRE_FEMatrix *matrix);
+
+/**
+ * destroy a new FE matrix
+ **/
+
+int HYPRE_FEMatrixDestroy(HYPRE_FEMatrix matrix);
+   
+/**
+ * prepare a matrix object for setting coefficient values
+ **/
+
+int HYPRE_FEMatrixInitialize(HYPRE_FEMatrix matrix);
+   
+/**
+ * sum the given element stiffness matrix into the global stiffness matrix 
+ **/
+
+int HYPRE_FEMatrixSumInElemMatrix(HYPRE_FEMesh mesh, int blockID, 
+            int elemID, int length, double **elemStiff, int elemFormat);
+   
+/**
+ * signal that loading has been completed
+ **/
+
+int HYPRE_FEMatrixAssemble(HYPRE_FEMatrix matrix);
+   
+/**
+ * Set the storage type of the matrix object to be constructed.
+ * Currently, {\tt type} can only be {\tt HYPRE\_PARCSR} (default).
+ *
+ **/
+int HYPRE_FEMatrixSetObjectType(HYPRE_FEMatrix  matrix, int type);
+
+/**
+ * Get a reference to the constructed matrix object.
+ *
+ * @see HYPRE_FEMatrixSetObjectType
+ **/
+int HYPRE_FEMatrixGetObject(HYPRE_FEMatrix matrix, void **object);
+
+/*@}*/
+
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------*/
+
+/**
+ * @name FE Vectors
+ **/
+/*@{*/
+
+struct hypre_FEVector_struct;
+/**
+ * The vector object.
+ **/
+typedef struct hypre_FEVector_struct *HYPRE_FEVector;
+
+/**
+ * Create a vector object.
+ **/
+int HYPRE_FEVectorCreate(MPI_Comm comm, HYPRE_FEMesh mesh,
+                         HYPRE_FEVector  *vector);
+
+/**
+ * Destroy a vector object.
+ **/
+int HYPRE_FEVectorDestroy(HYPRE_FEVector vector);
+
+/**
+ * Prepare a vector object for setting coefficient values.
+ **/
+int HYPRE_FEVectorInitialize(HYPRE_FEVector vector);
+
+/**
+ * Set vector coefficients element by element.
+ **/
+int HYPRE_FEVectorSetValues(HYPRE_FEVector vector, int elemID,
+                            int length, double *elemLoad)l
+
+/**
+ * Finalize the construction of the vector before using.
+ **/
+int HYPRE_FEVectorAssemble(HYPRE_FEVector vector);
+
+/**
+ * Set the storage type of the vector object to be constructed.
+ * Currently, {\tt type} can only be {\tt HYPRE\_PARCSR} (default).
+ **/
+int HYPRE_FEVectorSetObjectType(HYPRE_FEVector vector, int type);
+
+/**
+ * Get a reference to the constructed vector object.
+ **/
+int HYPRE_FEVectorGetObject(HYPRE_FEVector vector, void **object);
+
+/*@}*/
+/*@}*/
+
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ FEI_mv/fei-hypre/fei_mv.h
@@ -0,0 +1,179 @@
+/*BHEADER**********************************************************************
+ * Copyright (c) 2006   The Regents of the University of California.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Written by the HYPRE team <hypre-users@llnl.gov>, UCRL-CODE-222953.
+ * All rights reserved.
+ *
+ * This file is part of HYPRE (see http://www.llnl.gov/CASC/hypre/).
+ * Please see the COPYRIGHT_and_LICENSE file for the copyright notice, 
+ * disclaimer and the GNU Lesser General Public License.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License (as published by the Free
+ * Software Foundation) version 2.1 dated February 1999.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Revision$
+ **********************************************************************EHEADER*/
+
+#include "utilities.h"
+
+#include "HYPRE_fei_mv.h"
+
+#ifndef hypre_FE_MV_HEADER
+#define hypre_FE_MV_HEADER
+
+#include "HYPRE.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************
+ *
+ * Header info for the hypre_SStructGrid structures
+ *
+ *****************************************************************************/
+
+#ifndef hypre_FE_MESH_HEADER
+#define hypre_FE_MESH_HEADER
+
+/*--------------------------------------------------------------------------
+ * hypre_FEMesh:
+ *--------------------------------------------------------------------------*/
+
+struct hypre_FEMesh_struct
+{
+   MPI_Comm                   comm;
+   int                        ndim;
+   int                        nparts;
+
+} hypre_FEMesh;
+
+/*--------------------------------------------------------------------------
+ * Accessor macros: hypre_SStructGrid
+ *--------------------------------------------------------------------------*/
+
+#define hypre_FEMeshComm(grid)           ((grid) -> comm)
+#define hypre_FEMeshLocalSize(grid)      ((grid) -> local_size)
+#define hypre_FEMeshGlobalSize(grid)     ((grid) -> global_size)
+
+#endif
+
+/******************************************************************************
+ *
+ * Header info for the hypre_FEMatrix structures
+ *
+ *****************************************************************************/
+
+#ifndef hypre_FE_MATRIX_HEADER
+#define hypre_FE_MATRIX_HEADER
+
+/*--------------------------------------------------------------------------
+ * hypre_FEMatrix:
+ *--------------------------------------------------------------------------*/
+
+typedef struct
+{
+   MPI_Comm         comm;
+   hypre_FEMesh     *femesh;
+
+} hypre_FEMatrix;
+
+/*--------------------------------------------------------------------------
+ * Accessor macros: hypre_FEMatrix
+ *--------------------------------------------------------------------------*/
+
+#define hypre_FEMatrixComm(mat)           ((mat) -> comm)
+#define hypre_FEMatrixNDim(mat)           ((mat) -> ndim)
+#define hypre_FEMatrixGraph(mat)          ((mat) -> graph)
+#define hypre_FEMatrixSplits(mat)         ((mat) -> splits)
+#define hypre_FEMatrixParCSRMatrix(mat)   ((mat) -> parcsrmatrix)
+#define hypre_FEMatrixGlobalSize(mat)     ((mat) -> global_size)
+#define hypre_FEMatrixObjectType(mat)     ((mat) -> object_type)
+
+/******************************************************************************
+ *
+ * Header info for the hypre_FEVector structures
+ *
+ *****************************************************************************/
+
+#ifndef hypre_FE_VECTOR_HEADER
+#define hypre_FE_VECTOR_HEADER
+
+/*--------------------------------------------------------------------------
+ * hypre_FEVector:
+ *--------------------------------------------------------------------------*/
+
+typedef struct
+{
+   MPI_Comm       comm;
+   hypre_FEMesh  *mesh;
+
+} hypre_FEVector;
+
+/*--------------------------------------------------------------------------
+ * Accessor macros: hypre_FEVector
+ *--------------------------------------------------------------------------*/
+
+#define hypre_FEVectorComm(vec)           ((vec) -> comm)
+#define hypre_FEVectorMesh(vec)           ((vec) -> mesh)
+#define hypre_FEVectorObjectType(vec)     ((vec) -> object_type)
+                                                                                                            
+/* HYPRE_fei_mv.cxx */
+int HYPRE_FEMeshCreate( MPI_Comm comm, HYPRE_FEMesh *mesh_ptr );
+int HYPRE_FEMeshDestroy( HYPRE_SStructGrid grid );
+int HYPRE_FEMeshAssemble( HYPRE_SStructGrid grid );
+int HYPRE_FEMeshInitFields( HYPRE_FEMesh mesh, int numFields,
+                            int *fieldSizes, int *fieldIDs );
+int HYPRE_FEMeshInitElemBlock( HYPRE_FEMesh mesh, int blockID, 
+                               int nElements, int numNodesPerElement,
+                               int *numFieldsPerNode, int **nodalFieldIDs,
+                               int numElemDOFFieldsPerElement,
+                               int *elemDOFFieldIDs, int interleaveStrategy );
+int HYPRE_FEMeshInitElem( HYPRE_FEMesh mesh, int blockID, int elemID,
+                          *elemConn );
+int HYPRE_FEMeshInitSharedNodes( HYPRE_FEMesh mesh, int nShared,
+                                 int *sharedIDs, int *sharedLeng,
+                                 int **sharedProcs );
+int HYPRE_FEMeshInitComplete( HYPRE_FEMesh mesh );
+int HYPRE_FEMeshLoadNodeBCs( HYPRE_FEMesh mesh, int numNodes,
+                             int *nodeIDs, int fieldID, double **alpha,
+                             double **beta, double **gamma );
+
+/* HYPRE_fei_mv.cxx */
+int HYPRE_FEMatrixCreate( MPI_Comm comm, HYPRE_FEMesh mesh, 
+                          HYPRE_FEMatrix *matrix_ptr );
+int HYPRE_FEMatrixDestroy ( HYPRE_FEMatrix matrix );
+int HYPRE_FEMatrixInitialize ( HYPRE_FEMatrix matrix );
+int HYPRE_FEMatrixSetValues ( HYPRE_FEMatrix matrix , int blockID,
+          int elemID, int length, double **elemStiff, int elemFormat);
+int HYPRE_FEMatrixAssemble ( HYPRE_FEMatrix matrix );
+int HYPRE_FEMatrixSetObjectType ( HYPRE_FEMatrix vector, int type );
+int HYPRE_FEMatrixGetObject ( HYPRE_FEMatrix vector, void **object );
+
+/* HYPRE_fei_mv.cxx */
+int HYPRE_FEVectorCreate( MPI_Comm comm , HYPRE_FEMesh mesh, 
+                          HYPRE_FEVector *vector_ptr );
+int HYPRE_FEVectorDestroy ( HYPRE_FEVector vector );
+int HYPRE_FEVectorInitialize ( HYPRE_FEVector vector );
+int HYPRE_FEVectorSetValues ( HYPRE_FEVector vector, int elemID, 
+                              int length , double *elemLoad );
+int HYPRE_FEVectorAssemble ( HYPRE_FEVector vector );
+int HYPRE_FEVectorSetObjectType ( HYPRE_FEVector vector, int type );
+int HYPRE_FEVectorGetObject ( HYPRE_FEVector vector, void **object );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
