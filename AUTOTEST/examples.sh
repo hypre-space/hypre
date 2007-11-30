df5d687139462e9a20132862a1f1eb885335db77
kolev <kolev>
2007-11-30 22:46:12 +0000

    Added a test for the example codes.

---
+++ AUTOTEST/examples.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#BHEADER**********************************************************************
+# Copyright (c) 2007, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by the HYPRE team. UCRL-CODE-222953.
+# All rights reserved.
+#
+# This file is part of HYPRE (see http://www.llnl.gov/CASC/hypre/).
+# Please see the COPYRIGHT_and_LICENSE file for the copyright notice, 
+# disclaimer, contact information and the GNU Lesser General Public License.
+#
+# HYPRE is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free Software 
+# Foundation) version 2.1 dated February 1999.
+#
+# HYPRE is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE.  See the terms and conditions of the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# $Revision$
+#EHEADER**********************************************************************
+
+testname=`basename $0 .sh`
+
+# Echo usage information
+case $1 in
+   -h|-help)
+cat <<EOF
+
+   $0 [-h] {ex_dir} [options for examples make]
+
+   where: {ex_dir}  is the hypre examples directory
+          -h|-help  prints this usage information and exits
+
+   This script builds the hypre example codes in {ex_dir}.
+
+   Example usage: $0 ../examples
+
+EOF
+   exit
+   ;;
+esac
+
+# Setup
+ex_dir=$1
+shift
+
+# Run make in the examples directory
+cd $ex_dir
+make clean
+make $@
