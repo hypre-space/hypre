4aea20ec3c0ab1565ea954d52f1cc478a0220b2a
kolev <kolev>
2007-11-30 22:45:45 +0000

    Added a Mac test.

---
+++ AUTOTEST/machine-mac.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#BHEADER**********************************************************************
+# Copyright (c) 2007, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by the HYPRE team. UCRL-CODE-222953.
+# All rights reserved.
+#
+# This file is part of HYPRE (see http://www.llnl.gov/CASC/hypre/).
+# Please see the COPYRIGHT_and_LICENSE file for the copyright notice, 
+# disclaimer, contact information and the GNU Lesser General Public License.
+#
+# HYPRE is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free Software 
+# Foundation) version 2.1 dated February 1999.
+#
+# HYPRE is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE.  See the terms and conditions of the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# $Revision$
+#EHEADER**********************************************************************
+
+testname=`basename $0 .sh`
+
+# Echo usage information
+case $1 in
+   -h|-help)
+cat <<EOF
+
+   **** Only run this script on a Mac ****
+
+   $0 [-h|-help] {src_dir}
+
+   where: {src_dir}  is the hypre source directory
+          -h|-help   prints this usage information and exits
+
+   This script runs a number of tests suitable for a Mac.
+
+   Example usage: $0 ..
+
+EOF
+   exit
+   ;;
+esac
+
+# Setup
+test_dir=`pwd`
+output_dir=`pwd`/$testname.dir
+rm -fr $output_dir
+mkdir -p $output_dir
+src_dir=$1
+shift
+
+# Test various builds (last one is the default build)
+configure_opts="--without-MPI"
+for copt in $configure_opts ""
+do
+   ./test.sh configure.sh $src_dir $copt --enable-debug
+   output_subdir=$output_dir/build$copt
+   mkdir -p $output_subdir
+   mv -f configure.??? $output_subdir
+   ./test.sh make.sh $src_dir test
+   mv -f make.??? $output_subdir
+done
+
+# Test link for C++
+./test.sh make.sh $src_dir all++
+mkdir -p link-c++
+mv -f make.??? link-c++
+
+# Test link for Fortran
+./test.sh make.sh $src_dir all77
+mkdir -p link-f77
+mv -f make.??? link-f77
+
+# Test runtest tests
+./test.sh default.sh $src_dir
+mv -f default.??? $output_dir
+
+# Echo to stderr all nonempty error files in $output_dir
+for errfile in $( find $output_dir ! -size 0 -name "*.err" )
+do
+   echo $errfile >&2
+done
