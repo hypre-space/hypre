85e2f6ee01dd45fd9b7cacfb42afde9b376415fd
falgout <falgout>
2010-03-26 19:49:54 +0000

    Added reduced and enhanced connectivity regression tests and merged the 'fe_rhombus' tests with this new one.

--- test/TEST_sstruct/sharedpart.sh
+++ test/TEST_sstruct/sharedpart.sh
@@ -15,11 +15,38 @@
 TNAME=`basename $0 .sh`
 
 #=============================================================================
+#=============================================================================
+
+tail -3 ${TNAME}.out.0 > ${TNAME}.testdata
+
+for i in 1 2 3
+do
+   tail -3 ${TNAME}.out.$i > ${TNAME}.testdata.temp
+   diff ${TNAME}.testdata ${TNAME}.testdata.temp >&2
+done
+
+#=============================================================================
+#=============================================================================
+
+tail -3 ${TNAME}.out.10 > ${TNAME}.testdata
+tail -3 ${TNAME}.out.11 > ${TNAME}.testdata.temp
+diff ${TNAME}.testdata ${TNAME}.testdata.temp >&2
+
+#=============================================================================
 # compare with baseline case
 #=============================================================================
 
 FILES="\
  ${TNAME}.out.0\
+ ${TNAME}.out.1\
+ ${TNAME}.out.2\
+ ${TNAME}.out.3\
+ ${TNAME}.out.10\
+ ${TNAME}.out.11\
+ ${TNAME}.out.50\
+ ${TNAME}.out.51\
+ ${TNAME}.out.52\
+ ${TNAME}.out.53\
 "
 
 for i in $FILES
@@ -44,3 +71,4 @@ fi
 # remove temporary files
 #=============================================================================
 
+rm -f ${TNAME}.testdata*
