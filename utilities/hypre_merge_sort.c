eac2e16713fe3d13d09bfe34daa8dbc7060f280e
Jongsoo Park <jongsoo.park@intel.com>
2015-08-18 15:51:55 -0700

    bug fix

--- utilities/hypre_merge_sort.c
+++ utilities/hypre_merge_sort.c
@@ -1,6 +1,9 @@
 #include "_hypre_utilities.h"
-//#include <assert.h>
-//#include <algorithm>
+//#define DBG_MERGE_SORT
+#ifdef DBG_MERGE_SORT
+#include <assert.h>
+#include <algorithm>
+#endif
 
 #define SWAP(T, a, b) do { T tmp = a; a = b; b = tmp; } while (0)
 
@@ -90,11 +93,13 @@ void kth_element_(
 {
    while (1)
    {
-      //assert(left <= right && right <= k);
       HYPRE_Int i = (left + right)/2; // right < k -> i < k
-      //assert(i < k); // i == k implies left == right == k that can never happen
       HYPRE_Int j = k - i - 1;
-      //assert(j >= 0 && j < n2);
+#ifdef DBG_MERGE_SORT
+      assert(left <= right && right <= k);
+      assert(i < k); // i == k implies left == right == k that can never happen
+      assert(j >= 0 && j < n2);
+#endif
 
       if ((j == -1 || a1[i] >= a2[j]) && (j == n2 - 1 || a1[i] <= a2[j + 1]))
       {
@@ -193,7 +198,9 @@ void kth_element(
       *out1 += offset1;
       *out2 += offset2;
    }
-   //assert(*out1 + *out2 == k);
+#ifdef DBG_MERGE_SORT
+   assert(*out1 + *out2 == k);
+#endif
 }
 
 /**
@@ -218,8 +225,10 @@ HYPRE_Int hypre_parallel_merge_unique(
    HYPRE_Int begin_rank = hypre_min(n_per_thread*my_thread_num, n);
    HYPRE_Int end_rank = hypre_min(begin_rank + n_per_thread, n);
 
-   //int *buf = new int[n];
-   //std::merge(first1, last1, first2, last2, buf);
+#ifdef DBG_MERGE_SORT
+   int *dbg_buf = new int[n];
+   std::merge(first1, last1, first2, last2, dbg_buf);
+#endif
 
    HYPRE_Int begin1, begin2, end1, end2;
    kth_element(&begin1, &begin2, first1, first2, n1, n2, begin_rank);
@@ -230,8 +239,10 @@ HYPRE_Int hypre_parallel_merge_unique(
       first2 + begin2, first2 + end2,
       temp + begin1 + begin2) - (temp + begin1 + begin2);
 
-   //assert(std::is_sorted(temp + begin1 + begin2, temp + begin1 + begin2 + out_len));
-   //assert(std::adjacent_find(temp + begin1 + begin2, temp + begin1 + begin2 + out_len) == temp + begin1 + begin2 + out_len);
+#ifdef DBG_MERGE_SORT
+   assert(std::is_sorted(temp + begin1 + begin2, temp + begin1 + begin2 + out_len));
+   assert(std::adjacent_find(temp + begin1 + begin2, temp + begin1 + begin2 + out_len) == temp + begin1 + begin2 + out_len);
+#endif
 
    prefix_sum_workspace[my_thread_num] = out_len;
 
@@ -275,21 +286,25 @@ HYPRE_Int hypre_parallel_merge_unique(
       out[i + out_begin] = temp[i + begin_rank];
    }
 
-   //assert(std::is_sorted(out + out_begin, out + out_end));
-   //assert(std::adjacent_find(out + out_begin, out + out_end) == out + out_end);
+#ifdef DBG_MERGE_SORT
+   assert(std::is_sorted(out + out_begin, out + out_end));
+   assert(std::adjacent_find(out + out_begin, out + out_end) == out + out_end);
 
-/*#pragma omp barrier
+#pragma omp barrier
 
    if (0 == my_thread_num)
    {
       assert(std::is_sorted(out, out + prefix_sum_workspace[num_threads - 1]));
       assert(std::adjacent_find(out, out + prefix_sum_workspace[num_threads - 1]) == out + prefix_sum_workspace[num_threads - 1]);
-      int *buf_last = std::unique(buf, buf + n);
-      assert(buf_last - buf == prefix_sum_workspace[num_threads - 1]);
-      assert(std::equal(out, out + prefix_sum_workspace[num_threads - 1], buf));
+      int ref_len = std::unique(dbg_buf, dbg_buf + n) - dbg_buf;
+      assert(ref_len == prefix_sum_workspace[num_threads - 1]);
+      assert(std::equal(out, out + prefix_sum_workspace[num_threads - 1], dbg_buf));
+
+      delete[] dbg_buf;
    }
 
-#pragma omp barrier*/
+#pragma omp barrier
+#endif
 
    return prefix_sum_workspace[num_threads - 1];
 }
@@ -310,8 +325,10 @@ void hypre_parallel_merge(
    HYPRE_Int begin_rank = hypre_min(n_per_thread*my_thread_num, n);
    HYPRE_Int end_rank = hypre_min(begin_rank + n_per_thread, n);
 
-   //assert(std::is_sorted(first1, last1));
-   //assert(std::is_sorted(first2, last2));
+#ifdef DBG_MERGE_SORT
+   assert(std::is_sorted(first1, last1));
+   assert(std::is_sorted(first2, last2));
+#endif
 
    HYPRE_Int begin1, begin2, end1, end2;
    kth_element(&begin1, &begin2, first1, first2, n1, n2, begin_rank);
@@ -322,7 +339,9 @@ void hypre_parallel_merge(
       first2 + begin2, first2 + end2,
       out + begin1 + begin2);
 
-   //assert(std::is_sorted(out + begin1 + begin2, out + end1 + end2));
+#ifdef DBG_MERGE_SORT
+   assert(std::is_sorted(out + begin1 + begin2, out + end1 + end2));
+#endif
 }
 
 /**
@@ -331,6 +350,12 @@ void hypre_parallel_merge(
 HYPRE_Int hypre_merge_sort_unique2(HYPRE_Int *in, HYPRE_Int *temp, HYPRE_Int len, HYPRE_Int **out)
 {
    if (0 == len) return 0;
+
+#ifdef DBG_MERGE_SORT
+   int *dbg_buf = new int[len];
+   std::copy(in, in + len, dbg_buf);
+   std::sort(dbg_buf, dbg_buf + len);
+#endif
 
    HYPRE_Int thread_private_len[hypre_NumThreads()];
    HYPRE_Int out_len = 0;
@@ -349,58 +374,87 @@ HYPRE_Int hypre_merge_sort_unique2(HYPRE_Int *in, HYPRE_Int *temp, HYPRE_Int len
 
       qsort0(in, i_begin, i_end - 1);
 
-      // merge sorted sequences
-      HYPRE_Int in_group_size;
-      HYPRE_Int *in_buf = in;
-      HYPRE_Int *out_buf = temp;
-      for (in_group_size = 1; in_group_size < num_threads; in_group_size *= 2)
+      if (1 == num_threads)
       {
+         // when there's only one thread, we need to eliminate duplicates separately
+         if (len)
+         {
+            out_len = 1;
+
+            HYPRE_Int i;
+            for (i = 1; i < len; i++)
+            {
+               if (in[i] != in[i - 1]) in[out_len++] = in[i];
+            }
+         }
+
+         *out = in;
+      }
+      else
+      {
+         // merge sorted sequences
+         HYPRE_Int in_group_size;
+         HYPRE_Int *in_buf = in;
+         HYPRE_Int *out_buf = temp;
+         for (in_group_size = 1; in_group_size < num_threads; in_group_size *= 2)
+         {
 #ifdef HYPRE_USING_OPENMP
 #pragma omp barrier
 #endif
 
-         // merge 2 in-groups into 1 out-group
-         HYPRE_Int out_group_size = in_group_size*2;
-         HYPRE_Int group_leader = my_thread_num/out_group_size*out_group_size;
-         HYPRE_Int group_sub_leader = hypre_min(group_leader + in_group_size, num_threads - 1);
-         HYPRE_Int id_in_group = my_thread_num%out_group_size;
-         HYPRE_Int num_threads_in_group =
-            hypre_min(group_leader + out_group_size, num_threads) - group_leader;
+            // merge 2 in-groups into 1 out-group
+            HYPRE_Int out_group_size = in_group_size*2;
+            HYPRE_Int group_leader = my_thread_num/out_group_size*out_group_size;
+            HYPRE_Int group_sub_leader = hypre_min(group_leader + in_group_size, num_threads - 1);
+            HYPRE_Int id_in_group = my_thread_num%out_group_size;
+            HYPRE_Int num_threads_in_group =
+               hypre_min(group_leader + out_group_size, num_threads) - group_leader;
 
-         HYPRE_Int in_group1_begin = hypre_min(i_per_thread*group_leader, len);
-         HYPRE_Int in_group1_end = hypre_min(in_group1_begin + i_per_thread*in_group_size, len);
+            HYPRE_Int in_group1_begin = hypre_min(i_per_thread*group_leader, len);
+            HYPRE_Int in_group1_end = hypre_min(in_group1_begin + i_per_thread*in_group_size, len);
 
-         HYPRE_Int in_group2_begin = hypre_min(in_group1_begin + i_per_thread*in_group_size, len);
-         HYPRE_Int in_group2_end = hypre_min(in_group2_begin + i_per_thread*in_group_size, len);
+            HYPRE_Int in_group2_begin = hypre_min(in_group1_begin + i_per_thread*in_group_size, len);
+            HYPRE_Int in_group2_end = hypre_min(in_group2_begin + i_per_thread*in_group_size, len);
 
-         if (out_group_size < num_threads)
-         {
-            hypre_parallel_merge(
-               in_buf + in_group1_begin, in_buf + in_group1_end,
-               in_buf + in_group2_begin, in_buf + in_group2_end,
-               out_buf + in_group1_begin,
-               num_threads_in_group,
-               id_in_group);
+            if (out_group_size < num_threads)
+            {
+               hypre_parallel_merge(
+                  in_buf + in_group1_begin, in_buf + in_group1_end,
+                  in_buf + in_group2_begin, in_buf + in_group2_end,
+                  out_buf + in_group1_begin,
+                  num_threads_in_group,
+                  id_in_group);
+            }
+            else
+            {
+               HYPRE_Int len = hypre_parallel_merge_unique(
+                  in_buf + in_group1_begin, in_buf + in_group1_end,
+                  in_buf + in_group2_begin, in_buf + in_group2_end,
+                  out_buf + in_group1_begin,
+                  in_buf + in_group1_begin,
+                  num_threads_in_group,
+                  id_in_group,
+                  thread_private_len + group_leader);
+
+               if (0 == my_thread_num) out_len = len;
+            }
+
+            HYPRE_Int *temp = in_buf;
+            in_buf = out_buf;
+            out_buf = temp;
          }
-         else
-         {
-            out_len = hypre_parallel_merge_unique(
-               in_buf + in_group1_begin, in_buf + in_group1_end,
-               in_buf + in_group2_begin, in_buf + in_group2_end,
-               out_buf + in_group1_begin,
-               in_buf + in_group1_begin,
-               num_threads_in_group,
-               id_in_group,
-               thread_private_len + group_leader);
-         }
 
-         HYPRE_Int *temp = in_buf;
-         in_buf = out_buf;
-         out_buf = temp;
+         *out = out_buf;
       }
-
-      *out = out_buf;
    } /* omp parallel */
+
+#ifdef DBG_MERGE_SORT
+   int ref_len = std::unique(dbg_buf, dbg_buf + len) - dbg_buf;
+   assert(ref_len == out_len);
+   assert(std::equal(*out, *out + out_len, dbg_buf));
+
+   delete[] dbg_buf;
+#endif
 
    return out_len;
 }
