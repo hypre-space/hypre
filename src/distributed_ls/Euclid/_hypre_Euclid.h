4d6b4ec98cd2d9c7270ad99bed985824333e784a
Rob Falgout <rfalgout@llnl.gov>
2017-05-02 13:19:38 -0700

    Fixed incorrect use of HYPRE_USE_MANAGED in a few places

--- src/distributed_ls/Euclid/_hypre_Euclid.h
+++ src/distributed_ls/Euclid/_hypre_Euclid.h
@@ -481,7 +481,7 @@ extern void  printErrorMsg(FILE *fp);
 #endif
 
 #define MSG_BUF_SIZE_DH MAX(1024, hypre_MPI_MAX_ERROR_STRING)
-#if defined(HYPRE_MEMORY_GPU) || defined(HYPRE_USE_RAJA) || defined(HYPRE_USE_KOKKOS) || defined(HYPRE_USE_MANAGED)
+#if defined(HYPRE_MEMORY_GPU) || defined(HYPRE_USE_RAJA) || defined(HYPRE_USE_KOKKOS) || defined(HYPRE_USE_CUDA)
 static char  msgBuf_dh[MSG_BUF_SIZE_DH];
 #else
 extern char  msgBuf_dh[MSG_BUF_SIZE_DH];
