683f3e1dc4779139e90fa3c4ec5f5d8fa3f2c64e
Rob Falgout <rfalgout@llnl.gov>
2017-05-02 08:33:58 -0700

    Fixed a minor timing header file mistake

--- src/utilities/_hypre_utilities.h
+++ src/utilities/_hypre_utilities.h
@@ -753,10 +753,12 @@ HYPRE_Real time_get_cpu_seconds_( void );
 #ifndef HYPRE_TIMING
 
 #define hypre_InitializeTiming(name) 0
+#define hypre_FinalizeTiming(index)
 #define hypre_IncFLOPCount(inc)
 #define hypre_BeginTiming(i)
 #define hypre_EndTiming(i)
 #define hypre_PrintTiming(heading, comm)
+#define hypre_ClearTiming()
 #define hypre_FinalizeTiming(index)
 
 /*--------------------------------------------------------------------------
