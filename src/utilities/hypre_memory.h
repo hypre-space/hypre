83fdc171c360044341ccec0d796138ef856d4f93
Wang, Lu <wang84@llnl.gov>
2017-08-01 10:34:39 -0700

    add the draft of the macros for the new memory model

--- src/utilities/hypre_memory.h
+++ src/utilities/hypre_memory.h
@@ -17,28 +17,56 @@
  * The abstract memory model has a Host (think CPU) and a Device (think GPU) and
  * three basic types of memory management utilities:
  *
- *    1. Malloc       - This memory is available only on the Host
- *    2. DeviceMalloc - This memory is available only on the Device
- *    3. MemCopy      - Copies memory to/from the Device
+ *    1. Malloc(..., location) 
+ *             location=LOCATION_DEVICE - malloc memory on the device
+ *             location=LOCATION_HOST   - malloc memory on the host
+ *    2. MemCopy(..., method)
+ *             method=HOST_TO_DEVICE    - copy from host to device
+ *             method=DEVICE_TO_HOST    - copy from device to host
+ *             method=DEVICE_TO_DEVICE  - copy from device to device
+ *    3. SetExecutionMode
+ *             location=LOCATION_DEVICE - execute on the device
+ *             location=LOCATION_HOST   - execute on the host
  *
  * Although the abstract model does not explicitly reflect a managed memory
  * model (i.e., unified memory), it can support it.  Here is a summary of how
  * the abstract model would be mapped to specific hardware scenarios:
  *
  *    Not using a device, not using managed memory
- *       Malloc       = host malloc          e.g., malloc
- *       DeviceMalloc = host malloc          e.g., malloc
- *       MemCopy      = host/host memcpy     e.g., memcpy
+ *       Malloc(..., location) 
+ *             location=LOCATION_DEVICE - host malloc          e.g., malloc
+ *             location=LOCATION_HOST   - host malloc          e.g., malloc
+ *       MemCopy(..., method)
+ *             method=HOST_TO_DEVICE    - copy from host to host e.g., memcpy
+ *             method=DEVICE_TO_HOST    - copy from host to host e.g., memcpy
+ *             method=DEVICE_TO_DEVICE  - copy from host to host e.g., memcpy
+ *       SetExecutionMode
+ *             location=LOCATION_DEVICE - execute on the device
+ *             location=LOCATION_HOST   - execute on the host    
  *
  *    Using a device, not using managed memory
- *       Malloc       = host malloc          e.g., malloc
- *       DeviceMalloc = device malloc        e.g., cudaMalloc
- *       MemCopy      = host/device memcpy   e.g., cudaMemcpy
+ *       Malloc(..., location) 
+ *             location=LOCATION_DEVICE - device malloc        e.g., cudaMalloc
+ *             location=LOCATION_HOST   - host malloc          e.g., malloc
+ *       MemCopy(..., method)
+ *             method=HOST_TO_DEVICE    - copy from host to device e.g., cudaMemcpy
+ *             method=DEVICE_TO_HOST    - copy from device to host e.g., cudaMemcpy
+ *             method=DEVICE_TO_DEVICE  - copy from device to device e.g., cudaMemcpy
+ *       SetExecutionMode
+ *             location=LOCATION_DEVICE - execute on the device
+ *             location=LOCATION_HOST   - execute on the host 
  *
  *    Using a device, using managed memory
- *       Malloc       = managed malloc       e.g., cudaMallocManaged
- *       DeviceMalloc = managed malloc       e.g., cudaMallocManaged
- *       MemCopy      = no-op or pre-fetch   e.g., TBD
+ *       Malloc(..., location) 
+ *             location=LOCATION_DEVICE - managed malloc        e.g., cudaMallocManaged
+ *             location=LOCATION_HOST   - host malloc          e.g., malloc
+ *       MemCopy(..., method)
+ *             method=HOST_TO_DEVICE    - no-op
+ *             method=DEVICE_TO_HOST    - no-op
+ *             method=DEVICE_TO_DEVICE  - copy from device to device e.g., cudaMemcpy
+ *       SetExecutionMode
+ *             location=LOCATION_DEVICE - execute on the device
+ *             location=LOCATION_HOST   - execute on the host 
  *
  * Questions:
  *
@@ -61,16 +89,16 @@ extern "C" {
  * Interface prototypes (only for the no-device setting for starters)
  *--------------------------------------------------------------------------*/
 
-#define hypre_TAlloc(type, count) \
+#define hypre_HostTAlloc(type, count) \
 ( (type *)hypre_MAlloc((size_t)(sizeof(type) * (count))) )
 
-#define hypre_CTAlloc(type, count) \
-( (type *)hypre_CAlloc((size_t)(count), (size_t)sizeof(type)) )
+#define hypre_HostCTAlloc(type, count) \
+( (type *)hypre_HostfCAlloc((size_t)(count), (size_t)sizeof(type)) )
 
 #define hypre_TReAlloc(ptr, type, count) \
-( (type *)hypre_ReAlloc((char *)ptr, (size_t)(sizeof(type) * (count))) )
+( (type *)hypre_HostReAlloc((char *)ptr, (size_t)(sizeof(type) * (count))) )
 
-#define hypre_TFree(ptr) \
+#define hypre_HostTFree(ptr) \
 ( hypre_Free((char *)ptr), ptr = NULL )
 
 #define hypre_DeviceTAlloc(type, count)   hypre_TAlloc(type, (count))
@@ -84,8 +112,298 @@ memcpy(ptrDevice, ptrHost, sizeof(type)*(count))
 #define hypre_MemCopyFromDevice(ptrHost, ptrDevice, type, count) \
 memcpy(ptrHost, ptrDevice, sizeof(type)*(count))
 
+/*--------------------------------------------------------------------------
+ * NEW INTERFACE
+ *--------------------------------------------------------------------------*/
+#define LOCATION_DEVICE ( 0)
+#define LOCATION_HOST   ( 1)
+#define LOCATION_UNSET  (-1)
 
+#define HOST_TO_DEVICE   (0)
+#define DEVICE_TO_HOST   (1)
+#define DEVICE_TO_DEVICE (2)
 
+extern HYPRE_Int hypre_exec_policy;
+  
+#if defined(HYPRE_MEMORY_GPU)
+/* Using a device, not using managed memory */
+
+#define hypre_TAlloc(type, count, location)\
+({\
+   type *ptr;\
+   if (location==LOCATION_DEVICE)\
+   {\
+      ptr = hypre_DeviceTAlloc(type,count);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostTAlloc(type,count);\
+   }\
+   ptr;})
+
+#define hypre_CTAlloc(type, count, location)\
+({\
+   type *ptr;\
+   if (location==LOCATION_DEVICE)\
+   {\
+      ptr = hypre_DeviceCTAlloc(type,count);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostCTAlloc(type,count);\
+   }\
+   ptr;})
+  
+#define hypre_TReAlloc(type, count, location)\
+({\
+   type *ptr;\
+   if (location==LOCATION_DEVICE)\
+   {\
+      ptr = hypre_DeviceTReAlloc(type,count);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostTReAlloc(type,count);\
+   }\
+   ptr;})
+
+#define hypre_TFree(type, count, location)\
+({\
+   if (location==LOCATION_DEVICE)\
+   {\
+      hypre_DeviceTFree(type);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      hypre_HostTFree(type);\
+   }\
+})
+
+#define hypre_memory_copy(ptrTo, ptrFrom, type, count, method)\
+   if (method==HOST_TO_DEVICE)\
+   {\
+      cudaMemcpy(ptrTo, ptrFrom, sizeof(type)*count, cudaMemcpyHostToDevice);\
+   }\
+   else if (method==DEVICE_TO_HOST)\
+   {\
+      cudaMemcpy(ptrTo, ptrFrom, sizeof(type)*count, cudaMemcpyDeviceToHost);\
+   }\
+   else if (method==DEVICE_TO_DEVICE)\
+   {\
+      cudaMemcpy(ptrTo, ptrFrom, sizeof(type)*count, cudaMemcpyDeviceToDevice);\
+   }
+
+#define hypre_set_execution_mode(location)\
+     hypre_exec_policy = location;
+
+#elif defined(HYPRE_USE_MANAGED)
+/* Using a device, not using managed memory */
+
+#define hypre_TAlloc(type, count, location)\
+({\
+   type *ptr;\
+   if (location==LOCATION_DEVICE)\
+   {\
+      cudaMallocManaged((void**)&ptr, sizeof(type)*count, cudaMemAttachGlobal);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostTAlloc(type,count);\
+   }\
+   ptr;})
+
+#define hypre_CTAlloc(type, count, location)\
+({\
+   type *ptr;\
+   if (location==LOCATION_DEVICE)\
+   {\
+      cudaMallocManaged((void**)&ptr, sizeof(type)*count, cudaMemAttachGlobal);\
+      cudaMemset(ptr, 0, sizeof(type)*count);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostCTAlloc(type,count);\
+   }\
+   ptr;})
+  
+#define hypre_TReAlloc(type, count, location)\
+({\
+   type *ptr;\
+   if (location==LOCATION_DEVICE)\
+   {\
+      ptr = hypre_UMTReAlloc(type,count);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostTReAlloc(type,count);\
+   }\
+   ptr;})
+
+#define hypre_TFree(type, count, location)\
+({\
+   if (location==LOCATION_DEVICE)\
+   {\
+      hypre_DeviceTFree(type);\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      hypre_HostTFree(type);\
+   }\
+})
+
+#define hypre_memory_copy(ptrTo, ptrFrom, type, count, method)\
+   if (method==HOST_TO_DEVICE)\
+   {\
+   	  if (ptrTo != ptrFrom)\
+      {\
+         cudaMemcpy(ptrTo, ptrFrom, sizeof(type)*count, cudaMemcpyDefault);\
+      }\
+      else\
+      {\
+         ptrTo = ptrFrom;\
+      }\
+   }\
+   else if (method==DEVICE_TO_HOST)\
+   {\
+      if (ptrTo != ptrFrom)\
+      {\
+         cudaMemcpy(ptrTo, ptrFrom, sizeof(type)*count, cudaMemcpyDefault);\
+      }\
+      else\
+      {\
+         ptrTo = ptrFrom;\
+      }\
+   }\
+   else if (method==DEVICE_TO_DEVICE)\
+   {\
+      if (ptrTo != ptrFrom)\
+      {\
+         cudaMemcpy(ptrTo, ptrFrom, sizeof(type)*count, cudaMemcpyDeviceToDevice);\
+      }\
+      else\
+      {\
+         ptrTo = ptrFrom;\
+      }\
+   }
+
+#define hypre_set_execution_mode(location)\
+     hypre_exec_policy = location;
+
+#elif defined(HYPRE_USE_OMP45)
+/* Using OpenMP-4.5 */
+
+#define hypre_TAlloc(type, count, location)\
+({\
+   type *ptr;\
+   HYPRE_Int device_num = omp_get_default_device();\
+   if (location==LOCATION_DEVICE)\
+   {\
+      hypre_omp45_offload(device_num, ptr, type, 0, count, "enter", "alloc");\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostTAlloc(type,count);\
+   }\
+   ptr;})
+
+#define hypre_CTAlloc(type, count, location)\
+({\
+   type *ptr;\
+   HYPRE_Int device_num = omp_get_default_device();\
+   if (location==LOCATION_DEVICE)\
+   {\
+      hypre_omp45_offload(device_num, ptr, type, 0, count, "enter", "to");\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostCTAlloc(type,count);\
+   }\
+   ptr;})
+ 
+/* how to implement realloc fro omp4.5 */
+#define hypre_TReAlloc(type, count, location)\
+({\
+   type *ptr;\
+   HYPRE_Int device_num = omp_get_default_device();\
+   if (location==LOCATION_DEVICE)\
+   {\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      ptr = hypre_HostTReAlloc(type,count);\
+   }\
+   ptr;})
+
+#define hypre_TFree(type, count, location)\
+({\
+   HYPRE_Int device_num = omp_get_default_device();\
+   if (location==LOCATION_DEVICE)\
+   {\
+      hypre_omp45_offload(device_num, ptr, type, 0, count, "exit", "delete");\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      hypre_HostTFree(type);\
+   }\
+})
+
+/* how to implement device to device copy for Openmp4.5 */
+#define hypre_memory_copy(ptrTo, ptrFrom, type, count, method)\
+   HYPRE_Int device_num = omp_get_default_device();\
+   if (method==HOST_TO_DEVICE)\
+   {\
+      if (ptrTo != ptrFrom)\
+      {\
+         Memcpy(ptrTo, ptrFrom, sizeof(type)*count);\
+      }\
+      hypre_omp45_offload(device_num, ptrTo, ptrFrom, type, 0, count, "update", "to");\
+   }\
+   else if (method==DEVICE_TO_HOST)\
+   {\
+      if (ptrTo != ptrFrom)\
+      {\
+         Memcpy(ptrTo, ptrFrom, sizeof(type)*count);\
+      }\
+      hypre_omp45_offload(device_num, ptrTo, ptrFrom, type, 0, count, "update", "from");\
+   }\
+   else if (method==DEVICE_TO_DEVICE)\
+   {\
+      if (ptrTo != ptrFrom)\
+      {\
+      }\
+   }
+
+#define hypre_set_execution_mode(location)\
+   if (location==LOCATION_DEVICE)\
+   {\
+      HYPRE_OMP45_OFFLOAD_ON();\
+   }\
+   else if (location==LOCATION_HOST)\
+   {\
+      HYPRE_OMP45_OFFLOAD_OFF();\
+   }
+
+#else
+/* Not using a device, not using managed memory */
+
+#define hypre_TAlloc(type, count, location) hypre_HostTAlloc(type,count);
+#define hypre_CTAlloc(type, count, location) hypre_HostCTAlloc(type,count);
+#define hypre_TReAlloc(type, count, location) hypre_HostTReAlloc(type,count);
+#define hypre_TFree(type, count, location) hypre_HostTFree(type);
+
+#define hypre_memory_copy(ptrTo, ptrFrom, type, count, method)\
+   if (ptrTo != ptrFrom)\
+   {\
+      memcpy(ptrTo, ptrFrom, sizeof(type)*count);\
+   }\
+   else\
+   {\
+      ptrTo = ptrFrom;\
+   }
+
+#define hypre_set_execution_mode(location) ;
+
+#endif
 /*--------------------------------------------------------------------------
  * CURRENT CODE
  *--------------------------------------------------------------------------*/
