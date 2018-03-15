8100c61ee218dd295cce4be0ee4af0f36d848c91
Ruipeng Li <li50@llnl.gov>
2018-03-15 14:24:11 -0700

    some docs for AIR

--- src/parcsr_ls/HYPRE_parcsr_ls.h
+++ src/parcsr_ls/HYPRE_parcsr_ls.h
@@ -1059,10 +1059,18 @@ HYPRE_Int HYPRE_BoomerAMGSetEuSparseA(HYPRE_Solver solver,
 HYPRE_Int HYPRE_BoomerAMGSetEuBJ(HYPRE_Solver solver,
                                  HYPRE_Int    eu_bj);
 
-/*
- * (Optional)
- *  0: transpose of the interpolation
- *  1: AIR - approximate ideal restriction
+/**
+ * (Optional) Defines which parallel restriction operator is used.
+ * There are the following options for restr\_type: 
+ * 
+ * \begin{tabular}{|c|l|} \hline
+ *  0 & $P^T$ - Transpose of the interpolation operator \\
+ *  1 & AIR-1 - Approximate Ideal Restriction (distance 1) \\
+ *  2 & AIR-2 - Approximate Ideal Restriction (distance 2) \\
+ * \hline
+ * \end{tabular}
+ *
+ * The default is 0.
  **/
 HYPRE_Int HYPRE_BoomerAMGSetRestriction(HYPRE_Solver solver,
                                         HYPRE_Int    restr_par);
@@ -3625,9 +3633,10 @@ HYPRE_Int HYPRE_BoomerAMGSetCRRate(HYPRE_Solver solver,
 HYPRE_Int HYPRE_BoomerAMGSetCRStrongTh(HYPRE_Solver solver,
                                        HYPRE_Real   CR_strong_th);
 
-/*
- * (Optional) Defines drop tolerance for A-matrices from the 2nd level of AMG
- * The default is 0.0.
+/**
+ * (Optional) Defines the dropp tolerance for the A-matrices 
+ * from the 2nd level of AMG.
+ * The default is 0.
  **/
 HYPRE_Int
 HYPRE_BoomerAMGSetADropTol( HYPRE_Solver  solver, 
