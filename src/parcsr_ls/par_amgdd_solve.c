5a3f44ad25ec6dea228f88d3b665a37acd3aeaf8
Wayne Mitchell <wmitchell@mp-headnode.ziti.uni-heidelberg.de>
2018-08-21 15:04:43 +0200

    Hotfix: I fixed this problem before in a different branch, but didnt get it back into the main develop branch... dang. Anyway, I was not allocating starting_nodes appropriately when setting up processor neighbors.

--- src/parcsr_ls/par_amgdd_solve.c
+++ src/parcsr_ls/par_amgdd_solve.c
@@ -10,7 +10,7 @@
  * $Revision$
  ***********************************************************************EHEADER*/
 
-#define TEST_RES_COMM 1
+#define TEST_RES_COMM 0
 
 #include "_hypre_parcsr_ls.h"
 #include "par_amg.h"
