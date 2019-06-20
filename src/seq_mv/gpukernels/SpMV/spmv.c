1d8efcb92ae655139cafcdb5dfde22503f6244fc
Chaoyu Zhang <zhang62@lassen709.coral.llnl.gov>
2019-06-20 14:32:51 -0700

    the changable K version

--- src/seq_mv/gpukernels/SpMV/spmv.c
+++ src/seq_mv/gpukernels/SpMV/spmv.c
@@ -1,343 +1,7 @@
 #include "spmv.h"
 #include <cuda_runtime.h>
 #include "cusparse.h"
-
-
-__global__
-void csr_v_k_8(int n, int *d_ia, int *d_ja, REAL *d_a, REAL *d_x, REAL *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *  shared memory reduction, texture memory fetching
-    *           1/4-Warp (8 threads) per row
-    *------------------------------------------------------------*/
-   // num of 1/4-warps
-   int nqw = gridDim.x*BLOCKDIM/8;
-   // 1/4 warp id
-   int qwid = (blockIdx.x*BLOCKDIM+threadIdx.x)/8;
-   // thread lane in each half warp
-   int lane = threadIdx.x & (8-1);
-   // 1/4 warp lane in each block
-   int qwlane = threadIdx.x/8;
-   // shared memory for patial result
-   volatile __shared__ REAL r[BLOCKDIM+8];
-   volatile __shared__ int startend[BLOCKDIM/8][2];
-   for (int row = qwid; row < n; row += nqw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[qwlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[qwlane][0];
-      int q = startend[qwlane][1];
-      REAL sum = 0.0;
-      for (int i=p+lane; i<q; i+=8)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-      // parallel reduction
-      r[threadIdx.x] = sum;
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+4];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+2];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+1];
-      if (lane == 0)
-      d_y[row] = r[threadIdx.x];
-   }
-}
-
-
-__global__
-void csr_v_k_8_shuffle(int n, int *d_ia, int *d_ja, REAL *d_a, REAL *d_x, REAL *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *  shared memory reduction, texture memory fetching
-    *           1/4-Warp (8 threads) per row (shuffle reduction version)
-    *------------------------------------------------------------*/
-   // num of 1/4-warps
-   int nqw = gridDim.x*BLOCKDIM/8;
-   // 1/4 warp id
-   int qwid = (blockIdx.x*BLOCKDIM+threadIdx.x)/8;
-   // thread lane in each half warp
-   int lane = threadIdx.x & (8-1);
-   // 1/4 warp lane in each block
-   int qwlane = threadIdx.x/8;
-   // shared memory for patial result
-   volatile __shared__ REAL r[BLOCKDIM+8];
-   volatile __shared__ int startend[BLOCKDIM/8][2];
-   for (int row = qwid; row < n; row += nqw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[qwlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[qwlane][0];
-      int q = startend[qwlane][1];
-      REAL sum = 0.0;
-      for (int i=p+lane; i<q; i+=8)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-// parallel reduction
-#pragma unroll
-      for (int d = 8/2; d > 0; d >>= 1)
-      {
-         r[threadIdx.x] = sum += __shfl_down(sum, d);
-      }
-      if (lane == 0)
-      {
-         d_y[row] = r[threadIdx.x];
-      }
-   }
-}
-
-
-
-
-__global__
-void csr_v_k_16(int n, int *d_ia, int *d_ja, REAL *d_a, REAL *d_x, REAL *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *              shared memory reduction
-    *           Half-Warp (16 threads) per row
-    *------------------------------------------------------------*/
-   // num of half-warps
-   int nhw = gridDim.x*BLOCKDIM/HALFWARP;
-   // half warp id
-   int hwid = (blockIdx.x*BLOCKDIM+threadIdx.x)/HALFWARP;
-   // thread lane in each half warp
-   int lane = threadIdx.x & (HALFWARP-1);
-   // half warp lane in each block
-   int hwlane = threadIdx.x/HALFWARP;
-   // shared memory for patial result
-   volatile __shared__ REAL r[BLOCKDIM+8];
-   volatile __shared__ int startend[BLOCKDIM/HALFWARP][2];
-   for (int row = hwid; row < n; row += nhw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[hwlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[hwlane][0];
-      int q = startend[hwlane][1];
-      REAL sum = 0.0;
-      for (int i=p+lane; i<q; i+=HALFWARP)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-      // parallel reduction
-      r[threadIdx.x] = sum;
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+8];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+4];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+2];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+1];
-      if (lane == 0)
-      {
-         d_y[row] = r[threadIdx.x];
-      }
-   }
-}
-
-
-
-__global__
-void csr_v_k_16_shuffle(int n, int *d_ia, int *d_ja, REAL *d_a, REAL *d_x, REAL *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *               shared memory reduction
-    *           Half-Warp (16 threads) per row(shuffle version)
-    *------------------------------------------------------------*/
-   // num of half-warps
-   int nhw = gridDim.x*BLOCKDIM/HALFWARP;
-   // half warp id
-   int hwid = (blockIdx.x*BLOCKDIM+threadIdx.x)/HALFWARP;
-   // thread lane in each half warp
-   int lane = threadIdx.x & (HALFWARP-1);
-   // half warp lane in each block
-   int hwlane = threadIdx.x/HALFWARP;
-   // shared memory for patial result
-   volatile __shared__ int startend[BLOCKDIM/HALFWARP][2];
-   for (int row = hwid; row < n; row += nhw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[hwlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[hwlane][0];
-      int q = startend[hwlane][1];
-      REAL sum = 0.0;
-      for (int i=p+lane; i<q; i+=HALFWARP)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-// parallel reduction
-#pragma unroll
-      for (int d = HALFWARP/2; d > 0; d >>= 1)
-      {
-         sum += __shfl_down(sum, d);
-      }
-      if (lane == 0)
-      {
-         d_y[row] = sum;
-      }
-   }
-}
-
-__global__
-void csr_v_k_32(int n, int *d_ia, int *d_ja, REAL *d_a, REAL *d_x, REAL *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *              shared memory reduction
-    *          FULL-Warp (32 threads) per row
-    *------------------------------------------------------------*/
-   // num of full-warps
-   int nw = gridDim.x*BLOCKDIM/WARP;
-   // full warp id
-   int wid = (blockIdx.x*BLOCKDIM+threadIdx.x)/WARP;
-   // thread lane in each full warp
-   int lane = threadIdx.x & (WARP-1);
-   // full warp lane in each block
-   int wlane = threadIdx.x/WARP;
-   // shared memory for patial result
-   volatile __shared__ REAL r[BLOCKDIM+16];
-   volatile __shared__ int startend[BLOCKDIM/WARP][2];
-   for (int row = wid; row < n; row += nw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[wlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[wlane][0];
-      int q = startend[wlane][1];
-      REAL sum = 0.0;
-      for (int i=p+lane; i<q; i+=WARP)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-      // parallel reduction
-      r[threadIdx.x] = sum;
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+16];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+8];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+4];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+2];
-      r[threadIdx.x] = sum = sum + r[threadIdx.x+1];
-      if (lane == 0)
-      {
-         d_y[row] = r[threadIdx.x];
-      }
-   }
-}
-
-
-__global__
-void csr_v_k_32_shuffle(int n, int *d_ia, int *d_ja, REAL *d_a, REAL *d_x, REAL *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *              shared memory reduction
-    *          FULL-Warp (32 threads) per row(shuffle version)
-    *------------------------------------------------------------*/
-   // num of full-warps
-   int nw = gridDim.x*BLOCKDIM/WARP;
-   // full warp id
-   int wid = (blockIdx.x*BLOCKDIM+threadIdx.x)/WARP;
-   // thread lane in each full warp
-   int lane = threadIdx.x & (WARP-1);
-   // full warp lane in each block
-   int wlane = threadIdx.x/WARP;
-   // shared memory for patial result
-   volatile __shared__ REAL r[BLOCKDIM+16];
-   volatile __shared__ int startend[BLOCKDIM/WARP][2];
-   for (int row = wid; row < n; row += nw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[wlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[wlane][0];
-      int q = startend[wlane][1];
-      REAL sum = 0.0;
-      for (int i=p+lane; i<q; i+=WARP)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-      // parallel reduction
-      #pragma unroll
-      for (int d = WARP/2; d > 0; d >>= 1)
-      {
-      r[threadIdx.x] = sum += __shfl_down(sum, d);
-      }
-      if (lane == 0)
-      {
-         d_y[row] = r[threadIdx.x];
-      }
-   }
-}
-
-
-//example
-template <int K, typename T>
-__global__
-void csr_v_k(int n, int *d_ia, int *d_ja, T *d_a, T *d_x, T *d_y)
-{
-   /*------------------------------------------------------------*
-    *               CSR spmv-vector kernel
-    *              shared memory reduction
-    *              K threads-Warp  per row
-    *------------------------------------------------------------*/
-   // num of full-warps
-   int nw = gridDim.x*BLOCKDIM/K;
-   // full warp id
-   int wid = (blockIdx.x*BLOCKDIM+threadIdx.x)/K;
-   // thread lane in each full warp
-   int lane = threadIdx.x & (K-1);
-   // full warp lane in each block
-   int wlane = threadIdx.x/K;
-   // shared memory for patial result
-   volatile __shared__ T r[BLOCKDIM+K/2];
-   volatile __shared__ int startend[BLOCKDIM/K][2];
-   for (int row = wid; row < n; row += nw)
-   {
-      // row start and end point
-      if (lane < 2)
-      {
-         startend[wlane][lane] = d_ia[row+lane];
-      }
-      int p = startend[wlane][0];
-      int q = startend[wlane][1];
-      T sum = 0.0;
-      for (int i=p+lane; i<q; i+=K)
-      {
-         sum += d_a[i] * d_x[d_ja[i]];
-      }
-      // parallel reduction
-      r[threadIdx.x] = sum;
-#pragma unroll
-      for (int d = K/2; d > 0; d >>= 1)
-      {
-         r[threadIdx.x] = sum = sum + r[threadIdx.x+d];
-      }
-      if (lane == 0)
-      {
-         d_y[row] = r[threadIdx.x];
-      }
-   }
-}
-
-//example
-
-//example_shuffle
-
-//#define ROW_PTR_USE_SHARED
+#define FULL_MASK 0xffffffff
 
 template <int K, typename T>
 __global__
@@ -345,7 +9,7 @@ void csr_v_k_shuffle(int n, int *d_ia, int *d_ja, T *d_a, T *d_x, T *d_y)
 {
    /*------------------------------------------------------------*
     *               CSR spmv-vector kernel
-    *              shared memory reduction
+    *              shuffle version reduction
     *            K-Warp  ( K threads) per row
     *------------------------------------------------------------*/
    // num of full-warps
@@ -384,11 +48,12 @@ void csr_v_k_shuffle(int n, int *d_ia, int *d_ja, T *d_a, T *d_x, T *d_y)
       {
          sum += d_a[i] * d_x[d_ja[i]];
       }
+
       // parallel reduction
 #pragma unroll
       for (int d = K/2; d > 0; d >>= 1)
       {
-         sum += __shfl_down(sum, d);
+         sum += __shfl_down_sync(FULL_MASK, sum, d, K);
       }
       if (lane == 0)
       {
@@ -397,7 +62,7 @@ void csr_v_k_shuffle(int n, int *d_ia, int *d_ja, T *d_a, T *d_x, T *d_y)
    }
 }
 
-//example
+
 
 void spmv_csr_vector(struct csr_t *csr, REAL *x, REAL *y)
 {
@@ -427,17 +92,48 @@ void spmv_csr_vector(struct csr_t *csr, REAL *x, REAL *y)
    int gDim = min(MAXTHREADS/BLOCKDIM, (n+hwb-1)/hwb);
    int bDim = BLOCKDIM;
    //printf("CSR<<<%4d, %3d>>>  ",gDim,bDim);
-   /*-------- start timing */
-   t1 = wall_timer();
-   for (i=0; i<REPEAT; i++)
+   if(nnz/n>16)
    {
-      //cudaMemset((void *)d_y, 0, n*sizeof(REAL));
-      csr_v_k_shuffle<32, REAL> <<<gDim, bDim>>>(n, d_ia, d_ja, d_a, d_x, d_y);
+      /*-------- start timing */
+      t1 = wall_timer();
+      for (i=0; i<REPEAT; i++)
+      {
+         //cudaMemset((void *)d_y, 0, n*sizeof(REAL));
+         csr_v_k_shuffle<16, REAL> <<<gDim, bDim>>>(n, d_ia, d_ja, d_a, d_x, d_y);
+      }
+      /*-------- Barrier for GPU calls */
+      cudaThreadSynchronize();
+      /*-------- stop timing */
+      t2 = wall_timer()-t1;   
    }
-   /*-------- Barrier for GPU calls */
-   cudaThreadSynchronize();
-   /*-------- stop timing */
-   t2 = wall_timer()-t1;
+   else if(nnz/n>8)
+   {
+      /*-------- start timing */
+      t1 = wall_timer();
+      for (i=0; i<REPEAT; i++)
+      {
+         //cudaMemset((void *)d_y, 0, n*sizeof(REAL));
+         csr_v_k_shuffle<8, REAL> <<<gDim, bDim>>>(n, d_ia, d_ja, d_a, d_x, d_y);
+      }
+      /*-------- Barrier for GPU calls */
+      cudaThreadSynchronize();
+      /*-------- stop timing */
+      t2 = wall_timer()-t1;
+   }
+   else
+   {
+      /*-------- start timing */
+      t1 = wall_timer();
+      for (i=0; i<REPEAT; i++)
+      {
+         //cudaMemset((void *)d_y, 0, n*sizeof(REAL));
+         csr_v_k_shuffle<4, REAL> <<<gDim, bDim>>>(n, d_ia, d_ja, d_a, d_x, d_y);
+      }
+      /*-------- Barrier for GPU calls */
+      cudaThreadSynchronize();
+      /*-------- stop timing */
+      t2 = wall_timer()-t1;
+   }
 /*--------------------------------------------------*/
    printf("\n=== [GPU] CSR-vector Kernel ===\n");
    printf("  Number of Threads <%d*%d>\n",gDim,bDim);
