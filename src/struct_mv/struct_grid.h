7b9b5d747b30c49aa3d7b2b6f9ff98414549e3b7
victor@tux <paludettomag1@llnl.gov>
2019-07-09 10:44:52 -0700

    Implement accessor macro for box ID inside a grid

--- src/struct_mv/struct_grid.h
+++ src/struct_mv/struct_grid.h
@@ -28,9 +28,9 @@
 typedef struct hypre_StructGrid_struct
 {
    MPI_Comm             comm;
-                      
+
    HYPRE_Int            ndim;         /* Number of grid dimensions */
-                      
+
    hypre_BoxArray      *boxes;        /* Array of boxes in this process */
    HYPRE_Int           *ids;          /* Unique IDs for boxes */
    hypre_Index          max_distance; /* Neighborhood size - in each dimension*/
@@ -42,7 +42,7 @@ typedef struct hypre_StructGrid_struct
 
    hypre_Index          periodic;     /* Indicates if grid is periodic */
    HYPRE_Int            num_periods;  /* number of box set periods */
-   
+
    hypre_Index         *pshifts;      /* shifts of periodicity */
 
 
@@ -50,7 +50,7 @@ typedef struct hypre_StructGrid_struct
 
 
    HYPRE_Int            ghlocal_size; /* Number of vars in box including ghosts */
-   HYPRE_Int            num_ghost[2*HYPRE_MAXDIM]; /* ghost layer size */  
+   HYPRE_Int            num_ghost[2*HYPRE_MAXDIM]; /* ghost layer size */
 
    hypre_BoxManager    *boxman;
 
@@ -64,6 +64,7 @@ typedef struct hypre_StructGrid_struct
 #define hypre_StructGridNDim(grid)          ((grid) -> ndim)
 #define hypre_StructGridBoxes(grid)         ((grid) -> boxes)
 #define hypre_StructGridIDs(grid)           ((grid) -> ids)
+#define hypre_StructGridID(grid, i)         ((grid) -> ids[i])
 #define hypre_StructGridMaxDistance(grid)   ((grid) -> max_distance)
 #define hypre_StructGridBoundingBox(grid)   ((grid) -> bounding_box)
 #define hypre_StructGridLocalSize(grid)     ((grid) -> local_size)
@@ -75,7 +76,7 @@ typedef struct hypre_StructGrid_struct
 #define hypre_StructGridRefCount(grid)      ((grid) -> ref_count)
 #define hypre_StructGridGhlocalSize(grid)   ((grid) -> ghlocal_size)
 #define hypre_StructGridNumGhost(grid)      ((grid) -> num_ghost)
-#define hypre_StructGridBoxMan(grid)        ((grid) -> boxman) 
+#define hypre_StructGridBoxMan(grid)        ((grid) -> boxman)
 
 #define hypre_StructGridBox(grid, i) \
 (hypre_BoxArrayBox(hypre_StructGridBoxes(grid), i))
@@ -88,9 +89,8 @@ hypre_BoxNeighborsIDPeriod(hypre_StructGridNeighbors(grid))
 /*--------------------------------------------------------------------------
  * Looping macros:
  *--------------------------------------------------------------------------*/
- 
+
 #define hypre_ForStructGridBoxI(i, grid) \
 hypre_ForBoxI(i, hypre_StructGridBoxes(grid))
 
 #endif
-
