1da9b8009d38057b0c6ef818445c5822101bb0e9
Sarah Virginia Osborn <osborn9@llnl.gov>
2019-08-15 09:20:19 -0700

    Each subdirectory now contains its own cmake list.

--- src/cmake/hypre_CMakeUtilities.cmake
+++ src/cmake/hypre_CMakeUtilities.cmake
@@ -1,3 +1,13 @@
+# A handy function to add the current source directory to a local
+# filename. To be used for creating a list of sources.
+function(convert_filenames_to_full_paths NAMES)
+  unset(tmp_names)
+  foreach(name ${${NAMES}})
+    list(APPEND tmp_names ${CMAKE_CURRENT_SOURCE_DIR}/${name})
+  endforeach()
+  set(${NAMES} ${tmp_names} PARENT_SCOPE)
+endfunction()
+
 # A function to add each executable in the list to the build with the
 # correct flags, includes, and linkage.
 function(add_hypre_executables EXE_SRCS)
