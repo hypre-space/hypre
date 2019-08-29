1de3094d6c3a264019478995b3d4cbf667c2c076
Kote Hitenze <hitenze@jotenshis-MacBook-Pro.local>
2019-08-29 11:20:42 -0500

    Fix a bug when running on Lassen

--- src/parcsr_ls/par_ilu.c
+++ src/parcsr_ls/par_ilu.c
@@ -2127,6 +2127,7 @@ hypre_ParILUCusparseSchurGMRESDummySolve( void               *ilu_vdata,
    hypre_Vector            *f_local          = hypre_ParVectorLocalVector(f);
    HYPRE_Real              *f_data           = hypre_VectorData(f_local);
    
+   cudaDeviceSynchronize();
    hypre_TMemcpy(u_data, f_data, HYPRE_Real, n_local, HYPRE_MEMORY_SHARED, HYPRE_MEMORY_SHARED);
    
    return hypre_error_flag;
