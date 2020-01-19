2a2c8107b1e152f00e250cfbe82d66b69efb9920
Rob Falgout <rfalgout@llnl.gov>
2020-01-18 16:30:40 -0800

    Adding some readthedocs stuff to usr-manual/conf.py

--- src/docs/usr-manual/conf.py
+++ src/docs/usr-manual/conf.py
@@ -19,6 +19,14 @@ import alabaster
 # Read the docs
 #import sphinx_rtd_theme
 
+import subprocess
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../ref-manual; doxygen conf.doxygen', shell=True)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
