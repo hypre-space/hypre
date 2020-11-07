f5c0246b49249eff1d4defd32db1e48b460accde
Ruipeng Li <li50@llnl.gov>
2020-11-07 00:23:57 -0800

    fixed IJ headers

--- src/IJ_mv/IJ_vector.h
+++ src/IJ_mv/IJ_vector.h
@@ -5,7 +5,6 @@
  * SPDX-License-Identifier: (Apache-2.0 OR MIT)
  ******************************************************************************/
 
-#if 0
 /******************************************************************************
  *
  * Header info for the hypre_IJMatrix structures
@@ -23,7 +22,7 @@ typedef struct hypre_IJVector_struct
 {
    MPI_Comm      comm;
 
-   HYPRE_BigInt	*partitioning;      /* Indicates partitioning over tasks */
+   HYPRE_BigInt *partitioning;      /* Indicates partitioning over tasks */
 
    HYPRE_Int     object_type;       /* Indicates the type of "local storage" */
 
@@ -39,37 +38,37 @@ typedef struct hypre_IJVector_struct
                                     /* global partition */
    HYPRE_Int     print_level;
 
-
-
 } hypre_IJVector;
 
 /*--------------------------------------------------------------------------
  * Accessor macros: hypre_IJVector
  *--------------------------------------------------------------------------*/
 
-#define hypre_IJVectorComm(vector)           ((vector) -> comm)
-
-#define hypre_IJVectorPartitioning(vector)   ((vector) -> partitioning)
-
-#define hypre_IJVectorObjectType(vector)     ((vector) -> object_type)
-
-#define hypre_IJVectorObject(vector)         ((vector) -> object)
-
-#define hypre_IJVectorTranslator(vector)     ((vector) -> translator)
-
+#define hypre_IJVectorComm(vector)            ((vector) -> comm)
+#define hypre_IJVectorPartitioning(vector)    ((vector) -> partitioning)
+#define hypre_IJVectorObjectType(vector)      ((vector) -> object_type)
+#define hypre_IJVectorObject(vector)          ((vector) -> object)
+#define hypre_IJVectorTranslator(vector)      ((vector) -> translator)
 #define hypre_IJVectorAssumedPart(vector)     ((vector) -> assumed_part)
-
 #define hypre_IJVectorGlobalFirstRow(vector)  ((vector) -> global_first_row)
+#define hypre_IJVectorGlobalNumRows(vector)   ((vector) -> global_num_rows)
+#define hypre_IJVectorPrintLevel(vector)      ((vector) -> print_level)
 
-#define hypre_IJVectorGlobalNumRows(vector)  ((vector) -> global_num_rows)
+static inline HYPRE_MemoryLocation
+hypre_IJVectorMemoryLocation(hypre_IJVector *vector)
+{
+   if ( hypre_IJVectorObject(vector) && hypre_IJVectorObjectType(vector) == HYPRE_PARCSR)
+   {
+      return hypre_ParVectorMemoryLocation( (hypre_ParVector *) hypre_IJVectorObject(vector) );
+   }
 
-#define hypre_IJVectorPrintLevel(vector)  ((vector) -> print_level)
+   return HYPRE_MEMORY_UNDEFINED;
+}
 
 /*--------------------------------------------------------------------------
  * prototypes for operations on local objects
  *--------------------------------------------------------------------------*/
 /* #include "./internal_protos.h" */
 
-#endif
+#endif /* #ifndef hypre_IJ_VECTOR_HEADER */
 
-#endif
