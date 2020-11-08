fdc3a602342ca74e2e5d62bfa0052134172fab4d
Ruipeng Li <li50@llnl.gov>
2020-11-08 00:12:30 -0800

    a minor change

--- src/test/sstruct.c
+++ src/test/sstruct.c
@@ -2433,7 +2433,7 @@ main( hypre_int argc,
    HYPRE_Init();
 
 #if defined(HYPRE_USING_CUDA) || defined(HYPRE_USING_DEVICE_OPENMP)
-   hypre_HandleDefaultExecPolicy(hypre_handle()) = HYPRE_EXEC_DEVICE;
+   //hypre_HandleDefaultExecPolicy(hypre_handle()) = HYPRE_EXEC_DEVICE;
    //hypre_HandleSpgemmUseCusparse(hypre_handle()) = 1;
 #endif
 
