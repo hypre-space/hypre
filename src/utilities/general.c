74e6c0a29e77026a7935873b9a5eee3223d8d500
Victor A. P. Magri <paludettomag1@llnl.gov>
2020-11-24 09:06:04 -0800

    Move hypre_Log2 to utilities

--- src/utilities/general.c
+++ src/utilities/general.c
@@ -10,7 +10,7 @@
  * $Revision$
  ***********************************************************************EHEADER*/
 
-#include "_hypre_struct_ls.h"
+#include "_hypre_utilities.h"
 
 /*--------------------------------------------------------------------------
  * hypre_Log2:
@@ -19,12 +19,14 @@
  *--------------------------------------------------------------------------*/
 
 HYPRE_Int
-hypre_Log2(HYPRE_Int p)
+hypre_Log2( HYPRE_Int p )
 {
    HYPRE_Int  e;
 
    if (p <= 0)
+   {
       return -1;
+   }
 
    e = 0;
    while (p > 1)
@@ -32,6 +34,6 @@ hypre_Log2(HYPRE_Int p)
       e += 1;
       p /= 2;
    }
- 
+
    return e;
 }
