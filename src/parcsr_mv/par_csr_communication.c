2289221fb2992ce646c7f6978a0abe75c7e62b87
Ruipeng Li <li50@llnl.gov>
2021-01-04 15:54:56 -0800

    add more comments than just ``TODO''

--- src/parcsr_mv/par_csr_communication.c
+++ src/parcsr_mv/par_csr_communication.c
@@ -412,7 +412,8 @@ hypre_ParCSRCommHandleCreate_v2 ( HYPRE_Int            job,
 #else /* #ifndef HYPRE_WITH_GPU_AWARE_MPI */
    send_data = send_data_in;
    recv_data = recv_data_in;
-   // TODO RL
+   // TODO RL: it seems that we need to sync the CUDA stream before doing GPU-GPU MPI.
+   // Need to check MPI documentation whether this is acutally true
    hypre_SyncCudaComputeStream(hypre_handle());
 #endif
 
