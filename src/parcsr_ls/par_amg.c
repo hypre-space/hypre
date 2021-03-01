c5740b7548b1f12ac6757be94f99f191de263d48
Paul T. Bauman <ptbauman@gmail.com>
2021-03-01 12:13:05 -0600

    emacs whitespace

--- src/parcsr_ls/par_amg.c
+++ src/parcsr_ls/par_amg.c
@@ -4075,7 +4075,7 @@ hypre_BoomerAMGSetILUMaxRowNnz( void     *data,
 
    return hypre_error_flag;
 }
-HYPRE_Int 
+HYPRE_Int
 hypre_BoomerAMGSetILULocalReordering( void     *data,
                         HYPRE_Int       ilu_reordering_type)
 {
