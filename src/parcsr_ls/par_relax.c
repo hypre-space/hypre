18dd3254f1260da030f1f96d183af4c2e5283e6d
Ruipeng Li <li50@llnl.gov>
2021-06-15 12:13:43 -0700

    gs device control

--- src/parcsr_ls/par_relax.c
+++ src/parcsr_ls/par_relax.c
@@ -873,6 +873,13 @@ hypre_BoomerAMGRelax3HybridGaussSeidel( hypre_ParCSRMatrix *A,
       exec = HYPRE_EXEC_HOST;
    }
 
+#if defined(HYPRE_USING_GPU)
+   if (hypre_HandleDeviceGSMethod(hypre_handle()) == 0)
+   {
+      exec = HYPRE_EXEC_HOST;
+   }
+#endif
+
    if (exec == HYPRE_EXEC_DEVICE)
    {
       return hypre_BoomerAMGRelaxHybridGaussSeidelDevice(A, f, cf_marker, relax_points, relax_weight, omega, NULL, u, Vtemp, Ztemp,
@@ -907,6 +914,13 @@ hypre_BoomerAMGRelax4HybridGaussSeidel( hypre_ParCSRMatrix *A,
    {
       exec = HYPRE_EXEC_HOST;
    }
+
+#if defined(HYPRE_USING_GPU)
+   if (hypre_HandleDeviceGSMethod(hypre_handle()) == 0)
+   {
+      exec = HYPRE_EXEC_HOST;
+   }
+#endif
 
    if (exec == HYPRE_EXEC_DEVICE)
    {
@@ -1040,6 +1054,13 @@ hypre_BoomerAMGRelax6HybridSSOR( hypre_ParCSRMatrix *A,
       exec = HYPRE_EXEC_HOST;
    }
 
+#if defined(HYPRE_USING_GPU)
+   if (hypre_HandleDeviceGSMethod(hypre_handle()) == 0)
+   {
+      exec = HYPRE_EXEC_HOST;
+   }
+#endif
+
    if (exec == HYPRE_EXEC_DEVICE)
    {
       return hypre_BoomerAMGRelaxHybridGaussSeidelDevice(A, f, cf_marker, relax_points, relax_weight, omega, NULL, u, Vtemp, Ztemp,
@@ -1130,6 +1151,13 @@ hypre_BoomerAMGRelax8HybridL1SSOR( hypre_ParCSRMatrix *A,
       exec = HYPRE_EXEC_HOST;
    }
 
+#if defined(HYPRE_USING_GPU)
+   if (hypre_HandleDeviceGSMethod(hypre_handle()) == 0)
+   {
+      exec = HYPRE_EXEC_HOST;
+   }
+#endif
+
    if (exec == HYPRE_EXEC_DEVICE)
    {
       return hypre_BoomerAMGRelaxHybridGaussSeidelDevice(A, f, cf_marker, relax_points, relax_weight, omega, l1_norms, u, Vtemp, Ztemp,
@@ -1184,6 +1212,13 @@ hypre_BoomerAMGRelax13HybridL1GaussSeidel( hypre_ParCSRMatrix *A,
       exec = HYPRE_EXEC_HOST;
    }
 
+#if defined(HYPRE_USING_GPU)
+   if (hypre_HandleDeviceGSMethod(hypre_handle()) == 0)
+   {
+      exec = HYPRE_EXEC_HOST;
+   }
+#endif
+
    if (exec == HYPRE_EXEC_DEVICE)
    {
       return hypre_BoomerAMGRelaxHybridGaussSeidelDevice(A, f, cf_marker, relax_points, relax_weight, omega, l1_norms, u, Vtemp, Ztemp,
@@ -1221,6 +1256,13 @@ hypre_BoomerAMGRelax14HybridL1GaussSeidel( hypre_ParCSRMatrix *A,
    {
       exec = HYPRE_EXEC_HOST;
    }
+
+#if defined(HYPRE_USING_GPU)
+   if (hypre_HandleDeviceGSMethod(hypre_handle()) == 0)
+   {
+      exec = HYPRE_EXEC_HOST;
+   }
+#endif
 
    if (exec == HYPRE_EXEC_DEVICE)
    {
