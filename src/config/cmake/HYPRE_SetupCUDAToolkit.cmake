a6926b8bcccf46f4bb505758b6b77ecc0210db44
Dan Ibanez <daibane@sandia.gov>
2021-06-29 16:06:58 -0600

    use FindCUDAToolkit.cmake when available

--- src/config/cmake/HYPRE_SetupCUDAToolkit.cmake
+++ src/config/cmake/HYPRE_SetupCUDAToolkit.cmake
@@ -9,16 +9,14 @@
 # the libraries are located in ${CUDA_TOOLKIT_ROOT_DIR}/lib64.
 # Please set cmake variable CUDA_TOOLKIT_ROOT_DIR. 
 
-# TODO Eventually should require cmake>=3.17
-# and use cmake's FindCUDAToolkit (also helps handle
-# shared vs. static libraries).
-
 # Collection of CUDA optional libraries
 set(EXPORT_INTERFACE_CUDA_LIBS "")
 
 if (NOT CUDA_FOUND)
   find_package(CUDA REQUIRED)
 endif ()
+
+if (CMAKE_VERSION VERSION_LESS 3.17)
 
 if (HYPRE_ENABLE_CUSPARSE)
   set(HYPRE_USING_CUSPARSE ON CACHE BOOL "" FORCE)
@@ -65,3 +63,45 @@ if (HYPRE_ENABLE_GPU_PROFILING)
   message(STATUS "NVidia tools extension library found in " ${NVTX_LIBRARY})
   list(APPEND EXPORT_INTERFACE_CUDA_LIBS ${NVTX_LIBRARY})
 endif (HYPRE_ENABLE_GPU_PROFILING)
+
+else()
+
+find_package(CUDAToolkit REQUIRED)
+
+if (HYPRE_ENABLE_CUSPARSE)
+  set(HYPRE_USING_CUSPARSE ON CACHE BOOL "" FORCE)
+  if (HYPRE_SHARED)
+    list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::cusparse)
+  else ()
+    list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::cusparse_static)
+  endif ()
+endif ()
+
+if (HYPRE_ENABLE_CURAND)
+  set(HYPRE_USING_CURAND ON CACHE BOOL "" FORCE)
+  if (HYPRE_SHARED)
+    list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::curand)
+  else ()
+    list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::curand_static)
+  endif ()
+endif ()
+
+if (HYPRE_ENABLE_CUBLAS)
+  set(HYPRE_USING_CUBLAS ON CACHE BOOL "" FORCE)
+  if (HYPRE_SHARED)
+    list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::cublas CUDA::cublasLt)
+  else ()
+    list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::cublas_static CUDA::cublasLt_static)
+  endif (HYPRE_SHARED)
+endif (HYPRE_ENABLE_CUBLAS)
+
+if (NOT HYPRE_SHARED)
+  list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::culibos)
+endif (NOT HYPRE_SHARED)
+
+if (HYPRE_ENABLE_GPU_PROFILING)
+  set(HYPRE_USING_NVTX ON CACHE BOOL "" FORCE)
+  list(APPEND EXPORT_INTERFACE_CUDA_LIBS CUDA::nvToolsExt)
+endif (HYPRE_ENABLE_GPU_PROFILING)
+
+endif()
