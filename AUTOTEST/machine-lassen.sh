3d0718e9a4097d5eace51ded6007b8d4dfcfb6dc
Ruipeng Li <li50@llnl.gov>
2021-09-21 08:41:14 -0700

    fixed comiple warnings; updated regression on ray

--- AUTOTEST/machine-lassen.sh
+++ AUTOTEST/machine-lassen.sh
@@ -52,8 +52,7 @@ save="lassen"
 # CUDA with UM in debug mode [ij, ams, struct, sstruct]
 co="--with-cuda --enable-unified-memory --enable-persistent --enable-debug --with-gpu-arch=\\'60 70\\' --with-extra-CFLAGS=\\'-qmaxmem=-1 -qsuppress=1500-029\\' --with-extra-CXXFLAGS=\\'-qmaxmem=-1 -qsuppress=1500-029\\'"
 ro="-ij-gpu -ams -struct -sstruct -rt -mpibind -save ${save} -rtol ${rtol} -atol ${atol}"
-eo="-gpu -rt -mpibind -save ${save} -rtol ${rtol} -atol ${atol}"
-./test.sh basic.sh $src_dir -co: $co -mo: $mo -ro: $ro -eo: $eo
+./test.sh basic.sh $src_dir -co: $co -mo: $mo -ro: $ro
 ./renametest.sh basic $output_dir/basic-cuda-um
 
 #CUDA with UM and mixed-int
@@ -109,6 +108,17 @@ co="-DCMAKE_C_COMPILER=$(which xlc) -DCMAKE_CXX_COMPILER=$(which xlc++) -DCMAKE_
 co="-DCMAKE_C_COMPILER=$(which xlc) -DCMAKE_CXX_COMPILER=$(which xlc++) -DCMAKE_CUDA_COMPILER=$(which nvcc) -DMPI_C_COMPILER=$(which mpicc) -DMPI_CXX_COMPILER=$(which mpicxx) -DHYPRE_WITH_CUDA=ON -DCMAKE_BUILD_TYPE=Debug -DHYPRE_WITH_EXTRA_CFLAGS="\'"-qmaxmem=-1 -qsuppress=1500-029"\'" -DHYPRE_WITH_EXTRA_CXXFLAGS="\'"-qmaxmem=-1 -qsuppress=1500-029"\'" -DHYPRE_CUDA_SM=70"
 ./test.sh cmake.sh $src_dir -co: $co -mo: $mo
 ./renametest.sh cmake $output_dir/cmake-cuda-nonum-struct
+
+############
+#   gcc    #
+############
+
+# CUDA with UM in debug mode [compile with gcc; run examples]
+module load gcc
+co="--with-cuda --enable-unified-memory --enable-debug --with-gpu-arch=\\'60 70\\' CC=mpicc CXX=mpicxx"
+eo="-gpu -rt -mpibind -save ${save} -rtol ${rtol} -atol ${atol}"
+./test.sh basic.sh $src_dir -co: $co -mo: $mo -eo: $eo
+./renametest.sh basic $output_dir/basic-cuda-um-gcc
 
 # Echo to stderr all nonempty error files in $output_dir
 for errfile in $( find $output_dir ! -size 0 -name "*.err" )
