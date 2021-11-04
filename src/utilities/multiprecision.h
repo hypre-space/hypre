c6bd888ffc6ad85a821d0f1a1d65ab2526d62dbe
Daniel Osei-Kuffuor <oseikuffuor1@llnl.gov>
2021-11-04 08:43:26 -0700

    Header file fix and update to configure script.
    * Fixed copyight message in multiprecision.h.
    * Added multiprecision build options to configure script.

--- src/utilities/multiprecision.h
+++ src/utilities/multiprecision.h
@@ -1,19 +1,9 @@
-/*BHEADER**********************************************************************
- * Copyright (c) 2017,  Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- * Written by Ulrike Yang (yang11@llnl.gov) et al. CODE-LLNL-738-322.
- * This file is part of AMG.  See files README and COPYRIGHT for details.
+/******************************************************************************
+ * Copyright 1998-2019 Lawrence Livermore National Security, LLC and other
+ * HYPRE Project Developers. See the top-level COPYRIGHT file for details.
  *
- * AMG is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License (as published by the Free
- * Software Foundation) version 2.1 dated February 1999.
- *
- * This software is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTIBILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
- * GNU General Public License for more details.
- *
- ***********************************************************************EHEADER*/
+ * SPDX-License-Identifier: (Apache-2.0 OR MIT)
+ ******************************************************************************/
 
 /******************************************************************************
  *
