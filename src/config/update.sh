a8d423013b518c542b98ed3dce45e0db99bb3807
Ruipeng Li <li50@llnl.gov>
2022-05-24 13:39:33 -0700

    missing -f in softlink commands (#594)
    This PR adds in a few of the Mafefiles the missing -f in the softlink commands.
    
    Co-authored-by: Paul Mullowney <Paul.Mullowney@nrel.gov>

--- src/config/update.sh
+++ src/config/update.sh
@@ -34,7 +34,7 @@ sed -e 's#m4_define.*HYPRE_DATE[^)]*#m4_define([M4_HYPRE_DATE],    ['$hypre_reld
 > config/configure.in.tmp
 mv config/configure.in.tmp config/configure.in
 
-ln -s config/configure.in .
+ln -s -f config/configure.in .
 rm -rf aclocal.m4 configure autom4te.cache
 autoconf --include=config
 autoheader configure.in
