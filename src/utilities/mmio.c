84baffc1da9a23e20adf52c0a8077f2ab337366a
Ruipeng Li <li50@llnl.gov>
2022-06-10 15:08:41 -0700

    add license

--- src/utilities/mmio.c
+++ src/utilities/mmio.c
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+ * HYPRE Project Developers. See the top-level COPYRIGHT file for details.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR MIT)
+ ******************************************************************************/
+
 /*
 *   Matrix Market I/O library for ANSI C
 *
