b0c0040632b7b5795466bfa7bd4225e88c4c1aae
Victor A. P. Magri <paludettomag1@llnl.gov>
2022-07-27 10:41:18 -0400

    Extend hypre's SpMV on GPUs to multivecs and add calls to cusparse's SpMM

--- src/seq_mv/seq_mv.h
+++ src/seq_mv/seq_mv.h
@@ -579,8 +579,10 @@ HYPRE_Int hypre_CSRMatrixSpMVDevice( HYPRE_Int trans, HYPRE_Complex alpha, hypre
                                      hypre_Vector *x,
                                      HYPRE_Complex beta, hypre_Vector *y, HYPRE_Int fill );
 
-HYPRE_Int hypre_CSRMatrixIntSpMVDevice( HYPRE_Int nrows, HYPRE_Int nnz, HYPRE_Int alpha,
-                                        HYPRE_Int *d_ia, HYPRE_Int *d_ja, HYPRE_Int *d_a, HYPRE_Int *d_x, HYPRE_Int beta, HYPRE_Int *d_y );
+HYPRE_Int hypre_CSRMatrixIntSpMVDevice( HYPRE_Int num_rows, HYPRE_Int num_nonzeros,
+                                        HYPRE_Int alpha, HYPRE_Int *d_ia, HYPRE_Int *d_ja,
+                                        HYPRE_Int *d_a, HYPRE_Int *d_x, HYPRE_Int beta,
+                                        HYPRE_Int *d_y );
 
 #if defined(HYPRE_USING_CUSPARSE) || defined(HYPRE_USING_ROCSPARSE) || defined(HYPRE_USING_ONEMKLSPARSE)
 hypre_CsrsvData* hypre_CsrsvDataCreate();
