dcc8a31c44b36a457d52d3a71b8c5db320887f0f
Rui Peng Li <li50@llnl.gov>
2022-11-07 08:41:07 -0800

    add comment

--- src/IJ_mv/HYPRE_IJ_mv.h
+++ src/IJ_mv/HYPRE_IJ_mv.h
@@ -469,7 +469,12 @@ HYPRE_Int HYPRE_IJVectorAddToValues(HYPRE_IJVector       vector,
 HYPRE_Int HYPRE_IJVectorAssemble(HYPRE_IJVector vector);
 
 /**
+ * Update vectors by setting (action 1) or
+ * adding to (action 0) values in 'vector'.
+ * Note that this function cannot update values owned by other processes
+ * and does not allow repeated index values in 'indices'.
  *
+ * Not collective.
  **/
 HYPRE_Int HYPRE_IJVectorUpdateValues(HYPRE_IJVector       vector,
                                      HYPRE_Int            nvalues,
