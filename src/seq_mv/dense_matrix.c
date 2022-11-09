289a6e7c8b5a38c867b44b51ab63a1c4579da9af
Victor A. P. Magri <paludettomag1@llnl.gov>
2022-11-08 19:25:15 -0500

    Add new members to DenseMatrix

--- src/seq_mv/dense_matrix.c
+++ src/seq_mv/dense_matrix.c
@@ -25,17 +25,25 @@ hypre_DenseMatrixCreate( HYPRE_Int num_rows,
 
    matrix = hypre_CTAlloc(hypre_DenseMatrix, 1, HYPRE_MEMORY_HOST);
 
-   hypre_DenseMatrixNumRows(matrix)        = num_rows;
-   hypre_DenseMatrixNumCols(matrix)        = num_cols;
-   hypre_DenseMatrixSize(matrix)           = num_rows * num_cols;
-   hypre_DenseMatrixData(matrix)           = NULL;
-   hypre_DenseMatrixDataAOP(matrix)        = NULL;
-   hypre_DenseMatrixMemoryLocation(matrix) = hypre_HandleMemoryLocation(hypre_handle());
+   hypre_DenseMatrixType(matrix)             = HYPRE_DENSE_MATRIX_STANDARD;
+   hypre_DenseMatrixNumRows(matrix)          = num_rows;
+   hypre_DenseMatrixNumCols(matrix)          = num_cols;
+   hypre_DenseMatrixNumCoefs(matrix)         = num_rows * num_cols;
+   hypre_DenseMatrixData(matrix)             = NULL;
+   hypre_DenseMatrixDataAOP(matrix)          = NULL;
+   hypre_DenseMatrixVBatchNumRows(matrix)    = NULL;
+   hypre_DenseMatrixVBatchNumCols(matrix)    = NULL;
+   hypre_DenseMatrixVBatchNumCoefs(matrix)   = NULL;
+   hypre_DenseMatrixMemoryLocation(matrix)   = hypre_HandleMemoryLocation(hypre_handle());
 
    /* set defaults */
-   hypre_DenseMatrixRowMajor(matrix)       = 1;
-   hypre_DenseMatrixOwnsData(matrix)       = 0;
-   hypre_DenseMatrixNumBatches(matrix)     = 1;
+   hypre_DenseMatrixRowMajor(matrix)         = 1;
+   hypre_DenseMatrixOwnsData(matrix)         = 0;
+   hypre_DenseMatrixNumBatches(matrix)       = 1;
+   hypre_DenseMatrixUBatchNumRows(matrix)    = num_rows;
+   hypre_DenseMatrixUBatchNumCols(matrix)    = num_cols;
+   hypre_DenseMatrixUBatchNumCoefs(matrix)   = hypre_DenseMatrixNumCoefs(matrix);
+   hypre_DenseMatrixVBatchOwnsArrays(matrix) = 0;
 
    return matrix;
 }
@@ -59,9 +67,84 @@ hypre_DenseMatrixDestroy( hypre_DenseMatrix *matrix )
       /* data_aop is always owned by a hypre_DenseMatrix */
       hypre_TFree(hypre_DenseMatrixDataAOP(matrix), memory_location);
 
+      /* Free variable batched dense matrices info */
+      if (hypre_DenseMatrixVBatchOwnsArrays(matrix))
+      {
+         hypre_TFree(hypre_DenseMatrixVBatchNumCoefs(matrix), memory_location);
+         if (hypre_DenseMatrixVBatchNumRows(matrix) !=
+             hypre_DenseMatrixVBatchNumCols(matrix))
+         {
+            hypre_TFree(hypre_DenseMatrixVBatchNumRows(matrix), memory_location);
+            hypre_TFree(hypre_DenseMatrixVBatchNumCols(matrix), memory_location);
+         }
+         else
+         {
+            hypre_TFree(hypre_DenseMatrixVBatchNumRows(matrix), memory_location);
+         }
+      }
+
       /* Free matrix pointer */
       hypre_TFree(matrix, HYPRE_MEMORY_HOST);
    }
+
+   return hypre_error_flag;
+}
+
+/*--------------------------------------------------------------------------
+ * hypre_DenseMatrixSetBatchedUniform
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Int
+hypre_DenseMatrixSetBatchedUniform( hypre_DenseMatrix  *matrix,
+                                    HYPRE_Int           num_batches )
+{
+   HYPRE_Int  num_rows  = hypre_DenseMatrixNumRows(matrix);
+   HYPRE_Int  num_cols  = hypre_DenseMatrixNumCols(matrix);
+   HYPRE_Int  num_coefs = hypre_DenseMatrixNumCoefs(matrix);
+
+   if (num_batches < 1)
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Number of batches need to be greater than 0\n");
+      return hypre_error_flag;
+   }
+
+   hypre_DenseMatrixType(matrix)           = HYPRE_DENSE_MATRIX_UBATCHED;
+   hypre_DenseMatrixNumBatches(matrix)     = num_batches;
+   hypre_DenseMatrixUBatchNumRows(matrix)  = 1 + ((num_rows - 1) / num_batches);
+   hypre_DenseMatrixUBatchNumCols(matrix)  = 1 + ((num_cols - 1) / num_batches);
+   hypre_DenseMatrixUBatchNumCoefs(matrix) = 1 + ((num_coefs - 1) / num_batches);
+
+   return hypre_error_flag;
+}
+
+/*--------------------------------------------------------------------------
+ * hypre_DenseMatrixSetBatchedVariable
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Int
+hypre_DenseMatrixSetBatchedVariable( hypre_DenseMatrix  *matrix,
+                                     HYPRE_Int           num_batches,
+                                     HYPRE_Int          *vbatch_num_rows,
+                                     HYPRE_Int          *vbatch_num_cols,
+                                     HYPRE_Int          *vbatch_num_coefs )
+{
+   if (num_batches < 1)
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Number of batches need to be greater than 0\n");
+      return hypre_error_flag;
+   }
+
+   hypre_DenseMatrixType(matrix)             = HYPRE_DENSE_MATRIX_VBATCHED;
+   hypre_DenseMatrixNumBatches(matrix)       = num_batches;
+   hypre_DenseMatrixVBatchNumRows(matrix)    = vbatch_num_rows;
+   hypre_DenseMatrixVBatchNumCols(matrix)    = vbatch_num_cols;
+   hypre_DenseMatrixVBatchNumCoefs(matrix)   = vbatch_num_coefs;
+   hypre_DenseMatrixVBatchOwnsArrays(matrix) = 1;
+
+   /* Reset defaults referring to uniform batched matrices */
+   hypre_DenseMatrixUBatchNumRows(matrix)     = 0;
+   hypre_DenseMatrixUBatchNumCols(matrix)     = 0;
+   hypre_DenseMatrixUBatchNumCoefs(matrix)    = 0;
 
    return hypre_error_flag;
 }
@@ -72,25 +155,27 @@ hypre_DenseMatrixDestroy( hypre_DenseMatrix *matrix )
 
 HYPRE_Int
 hypre_DenseMatrixInitialize_v2( hypre_DenseMatrix    *matrix,
-                                HYPRE_MemoryLocation *memory_location )
+                                HYPRE_MemoryLocation  memory_location )
 {
-   HYPRE_Int   size        = hypre_DenseMatrixSize(matrix);
+   HYPRE_Int   num_coefs   = hypre_DenseMatrixNumCoefs(matrix);
    HYPRE_Int   num_batches = hypre_DenseMatrixNumBatches(matrix);
 
    hypre_DenseMatrixMemoryLocation(matrix) = memory_location;
 
    /* Allocate memory for data */
-   if (!hypre_DenseMatrixData(matrix) && size)
+   if (!hypre_DenseMatrixData(matrix) && num_coefs)
    {
-      hypre_DenseMatrixData(matrix) = hypre_CTAlloc(HYPRE_Complex, size, memory_location);
+      hypre_DenseMatrixData(matrix) = hypre_CTAlloc(HYPRE_Complex, num_coefs, memory_location);
 
       if (num_batches > 1)
       {
-         hypre_DenseMatrixDataAOP(matrix) = hypre_TAlloc(HYPRE_Complex,
+         hypre_DenseMatrixDataAOP(matrix) = hypre_TAlloc(HYPRE_Complex *,
                                                          num_batches,
                                                          memory_location);
       }
    }
+
+   hypre_DenseMatrixOwnsData(matrix) = 1;
 
    return hypre_error_flag;
 }
@@ -102,5 +187,5 @@ hypre_DenseMatrixInitialize_v2( hypre_DenseMatrix    *matrix,
 HYPRE_Int
 hypre_DenseMatrixInitialize( hypre_DenseMatrix *matrix )
 {
-   return hypre_DenseMatrixInitialize(matrix, hypre_DenseMatrixMemoryLocation(matrix));
+   return hypre_DenseMatrixInitialize_v2(matrix, hypre_DenseMatrixMemoryLocation(matrix));
 }
--- src/seq_mv/dense_matrix.h
+++ src/seq_mv/dense_matrix.h
@@ -9,34 +9,65 @@
 #define hypre_DENSE_MATRIX_HEADER
 
 /*--------------------------------------------------------------------------
+ * hypre_DenseMatrixType
+ *--------------------------------------------------------------------------*/
+
+typedef enum hypre_DenseMatrixType_enum
+{
+  HYPRE_DENSE_MATRIX_STANDARD = 0,
+  HYPRE_DENSE_MATRIX_UBATCHED = 1,
+  HYPRE_DENSE_MATRIX_VBATCHED = 2
+} hypre_DenseMatrixType;
+
+/*--------------------------------------------------------------------------
  * Dense Matrix data structure
  *--------------------------------------------------------------------------*/
 
-typedef struct
+typedef struct hypre_DenseMatrix_struct
 {
-   HYPRE_Int             row_major;
-   HYPRE_Int             num_rows;
-   HYPRE_Int             num_cols;
-   HYPRE_Int             num_batches;
-   HYPRE_Int             size;
-   HYPRE_Int             owns_data;
-   HYPRE_Complex        *data;
-   HYPRE_Complex       **data_aop;
-   HYPRE_MemoryLocation  memory_location; /* memory location of data array */
+   hypre_DenseMatrixType type;               /* Dense matrix type */
+   HYPRE_Int             row_major;          /* Flag indicating storage format */
+   HYPRE_Int             num_rows;           /* Number of rows of entire matrix */
+   HYPRE_Int             num_cols;           /* Number of columns of entire matrix */
+   HYPRE_Int             num_coefs;          /* Number of coefficients of entire matrix */
+   HYPRE_Int             owns_data;          /* Flag indicating ownership of the data array */
+   HYPRE_Complex        *data;               /* Matrix coefficients */
+   HYPRE_MemoryLocation  memory_location;    /* Memory location of data array */
+
+   /* Uniform batched dense matrices info */
+   HYPRE_Int             num_batches;        /* Number of sub-matrices (batch size) */
+   HYPRE_Int             ubatch_num_rows;    /* Number of rows of a sub-matrix */
+   HYPRE_Int             ubatch_num_cols;    /* Number of columns of a sub-matrix */
+   HYPRE_Int             ubatch_num_coefs;   /* Number of coefficients of a sub-matrix */
+
+   /* Variable batched dense matrices info */
+   HYPRE_Int            *vbatch_num_rows;    /* Number of rows of each sub-matrix */
+   HYPRE_Int            *vbatch_num_cols;    /* Number of columns of each sub-matrix */
+   HYPRE_Int            *vbatch_num_coefs;   /* Number of coefficients of each sub-matrix */
+   HYPRE_Int             vbatch_owns_arrays; /* Flag indicating ownership of the arrays above */
+   HYPRE_Complex       **data_aop;           /* Array of pointers to data */
 } hypre_DenseMatrix;
 
 /*--------------------------------------------------------------------------
  * Accessor functions for the Dense Matrix structure
  *--------------------------------------------------------------------------*/
 
+#define hypre_DenseMatrixType(matrix)                 ((matrix) -> type)
 #define hypre_DenseMatrixRowMajor(matrix)             ((matrix) -> row_major)
 #define hypre_DenseMatrixNumRows(matrix)              ((matrix) -> num_rows)
 #define hypre_DenseMatrixNumCols(matrix)              ((matrix) -> num_cols)
-#define hypre_DenseMatrixNumBatches(matrix)           ((matrix) -> num_batches)
-#define hypre_DenseMatrixSize(matrix)                 ((matrix) -> size)
+#define hypre_DenseMatrixNumCoefs(matrix)             ((matrix) -> num_coefs)
 #define hypre_DenseMatrixOwnsData(matrix)             ((matrix) -> owns_data)
 #define hypre_DenseMatrixData(matrix)                 ((matrix) -> data)
-#define hypre_DenseMatrixDataAOP(matrix)              ((matrix) -> data_aop)
 #define hypre_DenseMatrixMemoryLocation(matrix)       ((matrix) -> memory_location)
+#define hypre_DenseMatrixNumBatches(matrix)           ((matrix) -> num_batches)
+#define hypre_DenseMatrixUBatchNumRows(matrix)        ((matrix) -> ubatch_num_rows)
+#define hypre_DenseMatrixUBatchNumCols(matrix)        ((matrix) -> ubatch_num_cols)
+#define hypre_DenseMatrixUBatchNumCoefs(matrix)       ((matrix) -> ubatch_num_coefs)
+#define hypre_DenseMatrixVBatchNumRows(matrix)        ((matrix) -> vbatch_num_rows)
+#define hypre_DenseMatrixVBatchNumCols(matrix)        ((matrix) -> vbatch_num_cols)
+#define hypre_DenseMatrixVBatchNumCoefs(matrix)       ((matrix) -> vbatch_num_coefs)
+#define hypre_DenseMatrixVBatchOwnsArrays(matrix)     ((matrix) -> vbatch_owns_arrays)
+#define hypre_DenseMatrixDataAOP(matrix)              ((matrix) -> data_aop)
 
 #endif
