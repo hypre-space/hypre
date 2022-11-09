a6c31076dce61d9e7eb52d8662d41d357b3c7e5f
Victor A. P. Magri <paludettomag1@llnl.gov>
2022-11-08 19:25:47 -0500

    Add new member to DirectSolver

--- src/seq_ls/direct_solver.c
+++ src/seq_ls/direct_solver.c
@@ -13,29 +13,36 @@
  *--------------------------------------------------------------------------*/
 
 void*
-hypre_DirectSolverCreate(HYPRE_Int option)
+hypre_DirectSolverCreate( hypre_DirectSolverBackend backend,
+                          hypre_DirectSolverMethod  method,
+                          HYPRE_Int                 info_size,
+                          HYPRE_MemoryLocation      memory_location )
 {
    hypre_DirectSolverData  *data;
 
    data = hypre_TAlloc(hypre_DirectSolverData, 1, HYPRE_MEMORY_HOST);
 
    /* Set default values */
-   hypre_DirectSolverDataOption(data) = option;
-   hypre_DirectSolverDataInfo(data)   = 0;
+   hypre_DirectSolverDataBackend(data)        = backend;
+   hypre_DirectSolverDataMethod(data)         = method;
+   hypre_DirectSolverDataInfoSize(data)       = info_size;
+   hypre_DirectSolverDataPivots(data)         = NULL;
+   hypre_DirectSolverDataInfo(data)           = NULL;
+   hypre_DirectSolverDataMemoryLocation(data) = memory_location;
 
    /* TODO: Call specific create routines for each solver option */
-   switch (option)
+   switch (backend)
    {
-      case 1:
+      case HYPRE_DIRECT_SOLVER_VENDOR:
          hypre_printf("Call Create for option 1\n");
          break;
 
-      case 2:
+      case HYPRE_DIRECT_SOLVER_CUSTOM:
          hypre_printf("Call Create for option 2\n");
          break;
 
       default:
-         hypre_printf("Unknown solver option!\n");
+         hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unknown direct solver backend!\n");
    }
 
    return (void*) data;
@@ -49,26 +56,30 @@ HYPRE_Int
 hypre_DirectSolverDestroy(void* vdata)
 {
    hypre_DirectSolverData  *data = (hypre_DirectSolverData*) vdata;
+   HYPRE_MemoryLocation     memory_location = hypre_DirectSolverDataMemoryLocation(data);
 
    if (data)
    {
-      /* TODO: Call specific destroy routines for each solver option */
-      switch (hypre_DirectSolverDataOption(data))
-      {
-         case 1:
-            hypre_printf("Call Destroy for option 1\n");
-            break;
-
-         case 2:
-            hypre_printf("Call Destroy for option 2\n");
-            break;
-
-         default:
-            hypre_printf("Unknown solver option!\n");
-      }
-
+      hypre_TFree(hypre_DirectSolverDataInfo(data), memory_location);
+      hypre_TFree(hypre_DirectSolverDataPivots(data), memory_location);
       hypre_TFree(data, HYPRE_MEMORY_HOST);
    }
+
+   return hypre_error_flag;
+}
+
+/*--------------------------------------------------------------------------
+ * hypre_DirectSolverInitialize
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Int
+hypre_DirectSolverInitialize(void* vdata)
+{
+   hypre_DirectSolverData  *data = (hypre_DirectSolverData*) vdata;
+   HYPRE_Int                size = hypre_DirectSolverDataInfoSize(data);
+   HYPRE_MemoryLocation     memory_location = hypre_DirectSolverDataMemoryLocation(data);
+
+   hypre_DirectSolverDataInfo(data) = hypre_CTAlloc(HYPRE_Int, size, memory_location);
 
    return hypre_error_flag;
 }
@@ -78,12 +89,44 @@ hypre_DirectSolverDestroy(void* vdata)
  *--------------------------------------------------------------------------*/
 
 HYPRE_Int
-hypre_DirectSolverSetup( void               *direct_vdata,
+hypre_DirectSolverSetup( void               *vdata,
                          hypre_DenseMatrix  *A,
                          hypre_Vector       *f,
                          hypre_Vector       *u )
 {
-   /* TODO: Call specific setup routines for each solver option */
+   hypre_DirectSolverData     *data = (hypre_DirectSolverData*) vdata;
+   hypre_DirectSolverBackend   backend = hypre_DirectSolverDataBackend(data);
+   HYPRE_MemoryLocation        memory_location = hypre_DirectSolverDataMemoryLocation(data);
+
+   /*-----------------------------------------------------
+    * Sanity check
+    *-----------------------------------------------------*/
+
+   if (memory_location != hypre_DenseMatrixMemoryLocation(A))
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unexpected memory location for A!\n");
+      return hypre_error_flag;
+   }
+
+   /*-----------------------------------------------------
+    * Select appropiate backend
+    *-----------------------------------------------------*/
+
+   switch (backend)
+   {
+#if defined (HYPRE_USING_CUDA)
+      case HYPRE_DIRECT_SOLVER_VENDOR:
+         hypre_DirectSolverSetupVendor(data, A, f, u);
+         break;
+#endif
+
+      case HYPRE_DIRECT_SOLVER_CUSTOM:
+         hypre_DirectSolverSetupCustom(data, A, f, u);
+         break;
+
+      default:
+         hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unknown direct solver backend!\n");
+   }
 
    return hypre_error_flag;
 }
@@ -93,12 +136,108 @@ hypre_DirectSolverSetup( void               *direct_vdata,
  *--------------------------------------------------------------------------*/
 
 HYPRE_Int
-hypre_DirectSolverSolve( void               *direct_vdata,
+hypre_DirectSolverSolve( void               *vdata,
                          hypre_DenseMatrix  *A,
                          hypre_Vector       *f,
                          hypre_Vector       *u )
 {
-   /* TODO: Call specific solve routines for each solver option */
+   hypre_DirectSolverData     *data = (hypre_DirectSolverData*) vdata;
+   hypre_DirectSolverBackend   backend = hypre_DirectSolverDataBackend(data);
+   HYPRE_MemoryLocation        memory_location = hypre_DirectSolverDataMemoryLocation(data);
+
+   /*-----------------------------------------------------
+    * Sanity checks
+    *-----------------------------------------------------*/
+
+   if (memory_location != hypre_DenseMatrixMemoryLocation(A))
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unexpected memory location for A!\n");
+      return hypre_error_flag;
+   }
+
+   if (memory_location != hypre_VectorMemoryLocation(f))
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unexpected memory location for f!\n");
+      return hypre_error_flag;
+   }
+
+   if (memory_location != hypre_VectorMemoryLocation(u))
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unexpected memory location for u!\n");
+      return hypre_error_flag;
+   }
+
+   /*-----------------------------------------------------
+    * Select appropiate backend
+    *-----------------------------------------------------*/
+
+   switch (backend)
+   {
+#if defined (HYPRE_USING_CUDA)
+      case HYPRE_DIRECT_SOLVER_VENDOR:
+         hypre_DirectSolverSolveVendor(data, A, f, u);
+         break;
+#endif
+
+      case HYPRE_DIRECT_SOLVER_CUSTOM:
+         hypre_DirectSolverSolveCustom(data, A, f, u);
+         break;
+
+      default:
+         hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unknown direct solver backend!\n");
+   }
+
+   return hypre_error_flag;
+}
+
+/*--------------------------------------------------------------------------
+ * hypre_DirectSolverInvert
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Int
+hypre_DirectSolverInvert( void               *vdata,
+                          hypre_DenseMatrix  *A,
+                          hypre_DenseMatrix  *Ainv )
+{
+   hypre_DirectSolverData     *data = (hypre_DirectSolverData*) vdata;
+   hypre_DirectSolverBackend   backend = hypre_DirectSolverDataBackend(data);
+   HYPRE_MemoryLocation        memory_location = hypre_DirectSolverDataMemoryLocation(data);
+
+   /*-----------------------------------------------------
+    * Sanity check
+    *-----------------------------------------------------*/
+
+   if (memory_location != hypre_DenseMatrixMemoryLocation(A))
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unexpected memory location for A!\n");
+      return hypre_error_flag;
+   }
+
+   if (memory_location != hypre_DenseMatrixMemoryLocation(Ainv))
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unexpected memory location for Ainv!\n");
+      return hypre_error_flag;
+   }
+
+   /*-----------------------------------------------------
+    * Select appropiate backend
+    *-----------------------------------------------------*/
+
+   switch (backend)
+   {
+#if defined (HYPRE_USING_CUDA)
+      case HYPRE_DIRECT_SOLVER_VENDOR:
+         hypre_DirectSolverInvertVendor(data, A, Ainv);
+         break;
+#endif
+
+      case HYPRE_DIRECT_SOLVER_CUSTOM:
+         hypre_DirectSolverInvertCustom(data, A, Ainv);
+         break;
+
+      default:
+         hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Unknown direct solver backend!\n");
+   }
 
    return hypre_error_flag;
 }
--- src/seq_ls/direct_solver.h
+++ src/seq_ls/direct_solver.h
@@ -9,25 +9,51 @@
 #define hypre_DIRECT_SOLVER_DATA_HEADER
 
 /*--------------------------------------------------------------------------
+ * hypre_DirectSolverBackend
+ *--------------------------------------------------------------------------*/
+
+typedef enum hypre_DirectSolverBackend_enum
+{
+  HYPRE_DIRECT_SOLVER_VENDOR = 0,
+  HYPRE_DIRECT_SOLVER_CUSTOM = 1
+} hypre_DirectSolverBackend;
+
+/*--------------------------------------------------------------------------
+ * hypre_DirectSolverMethod
+ *--------------------------------------------------------------------------*/
+
+typedef enum hypre_DirectSolverMethod_enum
+{
+  HYPRE_DIRECT_SOLVER_LU    = 0,
+  HYPRE_DIRECT_SOLVER_LUPIV = 1,
+  HYPRE_DIRECT_SOLVER_CHOL  = 2
+} hypre_DirectSolverMethod;
+
+/*--------------------------------------------------------------------------
  * hypre_DirectSolverData
  *--------------------------------------------------------------------------*/
 
 typedef struct hypre_DirectSolverData_struct
 {
-   HYPRE_Int            option;
-   HYPRE_Int            info;
+   hypre_DirectSolverBackend   backend;
+   hypre_DirectSolverMethod    method;
 
-#if defined (HYPRE_USING_CUSOLVER)
-   cusolverDnHandle_t   cusolver_handle;
-#endif
+   HYPRE_Int                  *pivots;
+
+   HYPRE_Int                   info_size;
+   HYPRE_Int                  *info;
+   HYPRE_MemoryLocation        memory_location;
 } hypre_DirectSolverData;
 
 /*--------------------------------------------------------------------------
  *  Accessor functions for the hypre_DirectSolverData structure
  *--------------------------------------------------------------------------*/
 
-#define hypre_DirectSolverDataOption(data)          ((data) -> option)
+#define hypre_DirectSolverDataBackend(data)         ((data) -> backend)
+#define hypre_DirectSolverDataMethod(data)          ((data) -> method)
+#define hypre_DirectSolverDataPivots(data)          ((data) -> pivots)
+#define hypre_DirectSolverDataInfoSize(data)        ((data) -> info_size)
 #define hypre_DirectSolverDataInfo(data)            ((data) -> info)
-#define hypre_DirectSolverDataCuSolverHandle(data)  ((data) -> cusolver_handle)
+#define hypre_DirectSolverDataMemoryLocation(data)  ((data) -> memory_location)
 
 #endif
