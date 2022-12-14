fdf1f85fcd835c46ec801d4e3dbdf4fef79b5758
Victor A. P. Magri <paludettomag1@llnl.gov>
2022-12-14 11:13:46 -0500

    hypre_GetTiming was not declared properly in timing.h

--- src/utilities/_hypre_utilities.h
+++ src/utilities/_hypre_utilities.h
@@ -1103,7 +1103,7 @@ HYPRE_Real time_get_cpu_seconds_( void );
 #define hypre_EndTiming(i)
 #define hypre_PrintTiming(heading, comm)
 #define hypre_ClearTiming()
-#define hypre_GetTiming()
+#define hypre_GetTiming(heading, comm, time)
 
 /*--------------------------------------------------------------------------
  * With timing on
