16e105d794dfaec0cba51d5dcc1d242c3b65f9ce
Rob Falgout <rfalgout@llnl.gov>
2023-01-06 10:23:44 -0800

    The last commit worked.  Trying to minimize customizations.

--- src/docs/usr-manual/conf.py
+++ src/docs/usr-manual/conf.py
@@ -21,10 +21,8 @@ import subprocess
 # Read the docs
 import sphinx_rtd_theme
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-
+# Run doxygen if building on Read the Docs
+if os.environ.get('READTHEDOCS'):
     subprocess.call('cd ../ref-manual; doxygen conf.doxygen', shell=True)
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -40,7 +38,15 @@ if read_the_docs_build:
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'breathe', 'sphinx_rtd_theme']
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'breathe',
+    'sphinx_rtd_theme',
+]
 breathe_projects = { 'hypre': '../ref-manual/xml' }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -80,7 +86,7 @@ today = 'December 20, 2022'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -107,7 +113,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+
+# Classic
 #html_theme = 'classic'
 
 # Alabaster
@@ -119,16 +126,15 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
 
-## Classic
+# Classic
 #html_theme_options = {
 #    'body_min_width': '900px',
 #    'sidebarwidth': '300px',
 #    'stickysidebar': 'true',
 #}
 
-## Alabaster
+# Alabaster
 #html_theme_options = {
 #    'page_width': '1200px',
 #    'sidebar_width': '300px',
@@ -140,13 +146,9 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
 
 # Alabaster
 #html_theme_path = [alabaster.get_path()]
-
-# Read the docs
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -180,9 +182,8 @@ html_css_files = ["custom.css"]
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
 
-## Classic
+# Classic
 #html_sidebars = {
 #    '**': [
 #        'globaltoc.html',
