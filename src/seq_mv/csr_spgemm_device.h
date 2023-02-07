705ae0a7b1aa81da07e5cfa3ab181594c40559b4
Wayne Mitchell <mitchell82@llnl.gov>
2023-02-07 19:26:17 +0000

    Update comment

--- src/seq_mv/csr_spgemm_device.h
+++ src/seq_mv/csr_spgemm_device.h
@@ -56,8 +56,7 @@ constexpr HYPRE_Int T_GROUP_SIZE[11]    = { 0,  2,  4,   8,  16,  32,   64,  128
 
 /* ----------------------------------------------------------------------------------------------- *
  * these are under the assumptions made in spgemm on block sizes: only use in csr_spgemm routines
- * where we assume CUDA block is 3D and blockDim.x * blockDim.y = GROUP_SIZE which is a multiple
- * of HYPRE_WARP_SIZE
+ * where we assume CUDA block is 3D and blockDim.x * blockDim.y = GROUP_SIZE
  * WM: NOTE - sycl linearizes indices differently from cuda, i.e. a cuda block with dimensions
  * (x, y, z) is equivalent to a sycl nd_range (z, y, x)
  *------------------------------------------------------------------------------------------------ */
