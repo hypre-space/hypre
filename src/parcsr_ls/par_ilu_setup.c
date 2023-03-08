af8ab1a2f6f02b9bb554c166211173c1c7f1cff4
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-03-07 22:37:23 -0500

    Add multicomp vector check to ILU

--- src/parcsr_ls/par_ilu_setup.c
+++ src/parcsr_ls/par_ilu_setup.c
@@ -115,6 +115,13 @@ hypre_ILUSetup( void               *ilu_vdata,
    HYPRE_Int            send_size;
    HYPRE_Int            recv_size;
 
+   /* Sanity check */
+   if (hypre_ParVectorNumVectors(f) > 1)
+   {
+      hypre_error_w_msg(HYPRE_ERROR_GENERIC, "ILU doesn't support multicomponent vectors");
+      return hypre_error_flag;
+   }
+
    /* ----- begin -----*/
    HYPRE_ANNOTATE_FUNC_BEGIN;
 
