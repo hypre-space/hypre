b1ec3bc23350db50c5de0f38d9bd70bfe519c84d
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-03-21 17:34:47 -0400

    Add MAGMA interface files

---
+++ src/utilities/magma.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+ * HYPRE Project Developers. See the top-level COPYRIGHT file for details.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR MIT)
+ ******************************************************************************/
+
+#include "_hypre_utilities.h"
+#if defined(HYPRE_USING_MAGMA)
+
+/*--------------------------------------------------------------------------
+ * hypre_MagmaInit
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Int
+hypre_MagmaInit(hypre_Handle *hypre_handle_)
+{
+   /* Initialize MAGMA */
+   magma_init();
+
+   /* Create device queue */
+#if defined(HYPRE_USING_CUDA) || defined(HYPRE_USING_HIP)
+   hypre_int device_id;
+
+   hypre_GetDevice(&device_id);
+   magma_queue_create((magma_int_t) device_id, &hypre_HandleMagmaQueue(hypre_handle_));
+#endif
+
+   return hypre_error_flag;
+}
+
+/*--------------------------------------------------------------------------
+ * hypre_MagmaFinalize
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Int
+hypre_MagmaFinalize(void)
+{
+   /* Finalize MAGMA */
+   magma_finalize();
+
+   return hypre_error_flag;
+}
+
+#endif /* HYPRE_USING_MAGMA */
