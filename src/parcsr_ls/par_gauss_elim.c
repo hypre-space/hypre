e09851a061322644149b3d68fda9e71407e52450
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-04-05 18:12:34 -0400

    Code style changes + refactoring

--- src/parcsr_ls/par_gauss_elim.c
+++ src/parcsr_ls/par_gauss_elim.c
@@ -10,76 +10,80 @@
 #include "_hypre_blas.h"
 #include "_hypre_lapack.h"
 
-/*-------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
+ * hypre_GaussElimSetup
  *
- *                      Gaussian Elimination
+ * Gaussian elimination setup with local matrices and vectors generated via
+ * MPI collective calls on a sub-communicator containing only the ranks with
+ * nonzero info.
  *
- *------------------------------------------------------------------------ */
+ * This function is valid for relax_type:
+ *   - 9: hypre's internal Gaussian elimination.
+ *   - 99: LU factorization with pivoting via LAPACK.
+ *   - 199: explicit inverse A_inv = U^{-1}*L^{-1}.
+ *--------------------------------------------------------------------------*/
 
-HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYPRE_Int relax_type)
+HYPRE_Int
+hypre_GaussElimSetup(hypre_ParAMGData *amg_data,
+                     HYPRE_Int         level,
+                     HYPRE_Int         relax_type)
 {
+   /* Par Data Structure variables */
+   hypre_ParCSRMatrix   *A               = hypre_ParAMGDataAArray(amg_data)[level];
+   MPI_Comm              comm            = hypre_ParCSRMatrixComm(A);
+   HYPRE_Int             num_rows        = hypre_ParCSRMatrixNumRows(A);
+   HYPRE_Int             global_num_rows = (HYPRE_Int) hypre_ParCSRMatrixGlobalNumRows(A);
+   HYPRE_BigInt          first_row_index = hypre_ParCSRMatrixFirstRowIndex(A);
+   HYPRE_BigInt         *col_map_offd    = hypre_ParCSRMatrixColMapOffd(A);
+   hypre_CSRMatrix      *A_diag          = hypre_ParCSRMatrixDiag(A);
+   hypre_CSRMatrix      *A_offd          = hypre_ParCSRMatrixOffd(A);
+   HYPRE_MemoryLocation  memory_location = hypre_ParCSRMatrixMemoryLocation(A);
+
+   hypre_CSRMatrix      *A_diag_host;
+   hypre_CSRMatrix      *A_offd_host;
+   MPI_Comm              new_comm;
+
+   /* Local variables */
+   HYPRE_Real           *AT_mat;
+   HYPRE_Real           *Ainv;
+   HYPRE_Int             global_size;
+
 #ifdef HYPRE_PROFILE
    hypre_profile_times[HYPRE_TIMER_ID_GS_ELIM_SETUP] -= hypre_MPI_Wtime();
 #endif
 
-#if defined(HYPRE_USING_CUDA) || defined(HYPRE_USING_HIP)
+   HYPRE_ANNOTATE_FUNC_BEGIN;
    hypre_GpuProfilingPushRange("GaussElimSetup");
-#endif
-
-   /* Par Data Structure variables */
-   hypre_ParCSRMatrix *A      = hypre_ParAMGDataAArray(amg_data)[level];
-   hypre_CSRMatrix    *A_diag = hypre_ParCSRMatrixDiag(A);
-   hypre_CSRMatrix    *A_offd = hypre_ParCSRMatrixOffd(A);
-
-   HYPRE_Int num_rows = hypre_CSRMatrixNumRows(A_diag);
-   HYPRE_Int global_num_rows = (HYPRE_Int) hypre_ParCSRMatrixGlobalNumRows(A);
-   MPI_Comm comm = hypre_ParCSRMatrixComm(A);
-   MPI_Comm new_comm;
 
    /* Generate sub communicator: processes that have nonzero num_rows */
    hypre_GenerateSubComm(comm, num_rows, &new_comm);
 
    if (num_rows)
    {
-      hypre_CSRMatrix *A_diag_host, *A_offd_host;
-      if (hypre_GetActualMemLocation(hypre_CSRMatrixMemoryLocation(A_diag)) != hypre_MEMORY_HOST)
-      {
-         A_diag_host = hypre_CSRMatrixClone_v2(A_diag, 1, HYPRE_MEMORY_HOST);
-      }
-      else
-      {
-         A_diag_host = A_diag;
-      }
-      if (hypre_GetActualMemLocation(hypre_CSRMatrixMemoryLocation(A_offd)) != hypre_MEMORY_HOST)
-      {
-         A_offd_host = hypre_CSRMatrixClone_v2(A_offd, 1, HYPRE_MEMORY_HOST);
-      }
-      else
-      {
-         A_offd_host = A_offd;
-      }
+      A_diag_host = (hypre_GetActualMemLocation(memory_location) == hypre_MEMORY_DEVICE) ?
+                    hypre_CSRMatrixClone_v2(A_diag, 1, HYPRE_MEMORY_HOST) : A_diag;
+      A_offd_host = (hypre_GetActualMemLocation(memory_location) == hypre_MEMORY_DEVICE) ?
+                     hypre_CSRMatrixClone_v2(A_offd, 1, HYPRE_MEMORY_HOST) : A_offd;
 
-      HYPRE_BigInt *col_map_offd = hypre_ParCSRMatrixColMapOffd(A);
-      HYPRE_Int  *A_diag_i    = hypre_CSRMatrixI(A_diag_host);
-      HYPRE_Int  *A_offd_i    = hypre_CSRMatrixI(A_offd_host);
-      HYPRE_Int  *A_diag_j    = hypre_CSRMatrixJ(A_diag_host);
-      HYPRE_Int  *A_offd_j    = hypre_CSRMatrixJ(A_offd_host);
-      HYPRE_Real *A_diag_data = hypre_CSRMatrixData(A_diag_host);
-      HYPRE_Real *A_offd_data = hypre_CSRMatrixData(A_offd_host);
+      HYPRE_Int      *A_diag_i    = hypre_CSRMatrixI(A_diag_host);
+      HYPRE_Int      *A_offd_i    = hypre_CSRMatrixI(A_offd_host);
+      HYPRE_Int      *A_diag_j    = hypre_CSRMatrixJ(A_diag_host);
+      HYPRE_Int      *A_offd_j    = hypre_CSRMatrixJ(A_offd_host);
+      HYPRE_Complex  *A_diag_data = hypre_CSRMatrixData(A_diag_host);
+      HYPRE_Complex  *A_offd_data = hypre_CSRMatrixData(A_offd_host);
 
-      HYPRE_Real *A_mat, *A_mat_local;
-      HYPRE_Int *comm_info, *info, *displs;
-      HYPRE_Int *mat_info, *mat_displs;
-      HYPRE_Int new_num_procs, A_mat_local_size, i, jj, column;
-      HYPRE_BigInt first_row_index = hypre_ParCSRMatrixFirstRowIndex(A);
+      HYPRE_Complex  *A_mat, *A_mat_local;
+      HYPRE_Int      *comm_info, *info, *displs;
+      HYPRE_Int      *mat_info, *mat_displs;
+      HYPRE_Int       new_num_procs, A_mat_local_size, i, jj, column;
 
       hypre_MPI_Comm_size(new_comm, &new_num_procs);
 
       comm_info  = hypre_CTAlloc(HYPRE_Int, 2 * new_num_procs + 1, HYPRE_MEMORY_HOST);
-      mat_info   = hypre_CTAlloc(HYPRE_Int, new_num_procs,     HYPRE_MEMORY_HOST);
-      mat_displs = hypre_CTAlloc(HYPRE_Int, new_num_procs + 1,   HYPRE_MEMORY_HOST);
-      info = &comm_info[0];
-      displs = &comm_info[new_num_procs];
+      mat_info   = hypre_CTAlloc(HYPRE_Int, new_num_procs, HYPRE_MEMORY_HOST);
+      mat_displs = hypre_CTAlloc(HYPRE_Int, new_num_procs + 1, HYPRE_MEMORY_HOST);
+      info       = &comm_info[0];
+      displs     = &comm_info[new_num_procs];
 
       hypre_MPI_Allgather(&num_rows, 1, HYPRE_MPI_INT, info, 1, HYPRE_MPI_INT, new_comm);
 
@@ -92,11 +96,13 @@ HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYP
          mat_info[i] = global_num_rows * info[i];
       }
 
-      hypre_ParAMGDataBVec(amg_data) = hypre_CTAlloc(HYPRE_Real, global_num_rows, HYPRE_MEMORY_HOST);
+      hypre_ParAMGDataBVec(amg_data) = hypre_CTAlloc(HYPRE_Real, global_num_rows,
+                                                     HYPRE_MEMORY_HOST);
 
       A_mat_local_size = global_num_rows * num_rows;
-      A_mat_local = hypre_CTAlloc(HYPRE_Real, A_mat_local_size,                HYPRE_MEMORY_HOST);
-      A_mat       = hypre_CTAlloc(HYPRE_Real, global_num_rows * global_num_rows, HYPRE_MEMORY_HOST);
+      global_size = global_num_rows * global_num_rows;
+      A_mat_local = hypre_CTAlloc(HYPRE_Real, A_mat_local_size, HYPRE_MEMORY_HOST);
+      A_mat       = hypre_CTAlloc(HYPRE_Real, global_size, HYPRE_MEMORY_HOST);
 
       /* load local matrix into A_mat_local */
       for (i = 0; i < num_rows; i++)
@@ -107,6 +113,7 @@ HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYP
             column = A_diag_j[jj] + first_row_index;
             A_mat_local[i * global_num_rows + column] = A_diag_data[jj];
          }
+
          for (jj = A_offd_i[i]; jj < A_offd_i[i + 1]; jj++)
          {
             /* need col major */
@@ -118,10 +125,13 @@ HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYP
       hypre_MPI_Allgatherv(A_mat_local, A_mat_local_size, HYPRE_MPI_REAL, A_mat, mat_info,
                            mat_displs, HYPRE_MPI_REAL, new_comm);
 
-      if (relax_type == 99)
+      if (relax_type == 9)
       {
-         HYPRE_Real *AT_mat = hypre_CTAlloc(HYPRE_Real, global_num_rows * global_num_rows,
-                                            HYPRE_MEMORY_HOST);
+         hypre_ParAMGDataAMat(amg_data) = A_mat;
+      }
+      else if (relax_type == 99)
+      {
+         AT_mat = hypre_CTAlloc(HYPRE_Real, global_size, HYPRE_MEMORY_HOST);
          for (i = 0; i < global_num_rows; i++)
          {
             for (jj = 0; jj < global_num_rows; jj++)
@@ -132,14 +142,11 @@ HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYP
          hypre_ParAMGDataAMat(amg_data) = AT_mat;
          hypre_TFree(A_mat, HYPRE_MEMORY_HOST);
       }
-      else if (relax_type == 9)
-      {
-         hypre_ParAMGDataAMat(amg_data) = A_mat;
-      }
       else if (relax_type == 199)
       {
-         HYPRE_Real *AT_mat = hypre_TAlloc(HYPRE_Real, global_num_rows * global_num_rows, HYPRE_MEMORY_HOST);
-         HYPRE_Real *Ainv   = hypre_TAlloc(HYPRE_Real, num_rows * global_num_rows,        HYPRE_MEMORY_HOST);
+         AT_mat = hypre_TAlloc(HYPRE_Real, global_size, HYPRE_MEMORY_HOST);
+         Ainv   = hypre_TAlloc(HYPRE_Real, num_rows * global_num_rows, HYPRE_MEMORY_HOST);
+
          for (i = 0; i < global_num_rows; i++)
          {
             for (jj = 0; jj < global_num_rows; jj++)
@@ -147,17 +154,33 @@ HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYP
                AT_mat[i * global_num_rows + jj] = A_mat[i + jj * global_num_rows];
             }
          }
-         HYPRE_Int *ipiv, info, query = -1, lwork;
-         HYPRE_Real lwork_opt, *work;
+
+         HYPRE_Int  *ipiv, info, query = -1, lwork;
+         HYPRE_Real  lwork_opt, *work;
+
          ipiv = hypre_TAlloc(HYPRE_Int, global_num_rows, HYPRE_MEMORY_HOST);
          hypre_dgetrf(&global_num_rows, &global_num_rows, AT_mat, &global_num_rows, ipiv, &info);
-         hypre_assert(info == 0);
+         if (info != 0)
+         {
+            hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Problem with dgetrf!");
+            hypre_assert(0);
+         }
+
          hypre_dgetri(&global_num_rows, AT_mat, &global_num_rows, ipiv, &lwork_opt, &query, &info);
-         hypre_assert(info == 0);
-         lwork = (HYPRE_Int)lwork_opt;
+         if (info != 0)
+         {
+            hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Problem with dgetri (query)!");
+            hypre_assert(0);
+         }
+
+         lwork = (HYPRE_Int) lwork_opt;
          work = hypre_TAlloc(HYPRE_Real, lwork, HYPRE_MEMORY_HOST);
          hypre_dgetri(&global_num_rows, AT_mat, &global_num_rows, ipiv, work, &lwork, &info);
-         hypre_assert(info == 0);
+         if (info != 0)
+         {
+            hypre_error_w_msg(HYPRE_ERROR_GENERIC, "Problem with dgetri!");
+            hypre_assert(0);
+         }
 
          for (i = 0; i < global_num_rows; i++)
          {
@@ -195,13 +218,11 @@ HYPRE_Int hypre_GaussElimSetup (hypre_ParAMGData *amg_data, HYPRE_Int level, HYP
 
    hypre_ParAMGDataGSSetup(amg_data) = 1;
 
-#if defined(HYPRE_USING_CUDA) || defined(HYPRE_USING_HIP)
-   hypre_GpuProfilingPopRange();
-#endif
-
 #ifdef HYPRE_PROFILE
    hypre_profile_times[HYPRE_TIMER_ID_GS_ELIM_SETUP] += hypre_MPI_Wtime();
 #endif
+   hypre_GpuProfilingPopRange();
+   HYPRE_ANNOTATE_FUNC_END;
 
    return hypre_error_flag;
 }
@@ -339,14 +360,7 @@ hypre_GaussElimAllSetup(hypre_ParAMGData *amg_data,
 /*--------------------------------------------------------------------------
  * hypre_GaussElimSolve
  *
- * Gaussian elimination setup with local matrices and vectors generated via
- * MPI collective calls on a sub-communicator containing only the ranks with
- * nonzero info.
- *
- * This function is valid for relax_type:
- *   - 9: hypre's internal Gaussian elimination.
- *   - 99: LU factorization with pivoting via LAPACK.
- *   - 199: explicit inverse A_inv = U^{-1}*L^{-1}.
+ * Gaussian elimination solve. See hypre_GaussElimSetup for comments.
  *--------------------------------------------------------------------------*/
 
 HYPRE_Int
@@ -354,50 +368,53 @@ hypre_GaussElimSolve(hypre_ParAMGData *amg_data,
                      HYPRE_Int         level,
                      HYPRE_Int         relax_type)
 {
+   hypre_ParCSRMatrix  *A               = hypre_ParAMGDataAArray(amg_data)[level];
+   HYPRE_Int            first_row_index = (HYPRE_Int) hypre_ParCSRMatrixFirstRowIndex(A);
+   HYPRE_Int            num_rows_global = (HYPRE_Int) hypre_ParCSRMatrixGlobalNumRows(A);
+   HYPRE_Int            num_rows        = hypre_ParCSRMatrixNumRows(A);
+
+   HYPRE_Real          *b_vec           = hypre_ParAMGDataBVec(amg_data);
+   hypre_ParVector     *f               = hypre_ParAMGDataFArray(amg_data)[level];
+   HYPRE_Real          *f_data          = hypre_VectorData(hypre_ParVectorLocalVector(f));
+   HYPRE_Real          *f_data_host;
+
+   hypre_ParVector     *u               = hypre_ParAMGDataUArray(amg_data)[level];
+   HYPRE_Real          *u_data          = hypre_VectorData(hypre_ParVectorLocalVector(u));
+   HYPRE_Real          *u_data_host;
+
+   MPI_Comm             new_comm        = hypre_ParAMGDataNewComm(amg_data);
+   HYPRE_Int           *comm_info       = hypre_ParAMGDataCommInfo(amg_data);
+   HYPRE_Int           *displs, *info;
+   HYPRE_Int            new_num_procs;
+   HYPRE_Int            one_i = 1;
+   HYPRE_Int            global_size = num_rows_global * num_rows_global;
+
+   HYPRE_Int            ierr = 0;
+
 #ifdef HYPRE_PROFILE
    hypre_profile_times[HYPRE_TIMER_ID_GS_ELIM_SOLVE] -= hypre_MPI_Wtime();
 #endif
 
-#if defined(HYPRE_USING_CUDA) || defined(HYPRE_USING_HIP)
+   HYPRE_ANNOTATE_FUNC_BEGIN;
    hypre_GpuProfilingPushRange("GaussElimSolve");
-#endif
 
-   hypre_ParCSRMatrix *A = hypre_ParAMGDataAArray(amg_data)[level];
-   hypre_CSRMatrix *A_diag = hypre_ParCSRMatrixDiag(A);
-   HYPRE_Int n = hypre_CSRMatrixNumRows(A_diag);
-   HYPRE_Int error_flag = 0;
-
+   /* Call setup if not done before */
    if (hypre_ParAMGDataGSSetup(amg_data) == 0)
    {
       hypre_GaussElimSetup(amg_data, level, relax_type);
    }
 
-   if (n)
+   if (num_rows)
    {
-      MPI_Comm new_comm = hypre_ParAMGDataNewComm(amg_data);
-      hypre_ParVector *f = hypre_ParAMGDataFArray(amg_data)[level];
-      hypre_ParVector *u = hypre_ParAMGDataUArray(amg_data)[level];
-      HYPRE_Real *b_vec  = hypre_ParAMGDataBVec(amg_data);
-      HYPRE_Real *f_data = hypre_VectorData(hypre_ParVectorLocalVector(f));
-      HYPRE_Real *u_data = hypre_VectorData(hypre_ParVectorLocalVector(u));
-      HYPRE_Int *comm_info = hypre_ParAMGDataCommInfo(amg_data);
-      HYPRE_Int *displs, *info;
-      HYPRE_Int n_global = (HYPRE_Int) hypre_ParCSRMatrixGlobalNumRows(A);
-      HYPRE_Int new_num_procs;
-      HYPRE_Int first_row_index = (HYPRE_Int) hypre_ParCSRMatrixFirstRowIndex(A);
-      HYPRE_Int one_i = 1;
-
       hypre_MPI_Comm_size(new_comm, &new_num_procs);
       info = &comm_info[0];
       displs = &comm_info[new_num_procs];
 
-      HYPRE_Real *f_data_host, *u_data_host;
-
       if (hypre_GetActualMemLocation(hypre_ParVectorMemoryLocation(f)) != hypre_MEMORY_HOST)
       {
-         f_data_host = hypre_TAlloc(HYPRE_Real, n, HYPRE_MEMORY_HOST);
+         f_data_host = hypre_TAlloc(HYPRE_Real, num_rows, HYPRE_MEMORY_HOST);
 
-         hypre_TMemcpy(f_data_host, f_data, HYPRE_Real, n, HYPRE_MEMORY_HOST,
+         hypre_TMemcpy(f_data_host, f_data, HYPRE_Real, num_rows, HYPRE_MEMORY_HOST,
                        hypre_ParVectorMemoryLocation(f));
       }
       else
@@ -407,15 +424,15 @@ hypre_GaussElimSolve(hypre_ParAMGData *amg_data,
 
       if (hypre_GetActualMemLocation(hypre_ParVectorMemoryLocation(u)) != hypre_MEMORY_HOST)
       {
-         u_data_host = hypre_TAlloc(HYPRE_Real, n, HYPRE_MEMORY_HOST);
+         u_data_host = hypre_TAlloc(HYPRE_Real, num_rows, HYPRE_MEMORY_HOST);
       }
       else
       {
          u_data_host = u_data;
       }
 
-      hypre_MPI_Allgatherv (f_data_host, n, HYPRE_MPI_REAL, b_vec, info,
-                            displs, HYPRE_MPI_REAL, new_comm);
+      hypre_MPI_Allgatherv(f_data_host, num_rows, HYPRE_MPI_REAL, b_vec, info,
+                           displs, HYPRE_MPI_REAL, new_comm);
 
       if (f_data_host != f_data)
       {
@@ -426,56 +443,56 @@ hypre_GaussElimSolve(hypre_ParAMGData *amg_data,
       {
          HYPRE_Real *A_mat = hypre_ParAMGDataAMat(amg_data);
          HYPRE_Real *A_tmp;
-         HYPRE_Int   i, my_info;
 
-         A_tmp = hypre_CTAlloc(HYPRE_Real, n_global * n_global, HYPRE_MEMORY_HOST);
-         for (i = 0; i < n_global * n_global; i++)
-         {
-            A_tmp[i] = A_mat[i];
-         }
+         A_tmp = hypre_CTAlloc(HYPRE_Real, global_size, HYPRE_MEMORY_HOST);
+         hypre_TMemcpy(A_tmp, A_mat, HYPRE_Real, global_size,
+                       HYPRE_MEMORY_HOST, HYPRE_MEMORY_HOST);
 
          if (relax_type == 9)
          {
-            hypre_gselim(A_tmp, b_vec, n_global, error_flag);
+            hypre_gselim(A_tmp, b_vec, num_rows_global, ierr);
          }
          else if (relax_type == 99) /* use pivoting */
          {
-            HYPRE_Int *piv = hypre_CTAlloc(HYPRE_Int, n_global, HYPRE_MEMORY_HOST);
+            HYPRE_Int *piv = hypre_CTAlloc(HYPRE_Int, num_rows_global, HYPRE_MEMORY_HOST);
 
             /* write over A with LU */
-            hypre_dgetrf(&n_global, &n_global, A_tmp, &n_global, piv, &my_info);
+            hypre_dgetrf(&num_rows_global, &num_rows_global, A_tmp,
+                         &num_rows_global, piv, &ierr);
 
             /* now b_vec = inv(A)*b_vec */
-            hypre_dgetrs("N", &n_global, &one_i, A_tmp, &n_global, piv, b_vec, &n_global, &my_info);
+            hypre_dgetrs("N", &num_rows_global, &one_i, A_tmp,
+                         &num_rows_global, piv, b_vec,
+                         &num_rows_global, &ierr);
 
             hypre_TFree(piv, HYPRE_MEMORY_HOST);
          }
 
-         for (i = 0; i < n; i++)
-         {
-            u_data_host[i] = b_vec[first_row_index + i];
-         }
+         hypre_TMemcpy(u_data_host, b_vec + first_row_index, HYPRE_Real, num_rows,
+                       HYPRE_MEMORY_HOST, HYPRE_MEMORY_HOST);
 
          hypre_TFree(A_tmp, HYPRE_MEMORY_HOST);
       }
       else if (relax_type == 199)
       {
-         HYPRE_Real *Ainv = hypre_ParAMGDataAInv(amg_data);
+         HYPRE_Real  *Ainv = hypre_ParAMGDataAInv(amg_data);
+         HYPRE_Real  one = 1.0, zero = 0.0;
+         char        cN = 'N';
 
-         char cN = 'N';
-         HYPRE_Real one = 1.0, zero = 0.0;
-         hypre_dgemv(&cN, &n, &n_global, &one, Ainv, &n, b_vec, &one_i, &zero, u_data_host, &one_i);
+         hypre_dgemv(&cN, &num_rows, &num_rows_global, &one,
+                     Ainv, &num_rows, b_vec, &one_i, &zero,
+                     u_data_host, &one_i);
       }
 
       if (u_data_host != u_data)
       {
-         hypre_TMemcpy(u_data, u_data_host, HYPRE_Real, n, hypre_ParVectorMemoryLocation(u),
-                       HYPRE_MEMORY_HOST);
+         hypre_TMemcpy(u_data, u_data_host, HYPRE_Real, num_rows,
+                       hypre_ParVectorMemoryLocation(u), HYPRE_MEMORY_HOST);
          hypre_TFree(u_data_host, HYPRE_MEMORY_HOST);
       }
    }
 
-   if (error_flag)
+   if (ierr)
    {
       hypre_error(HYPRE_ERROR_GENERIC);
    }
@@ -484,9 +501,8 @@ hypre_GaussElimSolve(hypre_ParAMGData *amg_data,
    hypre_profile_times[HYPRE_TIMER_ID_GS_ELIM_SOLVE] += hypre_MPI_Wtime();
 #endif
 
-#if defined(HYPRE_USING_CUDA) || defined(HYPRE_USING_HIP)
    hypre_GpuProfilingPopRange();
-#endif
+   HYPRE_ANNOTATE_FUNC_END;
 
    return hypre_error_flag;
 }
@@ -595,5 +611,3 @@ hypre_GaussElimAllSolve(hypre_ParAMGData *amg_data,
 
    return hypre_error_flag;
 }
-
-#endif
