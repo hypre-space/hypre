a3cbf7e374d5642d004d47acd1e785c24cfd61a0
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-07-22 19:04:59 -0400

    Fix clang-13 build

--- src/parcsr_ls/par_mgr_stats.c
+++ src/parcsr_ls/par_mgr_stats.c
@@ -59,48 +59,50 @@ hypre_MGRGetGlobalRelaxName(hypre_ParMGRData  *mgr_data,
 
       case 16:
          /* TODO (VPM): Move this to hypre_ILUGetName */
-         HYPRE_Solver      smoother     = hypre_ParMGRDataLevelSmootherI(mgr_data, level);
-         hypre_ParILUData *ilu_smoother = (hypre_ParILUData*) smoother;
-         HYPRE_Int         ilu_type = hypre_ParILUDataIluType(ilu_smoother);
-         HYPRE_Int         ilu_fill = hypre_ParILUDataLfil(ilu_smoother);
-
-         switch (ilu_type)
          {
-            case 0:
-               return (ilu_fill == 0) ? "BJ-ILU0" : "BJ-ILUK";
+            hypre_ParILUData *ilu_smoother = (hypre_ParILUData*)
+                                               hypre_ParMGRDataLevelSmootherI(mgr_data, level);
+            HYPRE_Int         ilu_type = hypre_ParILUDataIluType(ilu_smoother);
+            HYPRE_Int         ilu_fill = hypre_ParILUDataLfil(ilu_smoother);
 
-            case 1:
-               return "BJ-ILUT";
+            switch (ilu_type)
+            {
+               case 0:
+                  return (ilu_fill == 0) ? "BJ-ILU0" : "BJ-ILUK";
 
-            case 10:
-               return (ilu_fill == 0) ? "GMRES-ILU0" : "GMRES-ILUK";
+               case 1:
+                  return "BJ-ILUT";
 
-            case 11:
-               return "GMRES-ILUT";
+               case 10:
+                  return (ilu_fill == 0) ? "GMRES-ILU0" : "GMRES-ILUK";
 
-            case 20:
-               return (ilu_fill == 0) ? "NSH-ILU0" : "NSH-ILUK";
+               case 11:
+                  return "GMRES-ILUT";
 
-            case 21:
-               return "NSH-ILUT";
+               case 20:
+                  return (ilu_fill == 0) ? "NSH-ILU0" : "NSH-ILUK";
 
-            case 30:
-               return (ilu_fill == 0) ? "RAS-ILU0" : "RAS-ILUK";
+               case 21:
+                  return "NSH-ILUT";
 
-            case 31:
-               return "RAS-ILUT";
+               case 30:
+                  return (ilu_fill == 0) ? "RAS-ILU0" : "RAS-ILUK";
 
-            case 40:
-               return (ilu_fill == 0) ? "ddPQ-GMRES-ILU0" : "ddPQ-GMRES-ILUK";
+               case 31:
+                  return "RAS-ILUT";
 
-            case 41:
-               return "ddPQ-GMRES-ILUT";
+               case 40:
+                  return (ilu_fill == 0) ? "ddPQ-GMRES-ILU0" : "ddPQ-GMRES-ILUK";
 
-            case 50:
-               return "RAP-modILU0";
+               case 41:
+                  return "ddPQ-GMRES-ILUT";
 
-            default:
-               return "Unknown";
+               case 50:
+                  return "RAP-modILU0";
+
+               default:
+                  return "Unknown";
+            }
          }
 
       default:
