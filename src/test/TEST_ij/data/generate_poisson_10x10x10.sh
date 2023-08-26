f7d412621f3bf0c70f82076a6fe4fb33e9e83379
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-08-26 17:49:11 -0400

    Fix tux regressions

--- src/test/TEST_ij/data/generate_poisson_10x10x10.sh
+++ src/test/TEST_ij/data/generate_poisson_10x10x10.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+# Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+# HYPRE Project Developers. See the top-level COPYRIGHT file for details.
 #
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+####################################################################
 # This bash script is intended to regenerate the binary files under
 # the folders poisson_10x10x10_np1 and poisson_10x10x10_np4 if the
 # binary input/output functions from the IJ interface get changed.
+####################################################################
 
-function movefile() {
-   mv -v ${1}.${SUFFIX} ${DEST_DIR}/${1}.${PREC}.${SUFFIX}
+function movefiles() {
+   mv -v IJ.out.A.${SUFFIX}  ${DEST_DIR}/IJ.A.${PREC}.${SUFFIX}
+   mv -v IJ.out.b.${SUFFIX}  ${DEST_DIR}/IJ.b.${PREC}.${SUFFIX}
+   mv -v IJ.out.x0.${SUFFIX} ${DEST_DIR}/IJ.x0.${PREC}.${SUFFIX}
 }
 
 CWD=$(pwd)
@@ -32,18 +40,14 @@ for I in {0..3}; do
     DEST_DIR=${HYPRE_SRC}/test/TEST_ij/data/poisson_10x10x10_np1
     mpirun -np 1 ./ij ${IJOPTS}
     SUFFIX=00000.bin
-    movefile IJ.out.A
-    movefile IJ.out.b
-    movefile IJ.out.x0
+    movefiles
 
     # Parallel case
     DEST_DIR=${HYPRE_SRC}/test/TEST_ij/data/poisson_10x10x10_np4
     mpirun -np 4 ./ij ${IJOPTS} -P 2 2 1
     for J in {0..3}; do
        SUFFIX=0000${J}.bin
-       movefile IJ.out.A
-       movefile IJ.out.b
-       movefile IJ.out.x0
+       movefiles
     done
 
     rm -rf IJ.out.x.*
