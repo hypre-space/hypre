bb9bb0a0f9fbfce2b46d38ec02d1606d7dfb253f
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-10-04 09:31:29 -0400

    Add HYPRE_ParCSRPCGSetGenericPrecond

--- src/parcsr_ls/HYPRE_parcsr_ls.h
+++ src/parcsr_ls/HYPRE_parcsr_ls.h
@@ -2745,6 +2745,9 @@ HYPRE_Int HYPRE_ParCSRPCGSetPrecond(HYPRE_Solver            solver,
                                     HYPRE_PtrToParSolverFcn precond_setup,
                                     HYPRE_Solver            precond_solver);
 
+HYPRE_Int HYPRE_ParCSRPCGSetGenericPrecond(HYPRE_Solver solver,
+                                           HYPRE_Solver precond);
+
 HYPRE_Int HYPRE_ParCSRPCGGetPrecond(HYPRE_Solver  solver,
                                     HYPRE_Solver *precond_data);
 
