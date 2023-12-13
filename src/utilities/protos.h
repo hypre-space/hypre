33524991d06f5e5b4c0c088ccf117aa09288867a
Rui Peng Li <li50@llnl.gov>
2023-12-13 15:09:24 -0800

    Make GPU-aware MPI a runtime option (#1031)
    This PR makes GPU-aware MPI a runtime option.

--- src/utilities/protos.h
+++ src/utilities/protos.h
@@ -389,6 +389,8 @@ HYPRE_Int hypre_SetUseGpuRand( HYPRE_Int use_gpurand );
 HYPRE_Int hypre_SetGaussSeidelMethod( HYPRE_Int gs_method );
 HYPRE_Int hypre_SetUserDeviceMalloc(GPUMallocFunc func);
 HYPRE_Int hypre_SetUserDeviceMfree(GPUMfreeFunc func);
+HYPRE_Int hypre_SetGpuAwareMPI( HYPRE_Int use_gpu_aware_mpi );
+HYPRE_Int hypre_GetGpuAwareMPI(void);
 
 /* int_array.c */
 hypre_IntArray* hypre_IntArrayCreate( HYPRE_Int size );
