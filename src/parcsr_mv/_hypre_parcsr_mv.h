3e067383baa5081ae5a002ba76ac855173821fd1
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-12-19 22:07:18 -0500

    Add hypre_ParCSRMatrixColSum

--- src/parcsr_mv/_hypre_parcsr_mv.h
+++ src/parcsr_mv/_hypre_parcsr_mv.h
@@ -1066,6 +1066,7 @@ HYPRE_Int hypre_ParCSRMatrixAddDevice( HYPRE_Complex alpha, hypre_ParCSRMatrix *
 HYPRE_Int hypre_ParCSRMatrixBlockColSum( hypre_ParCSRMatrix *A, HYPRE_Int row_major,
                                          HYPRE_Int num_rows_block, HYPRE_Int num_cols_block,
                                          hypre_DenseBlockMatrix **B_ptr );
+HYPRE_Int hypre_ParCSRMatrixColSum( hypre_ParCSRMatrix *A, hypre_ParVector **B_ptr );
 
 /* par_csr_matop_device.c */
 HYPRE_Int hypre_ParCSRMatrixDiagScaleDevice ( hypre_ParCSRMatrix *par_A, hypre_ParVector *par_ld,
