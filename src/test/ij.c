6fc944427268ad18b4e3cd29ab2fc7f1b0854658
Rui Peng Li <li50@llnl.gov>
2024-01-26 08:41:57 -0800

    add a back compatible function (#1058)
    hypre_bind_device interface change

--- src/test/ij.c
+++ src/test/ij.c
@@ -532,7 +532,7 @@ main( hypre_int argc,
     * GPU Device binding
     * Must be done before HYPRE_Initialize() and should not be changed after
     *-----------------------------------------------------------------*/
-   hypre_bind_device(device_id, myid, num_procs, hypre_MPI_COMM_WORLD);
+   hypre_bind_device_id(device_id, myid, num_procs, hypre_MPI_COMM_WORLD);
 
    /*-----------------------------------------------------------
     * Initialize : must be the first HYPRE function to call
