89757305d60b8b8c4068914c8a9e8644f80c3572
Wayne Mitchell <mitchell82@uan-0002.head.cm.americas.sgi.com>
2024-03-11 21:27:29 +0000

    Debugging constant iterator stuff

--- src/seq_mv/csr_matop_device.c
+++ src/seq_mv/csr_matop_device.c
@@ -1022,16 +1022,24 @@ hypre_CSRMatrixColNNzRealDevice( hypre_CSRMatrix  *A,
 #if defined(HYPRE_USING_SYCL)
 
    /* WM: todo - better way to get around lack of constant iterator in DPL? */
-   HYPRE_Int *ones = hypre_TAlloc(HYPRE_Int, nnz_A, HYPRE_MEMORY_DEVICE);
-   HYPRE_ONEDPL_CALL( std::fill_n, ones, nnz_A, 1 );
+   /* HYPRE_Int *ones = hypre_TAlloc(HYPRE_Int, nnz_A, HYPRE_MEMORY_DEVICE); */
+   /* HYPRE_ONEDPL_CALL( std::fill_n, ones, nnz_A, 1 ); */
+   /* auto new_end = HYPRE_ONEDPL_CALL( oneapi::dpl::reduce_by_segment, */
+   /*                                   A_j_sorted, */
+   /*                                   A_j_sorted + nnz_A, */
+   /*                                   ones, */
+   /*                                   reduced_col_indices, */
+   /*                                   reduced_col_nnz); */
+
+   /* hypre_TFree(ones, HYPRE_MEMORY_DEVICE); */
+   oneapi::dpl::counting_iterator count(0);
    auto new_end = HYPRE_ONEDPL_CALL( oneapi::dpl::reduce_by_segment,
                                      A_j_sorted,
                                      A_j_sorted + nnz_A,
-                                     ones,
+                                     oneapi::dpl::make_transform_iterator(count, hypreSycl_constant<HYPRE_Int>(1)),
                                      reduced_col_indices,
                                      reduced_col_nnz);
 
-   hypre_TFree(ones, HYPRE_MEMORY_DEVICE);
    hypre_assert(new_end.first - reduced_col_indices == new_end.second - reduced_col_nnz);
    num_reduced_col_indices = new_end.first - reduced_col_indices;
 #else
