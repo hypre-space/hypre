632ee552550650188d21e264d13d4387a47cbba0
Victor A. P. Magri <paludettomag1@llnl.gov>
2024-04-04 14:00:58 -0400

    Add license headers

--- src/config/update-cmake.sh
+++ src/config/update-cmake.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+# HYPRE Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 DIRNAME=$(dirname $0)
 FOLDERS=(blas examples IJ_mv krylov lapack parcsr_block_mv parcsr_ls parcsr_mv seq_block_mv seq_mv sstruct_ls sstruct_mv test utilities)
