66a54a0a6d11b6235a6bd91749cf8324d3ed5408
Rob Falgout <rfalgout@llnl.gov>
2024-09-19 16:35:49 -0700

    Fixed the TEST_sstruct/solvers test

--- src/test/TEST_sstruct/solvers.sh
+++ src/test/TEST_sstruct/solvers.sh
@@ -23,7 +23,6 @@ FILES="\
  ${TNAME}.out.9\
  ${TNAME}.out.10\
  ${TNAME}.out.11\
- ${TNAME}.out.14\
  ${TNAME}.out.15\
  ${TNAME}.out.16\
  ${TNAME}.out.17\
@@ -81,6 +80,7 @@ FILES="\
 # ${TNAME}.out.6\
 # ${TNAME}.out.12\
 # ${TNAME}.out.13\
+# ${TNAME}.out.14\
 
 for i in $FILES
 do
