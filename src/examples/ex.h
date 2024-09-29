bb5a171cc3a83039dc96f1a6b4909e3b5e4e1f3d
Victor A. P. Magri <paludettomag1@llnl.gov>
2024-09-28 21:37:46 -0400

    Improve robustness of Makefile in the example folder

--- src/examples/ex.h
+++ src/examples/ex.h
@@ -14,13 +14,18 @@
 
 #include <HYPRE_config.h>
 
-#if defined(HYPRE_EXAMPLE_USING_CUDA)
-
-#include <cuda_runtime.h>
-
+#if defined(HYPRE_EXAMPLE_USING_CUDA) || defined(HYPRE_EXAMPLE_USING_HIP)
 #ifndef HYPRE_USING_UNIFIED_MEMORY
 #error *** Running the examples on GPUs requires Unified Memory. Please reconfigure and rebuild with --enable-unified-memory ***
 #endif
+#define custom_malloc(size) gpu_malloc(size)
+#define custom_calloc(num, size) gpu_calloc(num, size)
+#define custom_free(ptr) gpu_free((void**) &(ptr))
+#endif
+
+#if defined(HYPRE_EXAMPLE_USING_CUDA)
+
+#include <cuda_runtime.h>
 
 static inline void*
 gpu_malloc(size_t size)
@@ -39,14 +44,47 @@ gpu_calloc(size_t num, size_t size)
    return ptr;
 }
 
-#define custom_malloc(size) gpu_malloc(size)
-#define custom_calloc(num, size) gpu_calloc(num, size)
-#define custom_free(ptr) ( cudaFree(ptr), ptr = NULL )
+static inline void
+gpu_free(void** ptr)
+{
+   cudaFree(*ptr);
+   *ptr = NULL;
+}
+
+#elif defined(HYPRE_EXAMPLE_USING_HIP)
+
+#include <hip/hip_runtime.h>
+
+static inline void*
+gpu_malloc(size_t size)
+{
+   void *ptr = NULL;
+   hipMallocManaged(&ptr, size, hipMemAttachGlobal);
+   return ptr;
+}
+
+static inline void*
+gpu_calloc(size_t num, size_t size)
+{
+   void *ptr = NULL;
+   hipMallocManaged(&ptr, num * size, hipMemAttachGlobal);
+   hipMemset(ptr, 0, num * size);
+   return ptr;
+}
+
+static inline void
+gpu_free(void** ptr)
+{
+   hipFree(*ptr);
+   *ptr = NULL;
+}
+
 #else
-/* Default malloc, calloc, and free */
+
+/* Host: Default malloc, calloc, and free */
 #define custom_malloc(size) malloc(size)
 #define custom_calloc(num, size) calloc(num, size)
-#define custom_free(ptr) free(ptr)
+#define custom_free(ptr) (free(ptr), ptr = NULL)
 #endif
 
 #endif /* #ifdef HYPRE_EXAMPLES_INCLUDES */
