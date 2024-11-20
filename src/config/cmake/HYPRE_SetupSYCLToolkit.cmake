ecdcf55e1c7a1eb110c7915cd11a12d6ccb1edaf
Victor A. P. Magri <50467563+victorapm@users.noreply.github.com>
2024-11-20 18:03:33 -0500

    Remove duplicate

--- src/config/cmake/HYPRE_SetupSYCLToolkit.cmake
+++ src/config/cmake/HYPRE_SetupSYCLToolkit.cmake
@@ -12,9 +12,6 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ferror-limit=1")
 # Find Intel SYCL
 find_package(IntelSYCL REQUIRED)
 
-# Find Intel SYCL
-find_package(IntelSYCL REQUIRED)
-
 # Set up SYCL flags
 if(IntelSYCL_FOUND)
   # Standard SYCL flags
