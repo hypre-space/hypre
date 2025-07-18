6a25be583879c4a6c1ef82a8a82d809fb52ed847
Rob Falgout <rfalgout@llnl.gov>
2025-07-18 14:49:12 -0700

    Updating scripts for checking function lists for multiprecision

---
+++ src/config/mup_check_dir.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+# HYPRE Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# Check that current multiprecision functions are up to date.  The script will
+# create three files:
+#
+#   mup_check.old - contains the original function list
+#   mup_check.new - contains the newly generated list
+#   mup_check.err - the difference between the old/new files
+#
+# NOTE: Must be run on symbols generated from the non-multiprecision build.
+
+scriptdir=`dirname $0`
+
+export LC_COLLATE=C  # sort by listing capital letters first
+
+cat mup.fixed mup.functions mup.methods | sort | uniq  > mup_check.old
+$scriptdir/generate_function_list.sh    | sort | uniq  > mup_check.new
+
+diff -wc mup_check.old mup_check.new                   > mup_check.err
+
+SZ=`ls -l mup_check.err | awk '{print $5}'`
+if [ $SZ != 0 ]
+then
+   echo "   UPDATE - see mup_check.err"
+else
+   echo "   OK"
+fi
