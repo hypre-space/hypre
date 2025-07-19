9f0a75fe3f3b50f8776ea77c12a0b0e0379a5d95
Rob Falgout <rfalgout@llnl.gov>
2025-07-19 06:58:36 -0700

    Updating struct_ls to multiprecision

--- src/struct_ls/_hypre_struct_ls.h
+++ src/struct_ls/_hypre_struct_ls.h
@@ -16,7 +16,7 @@
 
 #include "HYPRE_struct_ls_mp.h"
 #ifdef HYPRE_MIXED_PRECISION
-#include "struct_ls_mup_func.h"
+#include "_hypre_struct_ls_mup_def.h"
 #endif
 
 #ifdef __cplusplus
@@ -596,6 +596,17 @@ HYPRE_Int hypre_StructDiagScale( hypre_StructMatrix   *A, hypre_StructVector   *
 
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef HYPRE_MIXED_PRECISION
+/* The following is for user compiles and the order is important.  The first
+ * header ensures that we do not change prototype names in user files or in the
+ * second header file.  The second header contains all the prototypes needed by
+ * users for mixed precision. */
+#ifndef hypre_MP_BUILD
+#include "_hypre_struct_ls_mup_undef.h"
+#include "_hypre_struct_ls_mup.h"
+#endif
 #endif
 
 #endif
