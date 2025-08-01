cf259cb17a530ed63b079db8fa8e316a6c72ea15
Victor A. P. Magri <paludettomag1@llnl.gov>
2025-07-31 21:55:17 -0400

    Fix portability issue

--- src/config/generate_function_list.sh
+++ src/config/generate_function_list.sh
@@ -12,5 +12,22 @@
 # This prevents unmatched patterns from expanding (e.g., when there are no .obj files)
 shopt -s nullglob
 
+# Set arguments to "sed" based on the operating system for portability purposes
+OS_TYPE="$(uname -s)"
+case ${OS_TYPE} in
+    Darwin)
+        SED_ARGS=(-e 's/^_//' -e 's/_$//')
+        ;;
+
+    Linux)
+        SED_ARGS=(-e 's/_$//')
+        ;;
+
+    *)
+        echo "Unknown OS: ${OS_TYPE}"
+        exit 1
+        ;;
+esac
+
 # Use awk to avoid issues with spacing
-nm -P *.o *.obj | awk '$2 == "T" {print $1}' | sed -e 's/_$//'
+nm -P *.o *.obj | awk '$2 == "T" {print $1}' | sed "${SED_ARGS[@]}"
