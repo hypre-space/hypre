9d871b310b75c7dba4955ebe57d6fca775a67e54
Victor A. P. Magri <paludettomag1@llnl.gov>
2025-11-01 14:14:46 -0400

    Extend conversion function

--- src/parcsr_mv/par_csr_matrix.c
+++ src/parcsr_mv/par_csr_matrix.c
@@ -639,7 +639,7 @@ hypre_ParCSRMatrixCreateFromParVector(hypre_ParVector *b,
    HYPRE_Int             num_rows        = (HYPRE_Int) (row_starts[1] - row_starts[0]);
    HYPRE_Int             num_cols        = (HYPRE_Int) (col_starts[1] - col_starts[0]);
    HYPRE_Int             num_nonzeros    = hypre_ParVectorLocalSize(b);
-   /* HYPRE_Int             blk_dim         = num_rows / num_cols; */
+   HYPRE_Int             blk_dim         = hypre_max(1, num_rows / num_cols);
 
    /* Output matrix variables */
    hypre_ParCSRMatrix   *A;
@@ -695,15 +695,16 @@ hypre_ParCSRMatrixCreateFromParVector(hypre_ParVector *b,
    A_diag_j = hypre_CTAlloc(HYPRE_Int, num_nonzeros, HYPRE_MEMORY_HOST);
 
 #ifdef HYPRE_USING_OPENMP
-   #pragma omp parallel for HYPRE_SMP_SCHEDULE
+   #pragma omp parallel for private(i) HYPRE_SMP_SCHEDULE
 #endif
    for (i = 0; i < num_nonzeros; i++)
    {
-      A_diag_i[i] = A_diag_j[i] = i;
+      A_diag_i[i] = i;
+      A_diag_j[i] = i / blk_dim;
    }
 
 #ifdef HYPRE_USING_OPENMP
-   #pragma omp parallel for HYPRE_SMP_SCHEDULE
+   #pragma omp parallel for private(i) HYPRE_SMP_SCHEDULE
 #endif
    for (i = num_nonzeros; i < num_rows + 1; i++)
    {
