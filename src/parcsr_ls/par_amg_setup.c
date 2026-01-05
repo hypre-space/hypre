3f64ba82083b4940a45b06460b1be1267fb4d067
Victor A. P. Magri <50467563+victorapm@users.noreply.github.com>
2026-01-05 07:08:52 -0800

    Update SuperLU interface + MGR (#1450)
    - Add hypre_SLUDistCreate for creating the solver object
    - Change the hypre_DSLUData struct members to pointers
    - Remove print_level from hypre_SLUDistSetup and create a dedicate hypre_SLUDistSetPrintLevel
    - Update the setup/solve hypre_SLUDist function signatures to take A, b, and x
    - Update SuperLU_Dist and aff_solver usage in MGR to reflect the above changes

--- src/parcsr_ls/par_amg_setup.c
+++ src/parcsr_ls/par_amg_setup.c
@@ -3152,8 +3152,9 @@ hypre_BoomerAMGSetup( void               *amg_vdata,
             (coarse_size > (HYPRE_BigInt)coarse_threshold) &&
             (level != max_levels - 1))
    {
-      HYPRE_Solver dslu_solver;
-      hypre_SLUDistSetup(&dslu_solver, A_array[level], amg_print_level);
+      HYPRE_Solver dslu_solver = hypre_SLUDistCreate();
+      hypre_SLUDistSetPrintLevel(dslu_solver, amg_print_level);
+      hypre_SLUDistSetup(dslu_solver, A_array[level], NULL, NULL);
       hypre_ParAMGDataDSLUSolver(amg_data) = dslu_solver;
    }
 #endif
