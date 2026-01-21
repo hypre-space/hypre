25b6adb7b01acd50fc3024e640e8a442d7ed2f71
Victor A. P. Magri <paludettomag1@llnl.gov>
2026-01-20 19:42:45 -0800

    Minor

--- AUTOTEST/machine-matrix.sh
+++ AUTOTEST/machine-matrix.sh
@@ -133,14 +133,14 @@ co="${aco} --without-MPI --without-umpire --enable-unified-memory --with-cuda --
 # 6A) GCC 13.3.1 + CUDA 12.9.1 with Umpire
 module reset && module -q load cuda/12.9.1 gcc/13.3.1
 UMPIRE_DIR=/usr/workspace/hypre/ext-libs/Umpire/install-umpire_2025.09.0-cuda_12.9_sm90-gcc_13.3
-co="${aco} --with-cuda --with-gpu-arch=90 --with-umpire --with-umpire-include=${UMPIRE_DIR}/include --with-umpire-lib-dirs=${UMPIRE_DIR}/lib --with-umpire-libs=\"umpire camp\" --with-print-errors"
+co="${aco} --with-cuda --with-gpu-arch=90 --with-umpire --with-umpire-include=${UMPIRE_DIR}/include --with-umpire-lib-dirs=${UMPIRE_DIR}/lib --with-umpire-libs=\\"umpire camp\\" --with-print-errors"
 ./test.sh basic.sh $src_dir -co: $co -mo: $mo
 ./renametest.sh basic $output_dir/basic-cuda-bench
 
 # 7A) GCC 13.3.1 + CUDA 12.9.1 with host execution
 module reset && module -q load cuda/12.9.1 gcc/13.3.1
 co="${aco} --with-cuda --with-gpu-arch=90 --without-umpire --with-test-using-host --with-memory-tracker --enable-debug --with-print-errors"
-./test.sh basic.sh $root_dir -co: $co -mo: $mo
+./test.sh basic.sh $src_dir -co: $co -mo: $mo
 ./renametest.sh basic $output_dir/basic-cuda-cpu
 
 # Echo to stderr all nonempty error files in $output_dir
