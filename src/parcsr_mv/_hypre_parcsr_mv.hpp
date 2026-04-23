a819385332cf16eea9558bf00aac7a68c440c14d
Daniel Osei-Kuffuor <oseikuffuor1@llnl.gov>
2026-04-22 17:17:15 -0700

    Updates to fix check-license warnings.

--- src/parcsr_mv/_hypre_parcsr_mv.hpp
+++ src/parcsr_mv/_hypre_parcsr_mv.hpp
@@ -15,6 +15,13 @@
 extern "C++" {
 #endif
 
+/******************************************************************************
+ * Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+ * HYPRE Project Developers. See the top-level COPYRIGHT file for details.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR MIT)
+ ******************************************************************************/
+
 
 #ifdef __cplusplus
 }
