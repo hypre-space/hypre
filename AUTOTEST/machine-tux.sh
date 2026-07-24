5b21e00e9cd5746b658bece2d0da9df63b45edb5
Daniel Osei-Kuffuor <oseikuffuor1@llnl.gov>
2026-07-24 13:47:51 -0700

    Fix regression test script and minor cleanup.

--- AUTOTEST/machine-tux.sh
+++ AUTOTEST/machine-tux.sh
@@ -139,8 +139,8 @@ co="--enable-bigint --enable-debug"
 ./renametest.sh basic $output_dir/basic--enable-bigint
 
 co="--enable-debug --enable-mixed-precision"
-ro="-mixed-precision -rt -save ${save}"
-./test.sh basic.sh $src_dir -co: $co -mo: $mo -ro: $ro
+RO="-mixed-precision -rt -save ${save}"
+./test.sh basic.sh $src_dir -co: $co -mo: $mo -ro: $RO
 ./renametest.sh basic $output_dir/basic--mixed-precision
 
 co="--enable-debug --enable-mixed-precision --enable-shared"
