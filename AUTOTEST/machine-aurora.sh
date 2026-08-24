07c779f4072950c1d31b49c21b67ae67f1ba2185
Victor A. P. Magri <paludettomag1@llnl.gov>
2026-08-24 04:53:45 +0000

    Aurora fix

--- AUTOTEST/machine-aurora.sh
+++ AUTOTEST/machine-aurora.sh
@@ -46,6 +46,7 @@ shift
 cco="-DHYPRE_ENABLE_PRINT_ERRORS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo"
 cmo="-j all test"
 mo="-j test"
+ro="-ij-gpu -struct -sstruct -rt -save ${save} -script gpu_tile_compact.sh -rtol ${rtol} -atol ${atol}"
 eo=""
 
 rtol="0.0"
@@ -53,7 +54,6 @@ atol="3e-15"
 
 #save=`echo $(hostname) | sed 's/[0-9]\+$//'`
 save="aurora"
-ro="-ij-gpu -struct -sstruct -rt -save ${save} -script gpu_tile_compact.sh -rtol ${rtol} -atol ${atol}"
 
 module load cmake
 
